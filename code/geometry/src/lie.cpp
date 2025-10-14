d55a0e643e9651ec4fb8789588ef07f61db3b836
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-14 21:04:07 +0200

    add lie algebra

--- code/geometry/src/lie.cpp
+++ code/geometry/src/lie.cpp
@@ -1,4 +1,18 @@
 #include "lie.hpp"
 
 namespace reprojection::geometry {
+
+Eigen::Matrix3d Exp(Eigen::Vector3d const& so3) {
+    Eigen::Matrix3d const SO3{Eigen::AngleAxisd(so3.norm(), so3.normalized()).toRotationMatrix()};
+
+    return SO3;
 }
+
+Eigen::Vector3d Log(Eigen::Matrix3d const& SO3) {
+    Eigen::AngleAxisd const rotation(SO3);
+    Eigen::Vector3d const so3{rotation.angle() * rotation.axis()};
+
+    return so3;
+}
+
+}  // namespace reprojection::geometry
--- code/geometry/src/lie.hpp
+++ code/geometry/src/lie.hpp
@@ -3,4 +3,9 @@
 #include <Eigen/Dense>
 
 namespace reprojection::geometry {
-}
+
+Eigen::Matrix3d Exp(Eigen::Vector3d const& so3);
+
+Eigen::Vector3d Log(Eigen::Matrix3d const& SO3);
+
+}  // namespace reprojection::geometry
