642dab3e418af7417c2d5450a65fd1281ccf3ade
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-15 11:32:27 +0200

    fix code check path

--- building/scripts/code_checks.sh
+++ building/scripts/code_checks.sh
@@ -6,8 +6,8 @@ find /temporary/building -iname '*.sh' -print0 | xargs --null shellcheck
 
 find /temporary/code \( -iname '*.cpp' -o -iname '*.hpp' -o -iname '*.c' -o -iname '*.h' \) -print0 | xargs --null clang-format --dry-run --Werror
 cppcheck /temporary/code --enable=all --error-exitcode=1 --suppress=missingIncludeSystem \
-  -I /temporary/code/feature-extraction/include \
-  -I /temporary/code/feature-extraction/src \
+  -I /temporary/code/feature_extraction/include \
+  -I /temporary/code/feature_extraction/src \
   -I /temporary/code/geometry/include \
   -I /temporary/code/pnp/include \
   -I /temporary/code/pnp/src
