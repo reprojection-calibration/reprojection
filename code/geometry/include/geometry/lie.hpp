2413ddcd3efe2711f807eab4e6420b074ef28b9b
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-15 16:22:53 +0200

    move se3 to geometry package

--- code/geometry/include/geometry/lie.hpp
+++ code/geometry/include/geometry/lie.hpp
@@ -4,6 +4,10 @@
 
 namespace reprojection::geometry {
 
+Eigen::Isometry3d Exp(Eigen::Vector<double, 6> const& se3);
+
+Eigen::Vector<double, 6> Log(Eigen::Isometry3d const& SE3);
+
 Eigen::Matrix3d Exp(Eigen::Vector3d const& so3);
 
 Eigen::Vector3d Log(Eigen::Matrix3d const& SO3);
---
+++ code/geometry/test/lie_se3.test.cpp
@@ -0,0 +1,60 @@
+#include <gtest/gtest.h>
+
+#include <vector>
+
+#include "geometry/lie.hpp"
+
+using namespace reprojection::geometry;
+
+Eigen::Isometry3d ConstructTestSE3(Eigen::Vector3d const& R_diagonal, Eigen::Vector3d const& t) {
+    Eigen::Isometry3d SE3{Eigen::Isometry3d::Identity()};
+    SE3.linear() = R_diagonal.asDiagonal();
+    SE3.translation() = t;
+
+    return SE3;
+}
+
+std::vector<Eigen::Vector<double, 6>> const se3{Eigen::Vector<double, 6>{0, 0, 0, 0, 0, 0},     //
+                                                Eigen::Vector<double, 6>{M_PI, 0, 0, 1, 0, 0},  //
+                                                Eigen::Vector<double, 6>{0, M_PI, 0, 0, 1, 0},  //
+                                                Eigen::Vector<double, 6>{0, 0, M_PI, 0, 0, 1}};
+
+std::vector<Eigen::Isometry3d> const SE3{Eigen::Isometry3d::Identity(),             //
+                                         ConstructTestSE3({1, -1, -1}, {1, 0, 0}),  //
+                                         ConstructTestSE3({-1, 1, -1}, {0, 1, 0}),  //
+                                         ConstructTestSE3({-1, -1, 1}, {0, 0, 1})};
+
+TEST(GeometryLie, TestSe3Exp) {
+    ASSERT_EQ(std::size(se3), std::size(SE3));
+
+    for (size_t i{0}; i < std::size(se3); ++i) {
+        Eigen::Vector<double, 6> const se3_i{se3[i]};
+        Eigen::Isometry3d const SE3_i{Exp(se3_i)};
+
+        std::cout << SE3_i.matrix() << std::endl;
+
+        EXPECT_TRUE(SE3_i.isApprox(SE3[i]));
+    }
+}
+
+TEST(GeometryLie, TestSe3Log) {
+    ASSERT_EQ(std::size(se3), std::size(SE3));
+
+    for (size_t i{0}; i < std::size(se3); ++i) {
+        Eigen::Isometry3d const SE3_i{SE3[i]};
+        Eigen::Vector<double, 6> const se3_i{Log(SE3_i)};
+
+        EXPECT_TRUE(se3_i.isApprox(se3[i]));
+    }
+}
+
+TEST(GeometryLie, TestSe3Invertability) {
+    Eigen::Vector<double, 6> const se3_random{M_PI * Eigen::Vector<double, 6>::Random()};
+    Eigen::Vector<double, 6> const se3_random_processed{Log(Exp(se3_random))};
+    EXPECT_TRUE(se3_random_processed.isApprox(se3_random));
+
+    Eigen::Isometry3d SE3_random{Eigen::Quaterniond::UnitRandom().toRotationMatrix()};
+    SE3_random.translation() = Eigen::Vector3d::Random();
+    Eigen::Isometry3d const SE3_random_processed{Exp(Log(SE3_random))};
+    EXPECT_TRUE(SE3_random_processed.isApprox(SE3_random));
+}
