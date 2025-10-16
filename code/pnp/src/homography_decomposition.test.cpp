1fd8b70d8581358fb90526d2ac97ae3dea45dad6
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-16 11:09:32 +0200

    preconditioning

--- code/pnp/src/homography_decomposition.test.cpp
+++ code/pnp/src/homography_decomposition.test.cpp
@@ -2,7 +2,45 @@
 
 #include <gtest/gtest.h>
 
+#include "matrix_utilities.hpp"
+
 using namespace reprojection::pnp;
+
+void PrepForHomographySolving(Eigen::MatrixX2d const& pixels, Eigen::MatrixX3d const& points) {
+    (void)pixels;
+    auto const [_, V]{WhatDoWeNameThis(points)};
+
+    Eigen::Matrix3d R{V};
+    if (V(0, 2) * V(0, 2) + V(1, 2) * V(1, 2) < 1e-10) {
+        R = Eigen::Matrix3d::Identity();
+    } else if (R.determinant() < 0) {
+        R *= -1;
+    }
+
+    Eigen::Vector3d const center{points.colwise().mean()};
+    Eigen::Vector3d const t{-R * center};
+
+    Eigen::Isometry3d const T{ToIsometry3d(R, t)};
+
+    Eigen::MatrixX2d const normalized_chopped_points{
+        (T * points.rowwise().homogeneous().transpose()).transpose()(Eigen::all, {0, 1})};
+
+    std::cout << R << std::endl;
+    std::cout << t << std::endl;
+    std::cout << normalized_chopped_points << std::endl;
+}
+
+TEST(PnpHomographyDecomposition, TestYYY) {
+    Eigen::MatrixX2d const pixels{};  // ADD REAL PIXELS
+
+    // Any three points are on a plane!
+    Eigen::MatrixX3d const three_points{{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
+    PrepForHomographySolving(pixels, three_points);
+
+    // z=0 plane
+    Eigen::MatrixX3d plane{{0, 0, 0}, {1, 1, 0}, {-1, -1, 0}, {-1, 1, 0}, {1, -1, 0}};
+    PrepForHomographySolving(pixels, plane);
+}
 
 TEST(PnpHomographyDecomposition, TestXXX) {
     // Any three points are on a plane!
@@ -10,7 +48,7 @@ TEST(PnpHomographyDecomposition, TestXXX) {
     EXPECT_TRUE(IsPlane(three_points));
 
     // z=0 plane
-    Eigen::MatrixX3d plane{{1, 1, 0}, {2, 2, 0}, {3, 3, 0}, {-1, -1, 0}, {-1, 1, 0}, {1, -1, 0}};
+    Eigen::MatrixX3d plane{{0, 0, 0}, {1, 1, 0}, {-1, -1, 0}, {-1, 1, 0}, {1, -1, 0}};
     EXPECT_TRUE(IsPlane(plane));
 
     // Make an outlier
