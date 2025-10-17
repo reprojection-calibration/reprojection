49c9af77491f133c5caf6cc290b890fe4c88eba2
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-17 14:54:13 +0200

    add testing mock test

--- code/testing_mocks/test/mvg_generator.test.cpp
+++ code/testing_mocks/test/mvg_generator.test.cpp
@@ -1,5 +1,9 @@
-#include <gtest/gtest.h>
-
 #include "testing_mocks/mvg_generator.hpp"
 
+#include <gtest/gtest.h>
+
+#include "se3_spline.hpp"
+
 using namespace reprojection::testing_mocks;
+
+TEST(TestingMocks, TestTrajectoryGenerator) { EXPECT_FALSE(true); }
