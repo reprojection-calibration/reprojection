a5cc8e6ba79130fb4ce06f5c0f081aba2795cd0c
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-17 14:53:05 +0200

    add cmake thinking error note

--- code/cmake_macros/add_library.cmake
+++ code/cmake_macros/add_library.cmake
@@ -2,6 +2,9 @@ macro(AddLibrary)
     add_library(${LIBRARY_NAME} SHARED
             ${SRC_FILES}
     )
+    # ERROR(Jack): This separation of of the include/ and src/ folder does NOT work like I want it to. Any other sub
+    # directory can include any header file from either the include/ or src/ directory. My intention, and our design
+    # here should be that only the headers in the include folder can be used in other sub directories!
     target_include_directories(${LIBRARY_NAME} PUBLIC
             $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include/>
             $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/src/>
