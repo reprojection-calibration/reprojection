b7f624fdcf7303def0d0aa7c8ad6f0b87bd40cc1
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-17 15:05:06 +0200

    possible working src/ include/ separation

--- code/cmake_macros/add_library.cmake
+++ code/cmake_macros/add_library.cmake
@@ -2,15 +2,14 @@ macro(AddLibrary)
     add_library(${LIBRARY_NAME} SHARED
             ${SRC_FILES}
     )
-    # ERROR(Jack): This separation of of the include/ and src/ folder does NOT work like I want it to. Any other sub
-    # directory can include any header file from either the include/ or src/ directory. My intention, and our design
-    # here should be that only the headers in the include folder can be used in other sub directories!
     target_include_directories(${LIBRARY_NAME} PUBLIC
             $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include/>
+    )
+    target_include_directories(${LIBRARY_NAME} PRIVATE
             $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/src/>
             ${INCLUDE_DIRECTORIES}
     )
-    target_link_libraries(${LIBRARY_NAME} PUBLIC
+    target_link_libraries(${LIBRARY_NAME} PRIVATE
             ${LINK_LIBRARIES}
     )
 endmacro()
