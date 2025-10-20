c0b0228b9eb86c4a6b498cc1bfdc3629088dacb0
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-20 11:53:45 +0200

    test project

--- code/testing_mocks/test/mvg_generator.test.cpp
+++ code/testing_mocks/test/mvg_generator.test.cpp
@@ -169,3 +169,16 @@ TEST(TestingMocks, XXX) {
 
     EXPECT_FALSE(true);
 }
+
+TEST(TestingMocks, TestProject) {
+    Eigen::MatrixX3d const points_w{{0.00, 0.00, 5.00},   {1.00, 1.00, 5.00},   {-1.00, -1.00, 5.00},
+                                    {2.00, -1.00, 10.00}, {-2.00, 1.00, 10.00}, {0.50, -0.50, 7.00}};
+    Eigen::Matrix3d const K{{600, 0, 360}, {0, 600, 240}, {0, 0, 1}};
+
+    Eigen::Isometry3d const tf_co_w{Eigen::Isometry3d::Identity()};
+    Eigen::MatrixX2d const pixels{MvgGenerator::Project(points_w, K, tf_co_w)};
+
+    Eigen::MatrixX2d const test_pixels{{360.00, 240.00}, {480.00, 360.00}, {240.00, 120.00},
+                                       {480.00, 180.00}, {240.00, 300.00}, {402.857, 197.144}};
+    ASSERT_TRUE(pixels.isApprox(test_pixels, 1e-3));
+}
