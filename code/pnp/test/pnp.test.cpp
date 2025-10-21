392355e8f8e2dabf53994908a90d98d992101466
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-21 09:20:18 +0200

    remove noise data test

--- code/pnp/test/pnp.test.cpp
+++ code/pnp/test/pnp.test.cpp
@@ -8,6 +8,8 @@
 using namespace reprojection;
 using namespace reprojection::pnp;
 
+// TODO(Jack): Test all functions with noisy data!
+
 TEST(Pnp, TestPnp) {
     testing_mocks::MvgGenerator const generator{testing_mocks::MvgGenerator(false)};
     for (size_t i{0}; i < 20; ++i) {
@@ -21,7 +23,6 @@ TEST(Pnp, TestPnp) {
     }
 }
 
-// TODO(Jack): Add noisy point test for Dlt22 path of Pnp
 TEST(Pnp, TestPnpFlat) {
     Eigen::Matrix3d const K{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};  // Pixels must be in normalized space for Dlt22
     testing_mocks::MvgGenerator const generator{
@@ -36,41 +37,6 @@ TEST(Pnp, TestPnpFlat) {
         Eigen::Isometry3d const pose_i{std::get<Eigen::Isometry3d>(pnp_result)};
         EXPECT_TRUE(pose_i.isApprox(frame_i.pose));
     }
-}
-
-// TODO(Jack): We need to rewrite these tests to check the known theoretical values. For example given certain input
-// noise covariances we can predict the results covariance, or expected residual error etc. This current test simply
-// runs it with noise a few times and check that the mean value of all guesses is roughly equal to the input value. This
-// is better than nothing but is not exactly scientific...
-TEST(Pnp, TestPnpWithNoisyInputData) {
-    // NOTE(Jack): We need a really long focal length here so that the features cover a large part of the image to make
-    // the noise have less of an impact.
-    Eigen::Matrix3d const K{{1800, 0, 360}, {0, 1800, 240}, {0, 0, 1}};  // Pixels must be in normalized space for Dlt22
-    testing_mocks::MvgGenerator const generator{testing_mocks::MvgGenerator(false, K)};
-    testing_mocks::MvgFrame const frame{generator.Generate(0.5)};  // Arbitrary spot in the middle
-
-    int const trials{20};
-    Eigen::Matrix<double, Eigen::Dynamic, 6> pose_estimates(trials, 6);
-    for (size_t i{0}; i < trials; ++i) {
-        // WARN(Jack): Noise is coming from a uniform distribution between -1 and 1, not a normal distribution like you
-        // might want.
-        Eigen::MatrixX2d const noisy_pixels{frame.pixels + Eigen::MatrixXd::Random(frame.pixels.rows(), 2)};
-        // WARN(Jack): This is a really really tiny noise multiplier! Maybe 25 points is too low to be robust to noise
-        // in the points.
-        Eigen::MatrixX3d const noisy_points{
-            frame.points + 0.0002 * Eigen::MatrixXd::Random(frame.points.rows(),
-                                                            3)};  // Smaller noise scale because we are in translation
-                                                                  // coordinates here and not pixel coordinates.
-
-        PnpResult const pnp_result{Pnp(noisy_pixels, noisy_points)};
-        EXPECT_TRUE(std::holds_alternative<Eigen::Isometry3d>(pnp_result));
-
-        Eigen::Isometry3d const pose_i{std::get<Eigen::Isometry3d>(pnp_result)};
-        pose_estimates.row(i) = geometry::Log(pose_i).transpose();
-    }
-
-    Eigen::Vector<double, 6> const mean_pose_estimate{pose_estimates.colwise().mean()};
-    EXPECT_TRUE(mean_pose_estimate.isApprox(geometry::Log(frame.pose), 1e-2));  // Heuristic tolerance
 }
 
 TEST(Pnp, TestMismatchedCorrespondence) {
--- code/testing_mocks/test/mvg_generator.test.cpp
+++ code/testing_mocks/test/mvg_generator.test.cpp
@@ -2,53 +2,15 @@
 
 #include <gtest/gtest.h>
 
-#include <fstream>   // SAVING POINTS REMOVE
-#include <iostream>  // SAVING POINTS REMOVE
-
-namespace reprojection::testing_mocks {
-
-// Function to save a vector of Eigen::Isometry3d transformations to a CSV file
-void saveTransformsToCSV(const std::vector<Eigen::Isometry3d>& transforms, const std::string& filename) {
-    // Open the file to write the matrix
-    std::ofstream file(filename);
-
-    // Check if the file is open
-    if (file.is_open()) {
-        // Iterate over each transform in the vector
-        for (const auto& transform : transforms) {
-            // Write the 4x4 matrix of each transform on a new row
-            for (int i = 0; i < 4; ++i) {
-                for (int j = 0; j < 4; ++j) {
-                    file << transform.matrix()(i, j);
-                    if (not(i * j == 9)) file << ",";  // Add a comma if not the last column
-                }
-            }
-            file << "\n";  // Newline after tf
-        }
-        std::cout << "Transformation matrices saved to " << filename << std::endl;
-    } else {
-        std::cerr << "Unable to open file " << filename << "!" << std::endl;
-    }
-}
-
-}  // namespace reprojection::testing_mocks
-
 using namespace reprojection::testing_mocks;
 
 TEST(TestingMocks, XXX) {
     MvgGenerator const generator{MvgGenerator(false)};
 
-    std::vector<Eigen::Isometry3d> tfs;
-    int const n{1000};
+    int const n{100};
     for (size_t i{0}; i < n; ++i) {
-        MvgFrame const frame_i{generator.Generate(static_cast<double>(i) / n)};
-
-        tfs.push_back(frame_i.pose);
+        EXPECT_NO_FATAL_FAILURE(generator.Generate(static_cast<double>(i) / n));
     }
-
-    saveTransformsToCSV(tfs, "sphere_cameras.txt");
-
-    EXPECT_FALSE(true);
 }
 
 TEST(TestingMocks, TestProject) {
