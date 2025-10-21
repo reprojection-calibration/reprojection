53f9e4324c85a763a3b13993eb799b336c1e731d
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-21 21:17:35 +0200

    Add FitCircle

--- code/calibration/test/focal_length_initialization.test.cpp
+++ code/calibration/test/focal_length_initialization.test.cpp
@@ -2,6 +2,39 @@
 
 #include <gtest/gtest.h>
 
+using namespace reprojection;
 using namespace reprojection::calibration;
 
-TEST(CalibrationFocalLengthInitialization, TestXXX) { EXPECT_TRUE(false); }
+// Modified Least Squares method (MLS) from "A Few Methods for Fitting Circles to Data"
+std::tuple<Eigen::Vector2d, double> FitCircle(Eigen::MatrixX2d const& data) {
+    Eigen::VectorXd const& x(data.col(0));
+    Eigen::VectorXd const& y(data.col(1));
+    Eigen::Index const n{data.rows()};
+
+    // TODO DONT RECALCULATE THINGS OR COPY PASTE
+    double const A{n * x.dot(x) - std::pow(x.sum(), 2)};  // II.10
+    double const B{n * x.dot(y) - x.sum() * y.sum()};     // II.11
+    double const C{n * y.dot(y) - std::pow(y.sum(), 2)};  // II.12
+    // TODO THERE HAS TO BE A MORE ELOQUENT WAY TO RAISE TO THE THIRD POWER
+    double const D{0.5 * (n * x.dot(y.array().square().matrix()) - x.sum() * y.dot(y) +
+                          n * (x.array() * x.array() * x.array()).sum() - x.sum() * x.dot(x))};  // II.13
+    double const E{0.5 * (n * y.dot(x.array().square().matrix()) - y.sum() * x.dot(x) +
+                          n * (y.array() * y.array() * y.array()).sum() - y.sum() * y.dot(y))};  // II.14
+
+    double const cx{(D * C - B * E) / (A * C - B * B)};  // II.8
+    double const cy{(A * E - B * D) / (A * C - B * B)};  // II.9
+
+    Eigen::VectorXd const r{
+        ((x.array() - cx) * (x.array() - cx) + (y.array() - cy) * (y.array() - cy)).sqrt()};  // II.15 (part)
+
+    return {{cx, cy}, r.mean()};
+}
+
+TEST(CalibrationFocalLengthInitialization, TestFitCircle) {
+    // Four points that sit exactly on the circle (x-2)^2 + (y-2)^2 = 1
+    Eigen::MatrixX2d const data{{1, 2}, {3, 2}, {2, 1}, {2, 3}};
+
+    auto const [center, radius]{FitCircle(data)};
+    EXPECT_TRUE(center.isApproxToConstant(2));
+    EXPECT_EQ(radius, 1);
+}
