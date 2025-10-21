a812a6bdbe4c874befafc3a8a9f290226a4ed6b7
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-21 15:21:43 +0200

    refactor pinhole testing

--- code/projection_functions/test/pinhole.test.cpp
+++ code/projection_functions/test/pinhole.test.cpp
@@ -2,32 +2,25 @@
 
 #include <gtest/gtest.h>
 
+#include "eigen_utilities/camera.hpp"
+
+using namespace reprojection;
 using namespace reprojection::projection_functions;
 
+Eigen::Array<double, 4, 1> const pinhole_intrinsics{600, 600, 360, 240};
+Eigen::MatrixX3d const gt_points{{0, 0, 10}, {-360, 0, 600}, {360, 0, 600}, {0, -240, 600}, {0, 240, 600}};
+// NOTE(Jack): I am not 100% sure if the pixels at x=720 should actually be part of the valid pixel group! These are
+// technically one out of bounds based on the discretization of the pixels in the image. Our camera model currently
+// does no bounds checking so this is not detected. If we add "valid pixel checking" these tests at the right and
+// bottom might change as they are invalid pixels by one pixel.
+Eigen::MatrixX2d const gt_pixels{{pinhole_intrinsics[2], pinhole_intrinsics[3]},
+                                 {0, pinhole_intrinsics[3]},
+                                 {720, pinhole_intrinsics[3]},
+                                 {pinhole_intrinsics[2], 0},
+                                 {pinhole_intrinsics[2], 480}};
+
 TEST(ProjectionFunctionsPinhole, TestPinholeProjection) {
-    Eigen::Array<double, 4, 1> const pinhole_intrinsics{600, 600, 360, 240};
+    Eigen::MatrixX2d const pixels(PinholeProjection(eigen_utilities::ToK(pinhole_intrinsics), gt_points));
 
-    Eigen::Vector3d const center_point{0, 0, 10};
-    Eigen::Vector2d const center_pixel{PinholeProjection(pinhole_intrinsics.data(), center_point)};
-    EXPECT_TRUE(center_pixel.isApprox(Eigen::Vector2d{pinhole_intrinsics[2], pinhole_intrinsics[3]}));
-
-    Eigen::Vector3d const left_point{-360, 0, 600};
-    Eigen::Vector2d const left_pixel{PinholeProjection(pinhole_intrinsics.data(), left_point)};
-    EXPECT_TRUE(left_pixel.isApprox(Eigen::Vector2d{0, pinhole_intrinsics[3]}));
-
-    // NOTE(Jack): I am not 100% sure if the pixels at x=720 should actually be part of the valid pixel group! These are
-    // technically one out of bounds based on the discretization of the pixels in the image. Our camera model currently
-    // does no bounds checking so this is not detected. If we add "valid pixel checking" these tests at the right and
-    // bottom might change as they are invalid pixels by one pixel.
-    Eigen::Vector3d const right_point{360, 0, 600};
-    Eigen::Vector2d const right_pixel{PinholeProjection(pinhole_intrinsics.data(), right_point)};
-    EXPECT_TRUE(right_pixel.isApprox(Eigen::Vector2d{720, pinhole_intrinsics[3]}));
-
-    Eigen::Vector3d const top_point{0, -240, 600};
-    Eigen::Vector2d const top_pixel{PinholeProjection(pinhole_intrinsics.data(), top_point)};
-    EXPECT_TRUE(top_pixel.isApprox(Eigen::Vector2d{pinhole_intrinsics[2], 0}));
-
-    Eigen::Vector3d const bottom_point{0, 240, 600};
-    Eigen::Vector2d const bottom_pixel{PinholeProjection(pinhole_intrinsics.data(), bottom_point)};
-    EXPECT_TRUE(bottom_pixel.isApprox(Eigen::Vector2d{pinhole_intrinsics[2], 480}));
+    EXPECT_TRUE(pixels.isApprox(gt_pixels));
 }
