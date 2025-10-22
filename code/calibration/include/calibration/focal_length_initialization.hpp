8d2d9a701c022df7f12c6d7078c3aa11c08b08a9
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-22 07:36:17 +0200

    add circle circle intersection

--- code/calibration/include/calibration/focal_length_initialization.hpp
+++ code/calibration/include/calibration/focal_length_initialization.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Eigen/Dense>
+#include <optional>
 
 namespace reprojection::calibration {
 
--- code/calibration/test/focal_length_initialization.test.cpp
+++ code/calibration/test/focal_length_initialization.test.cpp
@@ -4,6 +4,66 @@
 
 using namespace reprojection::calibration;
 
+// Adopted from https://stackoverflow.com/questions/3349125/circle-circle-intersection-points which copy and pasted from
+// here https://paulbourke.net/geometry/circlesphere/
+// Circles must be in common plane of course!
+// TODO(Jack): Do float math better and more expressive in the last condition - here and everywhere!
+std::optional<std::tuple<Eigen::Vector2d, Eigen::Vector2d>> CircleCircleIntersection(
+    std::tuple<Eigen::Vector2d, double> const& c1, std::tuple<Eigen::Vector2d, double> const& c2) {
+    auto const [P0, r0]{c1};
+    auto const [P1, r1]{c2};
+
+    double const d{(P1 - P0).norm()};
+    if (d > r0 + r1) {
+        return std::nullopt;  // Do not overlap at all
+    } else if (d < std::abs(r0 - r1)) {
+        return std::nullopt;  // One is inside the other
+    } else if (d < 1e-8 and std::abs(r0 - r1) < 1e-8) {
+        return std::nullopt;  // Coincident circles - infinite number of solutions
+    }
+
+    double const a{(r0 * r0 - r1 * r1 + d * d) / (2 * d)};
+    double const h{std::sqrt(r0 * r0 - a * a)};
+    if (h < 1e-8) {
+        return std::nullopt;  // Circles intersect at one point, maybe generally interesting but not for us calibrating
+    }
+
+    Eigen::Vector2d const P2{P0 + a * (P1 - P0) / d};
+
+    Eigen::Vector2d const P3_i{P2(0) + h * (P1(1) - P0(1)) / d, P2(1) - h * (P1(0) - P0(0)) / d};
+    Eigen::Vector2d const P3_j{P2(0) - h * (P1(1) - P0(1)) / d, P2(1) + h * (P1(0) - P0(0)) / d};
+
+    return std::tuple<Eigen::Vector2d, Eigen::Vector2d>{P3_i, P3_j};
+}
+
+TEST(CalibrationFocalLengthInitialization, TestCircleCircleIntersectionSeperate) {
+    std::tuple<Eigen::Vector2d, double> const c1{{1, 1}, 1};
+    std::tuple<Eigen::Vector2d, double> const c2{{3, 3}, 1};  // Completely outside c1
+
+    EXPECT_EQ(CircleCircleIntersection(c1, c2), std::nullopt);
+}
+
+TEST(CalibrationFocalLengthInitialization, TestCircleCircleIntersectionContained) {
+    std::tuple<Eigen::Vector2d, double> const c1{{1, 1}, 1};
+    std::tuple<Eigen::Vector2d, double> const c2{{1.5, 1.5}, 0.1};  // Completely inside c1
+
+    EXPECT_EQ(CircleCircleIntersection(c1, c2), std::nullopt);
+}
+
+TEST(CalibrationFocalLengthInitialization, TestCircleCircleIntersectionCoincident) {
+    std::tuple<Eigen::Vector2d, double> const c1{{1, 1}, 1};
+
+    // Coincident - infinite number of solutions
+    EXPECT_EQ(CircleCircleIntersection(c1, c1), std::nullopt);
+}
+
+TEST(CalibrationFocalLengthInitialization, TestCircleCircleIntersectionOnePoint) {
+    std::tuple<Eigen::Vector2d, double> const c1{{1, 1}, 1};
+    std::tuple<Eigen::Vector2d, double> const c2{{3, 1}, 1};  // Only one intersection point - no calibration interest
+
+    EXPECT_EQ(CircleCircleIntersection(c1, c2), std::nullopt);
+}
+
 TEST(CalibrationFocalLengthInitialization, TestFitCircle) {
     // Four points that sit exactly on the circle (x-2)^2 + (y-2)^2 = 1
     Eigen::MatrixX2d const data{{1, 2}, {3, 2}, {2, 1}, {2, 3}};
