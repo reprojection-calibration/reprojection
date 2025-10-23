ddd02c4cd87d1b4ce976783114bf37921362f3b0
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-23 10:49:29 +0200

    fix code checks

--- code/optimization/include/optimization/nonlinear_refinement.hpp
+++ code/optimization/include/optimization/nonlinear_refinement.hpp
@@ -2,9 +2,7 @@
 
 #include "types/eigen_types.hpp"
 
-namespace  reprojection::optimization {
-
-
+namespace reprojection::optimization {
 
 std::tuple<Isometry3d, Matrix3d> NonlinearRefinement(MatrixX2d const& pixels, MatrixX3d const& points,
                                                      Isometry3d const& initial_pose, Matrix3d const& initial_K);
