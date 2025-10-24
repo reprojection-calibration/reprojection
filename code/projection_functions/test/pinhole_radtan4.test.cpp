d8d0ae4aaa2daf548ef672b57732d898f2d274b6
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-24 10:32:24 +0200

    split into files

--- code/projection_functions/test/pinhole_radtan4.test.cpp
+++ code/projection_functions/test/pinhole_radtan4.test.cpp
@@ -1,6 +1,5 @@
 #include "projection_functions/pinhole_radtan4.hpp"
 
-#include <ceres/ceres.h>
 #include <gtest/gtest.h>
 
 #include "types/eigen_types.hpp"
@@ -39,54 +38,6 @@ TEST(ProjectionFunctionsPinholeRadtan4, TestPinholeEquivalentProjection) {
     }
 }
 
-struct Radtan4DistortionCostFunctor {
-    Radtan4DistortionCostFunctor(Eigen::Array<double, 4, 1> const& distortion, Eigen::Array2d const& pixel)
-        : distortion_{distortion}, pixel_{pixel} {}
-
-    // TODO(Jack): Clarify what is a point and what is a pixel! Unit/plane/normalized???
-    // TODO(Jack): Inconsistent use of array and vector
-    // Is this really a pure pixel of an image plane constrained 3D projected point?
-    template <typename T>
-    bool operator()(T const* const pixel_ptr, T* const residual) const {
-        Eigen::Map<Eigen::Array<T, 2, 1> const> const pixel(pixel_ptr);
-        Eigen::Vector<T, 2> const pixel_star{projection_functions::Radtan4Distortion<T>(distortion_.cast<T>(), pixel)};
-
-        residual[0] = T(pixel_[0]) - pixel_star[0];
-        residual[1] = T(pixel_[1]) - pixel_star[1];
-
-        return true;
-    }
-
-   private:
-    Eigen::Array<double, 4, 1> distortion_;
-    Vector2d pixel_;
-};
-
-// TODO(Jack): This should take a 2d pixel not a 3d point! Figure out some consistency here!
-// TODO(Jack): Must be called with a normalized image plane point/pixel! z=1
-std::tuple<Vector2d, Eigen::Matrix2d> Radtan4DistortionUpdate(Eigen::Array<double, 4, 1> const& radtan4_distortion,
-                                                              Eigen::Array2d const& image_plane_point) {
-    auto* cost_function = new ceres::AutoDiffCostFunction<Radtan4DistortionCostFunctor, 2, 2>(
-        new Radtan4DistortionCostFunctor(radtan4_distortion, image_plane_point));
-
-    const double values[2] = {image_plane_point[0], image_plane_point[1]};
-    const double* pValues = values;
-    const double* const* pixel = &pValues;
-
-    double residuals[2];
-    double jacobians_data[2 * 2];
-    double* jacobians[] = {jacobians_data};
-
-    // TODO(Jack): What would we do if the evaluation here was not successful?
-    bool success{cost_function->Evaluate(pixel, residuals, jacobians)};
-    static_cast<void>(success);
-
-    Eigen::Vector2d const e{residuals[0], residuals[1]};
-    Eigen::Matrix2d const J{Eigen::Map<const Eigen::Matrix2d>(jacobians_data)};
-
-    return {e, J};
-}
-
 TEST(ProjectionFunctionsPinholeRadtan4, TestRadtan4DistortionUpdate) {
     Eigen::Array<double, 4, 1> const radtan4_distortion{-0.1, 0.1, 0.001, 0.001};
     // What kind of guarantee/requirement do we have that the z value of the ray is z=1?
@@ -99,28 +50,6 @@ TEST(ProjectionFunctionsPinholeRadtan4, TestRadtan4DistortionUpdate) {
     EXPECT_FLOAT_EQ(J(0, 1), 0.0023200000000000004);
     EXPECT_FLOAT_EQ(J(1, 0), 0.0023200000000000004);
     EXPECT_FLOAT_EQ(J(1, 1), -0.99531999999999998);
-}
-
-// TODO(Jack): We are manually do an optimization here, therefore I am not sure if we can get jacobians here like a
-// classic templated "pass through" ceres autodiff capable function. Therefore maybe it does not make sense to template
-// here at all.
-template <typename T>
-Eigen::Vector<T, 3> PinholeRadtan4Unprojection(Eigen::Array<T, 8, 1> const& intrinsics,
-                                               Eigen::Array<T, 2, 1> const& pixel) {
-    Eigen::Array<T, 4, 1> const pinhole_intrinsics{intrinsics.topRows(4)};
-    Eigen::Array<T, 3, 1> const ray{PinholeUnrojection(pinhole_intrinsics, pixel)};  // Normalized image plane ray
-
-    // TODO(Jack): How many iterations do we really need here?
-    Eigen::Array<T, 4, 1> const radtan4_distortion{intrinsics.bottomRows(4)};
-    Eigen::Vector2d undistorted_ray{ray.topRows(2)};
-    for (int i{0}; i < 5; ++i) {
-        auto const [e, J]{Radtan4DistortionUpdate(radtan4_distortion, undistorted_ray)};
-
-        Eigen::Vector2d const du{(J.transpose() * J).inverse() * J.transpose() * e};
-        undistorted_ray += du;
-    }
-
-    return {undistorted_ray[0], undistorted_ray[1], 1.0};
 }
 
 TEST(ProjectionFunctionsPinholeRadtan4, TestPinholeRadtan4Unprojection) {
