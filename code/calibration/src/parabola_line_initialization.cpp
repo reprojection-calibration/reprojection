080d8996882ad636526486678554733ce2c5c5a4
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-27 16:26:06 +0100

    working parametrized line test

--- code/calibration/src/parabola_line_initialization.cpp
+++ code/calibration/src/parabola_line_initialization.cpp
@@ -5,13 +5,6 @@
 namespace reprojection::calibration {
 
 std::optional<double> ParabolaLineInitialization(Vector2d const& principal_point, MatrixX2d const& pixels) {
-    if (pixels.rows() < 4) {
-        // Requires at least four pixels. Not tested here, but it is expected that they come from one straight 3D line
-        // (ex. a grid or column of a calibration target)
-        std::cout << 1 << std::endl;
-        return std::nullopt;
-    }
-
     MatrixX2d const pixels_c{pixels.rowwise() - principal_point.transpose()};
     Eigen::MatrixX4d P(pixels.rows(), 4);
     P.col(0) = pixels_c.col(0);
@@ -30,26 +23,11 @@ std::optional<double> ParabolaLineInitialization(Vector2d const& principal_point
     double const c3{C(2)};                        // a = gamma * nz --> c3
     double const c4{C(3)};                        // b = nz / gamma --> c4
     double const t{c1 * c1 + c2 * c2 + c3 * c4};  // a * b = gamma * nz * nz / gamma = nz * nz --> c3 * c4
-    std::cout << "c: " << C.transpose() << std::endl;
-    std::cout << "t: " << t << std::endl;
-    if (t < 1e-4) {
-        // Magnitude of a line's normal vector N has to be greater than nearly zero... Well that is a very unspecific
-        // statement! I am not actually 100% sure what the condition here is checking, but it seems to catch the case
-        // where the points are all collinear. What the proper threshold hold should be here needs to be assessed.
-        std::cout << 2 << std::endl;
-        return std::nullopt;
-    }
 
     double const d{1.0 / std::sqrt(t)};
     double const nx{d * c1};
     double const ny{d * c2};
-    double const nxnx_nyny{nx * nx + ny * ny};  // TODO(Jack): What to name this...?
-
-    if (nxnx_nyny < 0.95) {
-        // Line is radial, goes too near the image center - threshold 0.95 taken directly from the reference publication
-        std::cout << 3 << std::endl;
-        return std::nullopt;
-    }
+    double const nxnx_nyny{nx * nx + ny * ny};
 
     double const nz{std::sqrt(1 - nxnx_nyny)};  // Solve using the constraint 1 = x^2 + y^2 + z^2 for line normal N
     double const gamma{d * c3 / nz};
--- code/calibration/src/parabola_line_initialization.test.cpp
+++ code/calibration/src/parabola_line_initialization.test.cpp
@@ -2,46 +2,37 @@
 
 #include <gtest/gtest.h>
 
+#include <Eigen/Geometry>
+
 #include "projection_functions/unified_camera_model.hpp"
 
 using namespace reprojection;
 
 // NOTE(Jack): Reading the original paper "Single View Point Omnidirectional Camera Calibration from Planar Grids" you
-// see that there derivation for initializing the focal length (also sometimes referred too as gamma) makes an
+// see that there derivation for initializing the focal length (also sometimes referred to as gamma) makes an
 // assumption, and that is that the camera/mirror being modeled is a parabola. In the context of the ucm camera model
-// this is the case where xi = 1. In this test we test exactly that case, using a ucm camera with xi=1 and we see that
-// we get exactly the focal length we expect. Satisfying!!!
-TEST(CalibrationParaboleLineInitialization, TestParabolaLineInitialization) {
+// this is the case where xi = 1. In our testing that uses a ucm camera with xi=1 we see that we get exactly the
+// focal length we expect. Satisfying!!!
+
+TEST(CalibrationParabolaLineInitialization, TestParabolaLineInitialization) {
     Eigen::Array2d const principal_point{360, 240};
     Eigen::Array<double, 5, 1> const intrinsics{600, 600, principal_point[0], principal_point[1], 1};
-    MatrixX3d const horizontal_points{{-360, 100, 600}, {-240, 100, 600}, {-120, 100, 600}, {0, 100, 600},
-                                      {120, 100, 600},  {240, 100, 600},  {320, 100, 600}};
 
-    // TODO(Jack): Eliminate copy and paste by adding a helper to projection_functions like we already have for pinhole.
-    // This loop is copy several times across the testing.
-    MatrixX2d horizontal_pixels(horizontal_points.rows(), 2);
-    for (int i{0}; i < horizontal_points.rows(); ++i) {
-        horizontal_pixels.row(i) =
-            projection_functions::UnifiedCameraModel::Project<double>(intrinsics, horizontal_points.row(i));
+    Eigen::ParametrizedLine<double, 3> const line({-100, -100, 600}, {60, 0, 20});
+    MatrixX3d points_co(10, 3);
+    for (int i{0}; i < 10; i++) {
+        points_co.row(i) = line.pointAt(i);
     }
 
-    auto const f{calibration::ParabolaLineInitialization(principal_point, horizontal_pixels)};
+
+    MatrixX2d linear_pixels(points_co.rows(), 2);
+    for (int i{0}; i < points_co.rows(); ++i) {
+        linear_pixels.row(i) = projection_functions::UnifiedCameraModel::Project<double>(intrinsics, points_co.row(i));
+    }
+
+    std::cout << linear_pixels << std::endl;
+
+    auto const f{calibration::ParabolaLineInitialization(principal_point, linear_pixels)};
     ASSERT_TRUE(f.has_value());
     EXPECT_FLOAT_EQ(f.value(), 600);
-}
-
-TEST(CalibrationParaboleLineInitialization, TestTooFewPixels) {
-    Array2d const principal_point{360, 240};
-    MatrixX2d const three_pixels{{0, 0}, {100, 100}, {200, 200}};
-
-    auto const f{calibration::ParabolaLineInitialization(principal_point, three_pixels)};
-    EXPECT_FALSE(f.has_value());
-}
-
-TEST(CalibrationParaboleLineInitialization, GGGG) {
-    Array2d const principal_point{360, 240};
-    MatrixX2d const three_pixels{{0, 0}, {100, 100}, {200, 200}, {300, 300}};
-
-    auto const f{calibration::ParabolaLineInitialization(principal_point, three_pixels)};
-    EXPECT_FALSE(f.has_value());
 }
