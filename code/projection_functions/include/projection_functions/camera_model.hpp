13ee3b1d3a67299d7b004f376c1ecf682130af65
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-28 10:04:49 +0100

    use base class

--- code/projection_functions/include/projection_functions/camera_model.hpp
+++ code/projection_functions/include/projection_functions/camera_model.hpp
@@ -9,12 +9,21 @@ namespace reprojection::projection_functions {
 // TODO(Jack): Consider, if for a first show we actually want to use these classes for the optimization that we can
 // template the type so ceres jet can use it, instead of hardcoding double for Project and Unproject
 
-template <typename Model, typename Intrinsics>
-class CameraT {
+class Camera {
    public:
-    CameraT(Intrinsics const& intrinsics) : intrinsics_{intrinsics} {}
+    virtual ~Camera() = default;
 
-    MatrixX2d Project(MatrixX3d const& points_co) const {
+    virtual MatrixX2d Project(MatrixX3d const& points_co) const = 0;
+
+    virtual MatrixX3d Unproject(MatrixX2d const& pixels) const = 0;
+};
+
+template <typename Model, typename Intrinsics>
+class CameraT : public Camera  {
+   public:
+    explicit CameraT(Intrinsics const& intrinsics) : intrinsics_{intrinsics} {}
+
+    MatrixX2d Project(MatrixX3d const& points_co) const override {
         Eigen::MatrixX2d pixels(points_co.rows(), 2);
         for (int i{0}; i < points_co.rows(); ++i) {
             pixels.row(i) = Model::template Project<double>(intrinsics_, points_co.row(i));
@@ -23,7 +32,7 @@ class CameraT {
         return pixels;
     }
 
-    MatrixX3d Unproject(MatrixX2d const& pixels) const {
+    MatrixX3d Unproject(MatrixX2d const& pixels) const override {
         Eigen::MatrixX3d rays_co(pixels.rows(), 3);
         for (int i{0}; i < pixels.rows(); ++i) {
             rays_co.row(i) = Model::template Unproject<double>(intrinsics_, pixels.row(i));
