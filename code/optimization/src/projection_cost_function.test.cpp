b517ed647398332ddb3e5e8fd8aa02dfc068dabd
reprojection-calibration <reprojection.calibration@gmail.com>
2025-10-29 10:37:47 +0100

    add test for all branches of create

--- code/optimization/src/projection_cost_function.test.cpp
+++ code/optimization/src/projection_cost_function.test.cpp
@@ -2,15 +2,49 @@
 
 #include <gtest/gtest.h>
 
+#include "projection_functions/double_sphere.hpp"
 #include "projection_functions/pinhole.hpp"
+#include "projection_functions/pinhole_radtan4.hpp"
+#include "projection_functions/unified_camera_model.hpp"
 
 using namespace reprojection;
 
-// TODO(Jack): Test all other nonpinhole cost functions!
-// TODO(Jack): Test the nonlinear refinement with noisy data to make sure the optimization executes more than one step!
+// WARN(Jack): Is it possible that we are leaking memory with un-deallocated cost_function(s)?
+TEST(OptimizationProjectionCostFunction, TestCreate) {
+    Array2d const pixel{360, 240};
+    Array3d const point{0, 0, 600};
+
+    int const num_parameter_blocks{2};  // intrinsics and pose
+    int const pose_size{6};             // se3
+    int const residual_size{2};         // pixel_size
+
+    ceres::CostFunction* cost_function{optimization::Create(optimization::CameraModel::DoubleSphere, pixel, point)};
+    EXPECT_EQ(std::size(cost_function->parameter_block_sizes()), num_parameter_blocks);
+    EXPECT_EQ(cost_function->parameter_block_sizes()[0], projection_functions::DoubleSphere::Size);
+    EXPECT_EQ(cost_function->parameter_block_sizes()[1], pose_size);
+    EXPECT_EQ(cost_function->num_residuals(), residual_size);
+
+    cost_function = optimization::Create(optimization::CameraModel::Pinhole, pixel, point);
+    EXPECT_EQ(std::size(cost_function->parameter_block_sizes()), num_parameter_blocks);
+    EXPECT_EQ(cost_function->parameter_block_sizes()[0], projection_functions::Pinhole::Size);
+    EXPECT_EQ(cost_function->parameter_block_sizes()[1], pose_size);
+    EXPECT_EQ(cost_function->num_residuals(), residual_size);
+
+    cost_function = optimization::Create(optimization::CameraModel::PinholeRadtan4, pixel, point);
+    EXPECT_EQ(std::size(cost_function->parameter_block_sizes()), num_parameter_blocks);
+    EXPECT_EQ(cost_function->parameter_block_sizes()[0], projection_functions::PinholeRadtan4::Size);
+    EXPECT_EQ(cost_function->parameter_block_sizes()[1], pose_size);
+    EXPECT_EQ(cost_function->num_residuals(), residual_size);
+
+    cost_function = optimization::Create(optimization::CameraModel::UnifiedCameraModel, pixel, point);
+    EXPECT_EQ(std::size(cost_function->parameter_block_sizes()), num_parameter_blocks);
+    EXPECT_EQ(cost_function->parameter_block_sizes()[0], projection_functions::UnifiedCameraModel::Size);
+    EXPECT_EQ(cost_function->parameter_block_sizes()[1], pose_size);
+    EXPECT_EQ(cost_function->num_residuals(), residual_size);
+}
 
 // We test that a point on the optical axis (0,0,z) projects to the center of the image (cx, cy) and has residual zero.
-TEST(OptimizationCeresXxx, TestProjectionCostFunction_T) {
+TEST(OptimizationProjectionCostFunction, TestProjectionCostFunction_T) {
     // NOTE(Jack): The reason that we have these calls to .data(), but nowhere else, is because in this test we are
     // essentially manually simulating all the magic that Ceres will do behind the scenes for us, managing the memory
     // and passing arguments etc. during the optimization process. It is my hope and vision that these raw pointers etc.
@@ -33,7 +67,7 @@ TEST(OptimizationCeresXxx, TestProjectionCostFunction_T) {
 // NOTE: We do not test cost_function->Evaluate() in the following test because
 // allocating the memory of the input pointers takes some thought, but cost_function->Evaluate()
 // should be tested when there is interest and time :)
-TEST(OptimizationCeresXxx, TestCreate_T) {
+TEST(OptimizationProjectionCostFunction, TestCreate_T) {
     Array2d const pixel{360, 240};
     Array3d const point{0, 0, 600};
     ceres::CostFunction const* const cost_function{optimization::Create_T<projection_functions::Pinhole>(pixel, point)};
