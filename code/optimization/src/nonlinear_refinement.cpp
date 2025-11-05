71bd3e6f3e9ad5fb3c8258039eb08150f9c10269
reprojection-calibration <reprojection.calibration@gmail.com>
2025-11-05 11:39:24 +0100

    add tose3

--- code/optimization/src/nonlinear_refinement.cpp
+++ code/optimization/src/nonlinear_refinement.cpp
@@ -39,11 +39,7 @@ std::tuple<std::vector<Isometry3d>, ArrayXd, double> NonlinearRefinement(std::ve
     ceres::Solver::Summary summary;
     ceres::Solve(options, &problem, &summary);
 
-    std::vector<Isometry3d> SE3;
-    std::transform(std::cbegin(se3), std::cend(se3), std::back_inserter(SE3),
-                   [](Array6d const& se3_i) { return geometry::Exp(Vector6d{se3_i}); });
-
-    return {SE3, intrinsics_to_optimize, summary.final_cost};
+    return {geometry::ToSE3(se3), intrinsics_to_optimize, summary.final_cost};
 }
 
 }  // namespace  reprojection::optimization
