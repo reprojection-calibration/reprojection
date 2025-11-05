907b4475c1d1186b881fe3b557c632271f07b8f8
reprojection-calibration <reprojection.calibration@gmail.com>
2025-11-05 20:16:20 +0100

    order

--- code/demos/test/image_source.test.cpp
+++ code/demos/test/image_source.test.cpp
@@ -12,14 +12,16 @@ using namespace reprojection;
 TEST(DemosImageSource, VideoCaptureMp4) {
     std::string const folder{"test/video_capture/feed/"};
     std::filesystem::create_directories(folder);
-    cv::Mat const blank_image{cv::Mat::zeros(10, 10, CV_8UC1)};
 
     // Write two video frames into a mp4 video
+    cv::Mat const blank_image{cv::Mat::zeros(10, 10, CV_8UC1)};
+
     cv::VideoWriter writer;
     if (not writer.open(folder + "video.mp4", cv::VideoWriter::fourcc('m', 'p', '4', 'v'), 30.0,
                         cv::Size{blank_image.cols, blank_image.rows}, false)) {
         std::cerr << "Error: Could not open the video writer!" << std::endl;
     }
+
     writer.write(blank_image);
     writer.write(blank_image);
     writer.release();
