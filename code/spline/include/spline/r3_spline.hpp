83cae54a320203b0abb6a1b155a2b9a9f0f4d05f
reprojection-calibration <reprojection.calibration@gmail.com>
2025-11-10 07:57:57 +0100

    clang format

--- code/spline/include/spline/r3_spline.hpp
+++ code/spline/include/spline/r3_spline.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
+#include <cstdint>
 #include <optional>
 #include <vector>
-#include <cstdint>
 
 #include "spline/time_handler.hpp"
 #include "spline/types.hpp"
@@ -10,10 +10,10 @@
 
 namespace reprojection::spline {
 
-// NOTE(Jack): It is a uniform spline which presupposes that all added control_points correspond to specific evenly spaced
-// times.
-// NOTE(Jack): We static variables in some places because the values are needed in one and only one method, therefore it
-// does not make sense to make them part of the class and crowd the class scope.
+// NOTE(Jack): It is a uniform spline which presupposes that all added control_points correspond to specific evenly
+// spaced times.
+// NOTE(Jack): We static variables in some places because the values are constant and  needed in one and only one
+// method, therefore it does not make sense to make them part of the class and crowd the class scope.
 class r3Spline {
    public:
     r3Spline(std::uint64_t const t0_ns, std::uint64_t const delta_t_ns);
