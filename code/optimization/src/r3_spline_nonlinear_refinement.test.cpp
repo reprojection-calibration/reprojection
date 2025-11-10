0dd9626d41bd1c68504cb0bf950ad9f723e40506
reprojection-calibration <reprojection.calibration@gmail.com>
2025-11-10 20:26:29 +0100

    add handler class

--- code/optimization/src/r3_spline_nonlinear_refinement.test.cpp
+++ code/optimization/src/r3_spline_nonlinear_refinement.test.cpp
@@ -4,7 +4,20 @@
 #include "spline/r3_spline.hpp"
 #include "types/eigen_types.hpp"
 
-namespace reprojection::optimization {}
+namespace reprojection::optimization {
+
+class R3SplineProblemHandler {
+   public:
+    // TODO(Jack): Is there a way to programmatically force control points and time handler to match/correspond?
+    R3SplineProblemHandler(std::vector<Vector3d> const& control_points, spline::TimeHandler const& time_handler)
+        : control_points_{control_points}, time_handler_{time_handler} {}
+
+   private:
+    std::vector<Vector3d> control_points_;
+    spline::TimeHandler time_handler_;
+};
+
+}  // namespace reprojection::optimization
 
 using namespace reprojection;
 
@@ -13,6 +26,7 @@ double Squared(double const x) { return x * x; }
 TEST(OptimizationR3SplineNonlinearRefinement, TestXxx) {
     std::uint64_t const delta_t_ns{50};
     spline::r3Spline r3_spline{100, delta_t_ns};
+
     for (auto const& x : std::vector<double>{-1, -0.5, 0.5, 1}) {
         r3_spline.control_points_.push_back(Vector3d{x, Squared(x), Squared(x)});
     }
@@ -37,12 +51,11 @@ TEST(OptimizationR3SplineNonlinearRefinement, TestXxx) {
 
     // add one position constraint
     auto const position_i{r3_spline.Evaluate(100, spline::DerivativeOrder::Null)};
-    ceres::CostFunction* const cost_function_p{optimization::CreateR3SplineCostFunction(
-    spline::DerivativeOrder::Null, position_i.value(), 0, delta_t_ns)};
+    ceres::CostFunction* const cost_function_p{
+        optimization::CreateR3SplineCostFunction(spline::DerivativeOrder::Null, position_i.value(), 0, delta_t_ns)};
     problem.AddResidualBlock(cost_function_p, nullptr, noisy_init[0].data());
 
     for (size_t i{0}; i < delta_t_ns; ++i) {
-
         auto const velocity_i{r3_spline.Evaluate(100 + i, spline::DerivativeOrder::First)};
         auto const acceleration_i{r3_spline.Evaluate(100 + i, spline::DerivativeOrder::Second)};
         if (not velocity_i.has_value() or not acceleration_i.has_value()) {
@@ -52,8 +65,6 @@ TEST(OptimizationR3SplineNonlinearRefinement, TestXxx) {
 
         // TODO AUTO CALCULATE THIS u_i or package the info in a more accessible way
         double const u_i{static_cast<double>(i) / delta_t_ns};
-
-
 
         ceres::CostFunction* const cost_function_v{optimization::CreateR3SplineCostFunction(
             spline::DerivativeOrder::First, velocity_i.value(), u_i, delta_t_ns)};
