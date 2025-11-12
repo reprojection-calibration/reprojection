d2d7f5ee0dd6be149c629c58cf6752205159de8e
reprojection-calibration <reprojection.calibration@gmail.com>
2025-11-12 21:27:46 +0100

    split cost function into files

--- code/optimization/src/so3_spline_cost_function.test.cpp
+++ code/optimization/src/so3_spline_cost_function.test.cpp
@@ -1,64 +1,9 @@
-#include <ceres/ceres.h>
+#include "so3_spline_cost_function.hpp"
+
 #include <gtest/gtest.h>
 
-#include "spline/so3_spline.hpp"
+#include "spline/types.hpp"
 #include "types/eigen_types.hpp"
-
-namespace reprojection::optimization {
-
-// COPY AND PASTED FROM EXACT SAME AS FOR R3 case
-template <spline::DerivativeOrder D>
-class So3SplineCostFunction_T {
-   public:
-    So3SplineCostFunction_T(Vector3d const& r3, double const u_i, std::uint64_t const delta_t_ns)
-        : r3_{r3}, u_i_{u_i}, delta_t_ns_{delta_t_ns} {}
-
-    template <typename T>
-    bool operator()(T const* const control_point_0_ptr, T const* const control_point_1_ptr,
-                    T const* const control_point_2_ptr, T const* const control_point_3_ptr, T* const residual) const {
-        Eigen::Map<const Eigen::Matrix<T, 3, 1>> p0(control_point_0_ptr);
-        Eigen::Map<const Eigen::Matrix<T, 3, 1>> p1(control_point_1_ptr);
-        Eigen::Map<const Eigen::Matrix<T, 3, 1>> p2(control_point_2_ptr);
-        Eigen::Map<const Eigen::Matrix<T, 3, 1>> p3(control_point_3_ptr);
-
-        spline::Matrix3K<T> control_points;
-        control_points << p0, p1, p2, p3;
-
-        Eigen::Vector<T, 3> const r3{spline::So3SplineEvaluation::Evaluate<T, D>(control_points, u_i_, delta_t_ns_)};
-
-        residual[0] = T(r3_[0]) - r3[0];
-        residual[1] = T(r3_[1]) - r3[1];
-        residual[2] = T(r3_[2]) - r3[2];
-
-        return true;
-    }
-
-    static ceres::CostFunction* Create(Vector3d const& r3, double const u_i, std::uint64_t const delta_t_ns) {
-        return new ceres::AutoDiffCostFunction<So3SplineCostFunction_T, 3, 3, 3, 3, 3>(
-            new So3SplineCostFunction_T(r3, u_i, delta_t_ns));
-    }
-
-    Vector3d r3_;
-    double u_i_;
-    std::uint64_t delta_t_ns_;
-};
-
-// Exact same as r3 case!
-ceres::CostFunction* CreateSo3SplineCostFunction(spline::DerivativeOrder const derivative, Vector3d const& r3,
-                                                 double const u_i, std::uint64_t const delta_t_ns) {
-    if (derivative == spline::DerivativeOrder::Null) {
-        return So3SplineCostFunction_T<spline::DerivativeOrder::Null>::Create(r3, u_i, delta_t_ns);
-    } else if (derivative == spline::DerivativeOrder::First) {
-        return So3SplineCostFunction_T<spline::DerivativeOrder::First>::Create(r3, u_i, delta_t_ns);
-    } else if (derivative == spline::DerivativeOrder::Second) {
-        return So3SplineCostFunction_T<spline::DerivativeOrder::Second>::Create(r3, u_i, delta_t_ns);
-    } else {
-        throw std::runtime_error(
-            "Requested unknown derivative order from CreateR3SplineCostFunction()");  // LCOV_EXCL_LINE
-    }
-}
-
-}  // namespace reprojection::optimization
 
 using namespace reprojection;
 
