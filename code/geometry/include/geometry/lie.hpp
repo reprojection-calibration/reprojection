8efc3360cd9aa770076f87430a67596d5dcd9b24
reprojection-calibration <reprojection.calibration@gmail.com>
2025-11-13 08:13:46 +0100

    refactor lie functions to use ceres

--- code/geometry/include/geometry/lie.hpp
+++ code/geometry/include/geometry/lie.hpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 
+#include "ceres/rotation.h"
 #include "types/eigen_types.hpp"
 
 namespace reprojection::geometry {
@@ -10,17 +11,26 @@ Isometry3d Exp(Vector6d const& se3);
 
 Vector6d Log(Isometry3d const& SE3);
 
+// NOTE(Jack): We use ceres helper methods because they are autodiff compliant.
 template <typename T>
 Eigen::Matrix3<T> Exp(Eigen::Vector3<T> const& so3) {
-    Eigen::Matrix3<T> const SO3{Eigen::AngleAxis<T>(so3.norm(), so3.normalized()).toRotationMatrix()};
+    // ERROR(Jack): What error in our rotation convention requires that we multiply this by negative one? The library
+    // was first developer using Eigen axisangle functions here, but when we switched to ceres we had to add this -1 to
+    // preserve the reversability of the exp and log.
+    Eigen::Vector3<T> const negative_so3{-1.0 * so3};
+
+    T R[9];
+    ceres::AngleAxisToRotationMatrix(negative_so3.data(), R);
+
+    Eigen::Map<const Eigen::Matrix<T, 3, 3, Eigen::RowMajor>> SO3(R);
 
     return SO3;
 }
 
 template <typename T>
 Eigen::Vector3<T> Log(Eigen::Matrix3<T> const& SO3) {
-    Eigen::AngleAxis<T> const rotation(SO3);
-    Eigen::Vector3<T> const so3{rotation.angle() * rotation.axis()};
+    Eigen::Vector3<T> so3;
+    ceres::RotationMatrixToAngleAxis(SO3.data(), so3.data());
 
     return so3;
 }
