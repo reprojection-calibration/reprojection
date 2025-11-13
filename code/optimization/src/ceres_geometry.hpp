6d443bb10a61fa8de881af6470085632ee5453d2
reprojection-calibration <reprojection.calibration@gmail.com>
2025-11-13 21:50:26 +0100

    use Vector3<T>

--- code/optimization/src/ceres_geometry.hpp
+++ code/optimization/src/ceres_geometry.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
-#include <ceres/ceres.h>
 #include <ceres/rotation.h>
 
 #include <Eigen/Core>
+
+#include "types/eigen_types.hpp"
 
 namespace reprojection::optimization {
 
@@ -13,13 +14,13 @@ namespace reprojection::optimization {
 // types (in the testing for example).
 // TODO(Jack): Does the point here really need to be templated? Or as a constant can we avoid that?
 template <typename T>
-Eigen::Vector<T, 3> TransformPoint(Eigen::Ref<Eigen::Vector<T, 6> const> const& tf_i_j,
-                                   Eigen::Ref<Eigen::Vector<T, 3> const> const& point_j) {
-    Eigen::Vector<T, 3> const axis_angle{tf_i_j.topRows(3)};
-    Eigen::Vector<T, 3> point_i;
+Vector3<T> TransformPoint(Eigen::Ref<Eigen::Vector<T, 6> const> const& tf_i_j,
+                          Eigen::Ref<Vector3<T> const> const& point_j) {
+    Vector3<T> const axis_angle{tf_i_j.topRows(3)};
+    Vector3<T> point_i;
     ceres::AngleAxisRotatePoint(axis_angle.data(), point_j.data(), point_i.data());
 
-    Eigen::Vector<T, 3> const translation{tf_i_j.bottomRows(3)};
+    Vector3<T> const translation{tf_i_j.bottomRows(3)};
     point_i += translation;
 
     return point_i;
