8860de61a0e6037649288095d0ae6b9576e68a10
reprojection-calibration <reprojection.calibration@gmail.com>
2025-11-21 17:20:38 +0100

    reorder testing

--- code/spline/test/spline_initialization.test.cpp
+++ code/spline/test/spline_initialization.test.cpp
@@ -10,66 +10,6 @@
 using namespace reprojection;
 using namespace reprojection::spline;
 
-TEST(SplineSplineInitialization, TestBuildOmega) {
-    MatrixXd const Q_1{BuildOmega(1, 1)};
-    EXPECT_EQ(Q_1.rows(), 12);
-    EXPECT_EQ(Q_1.cols(), 12);
-    EXPECT_FLOAT_EQ(Q_1.diagonal().sum(), 8);  // Heuristic!
-
-    MatrixXd const Q_100{BuildOmega(100, 1)};
-    EXPECT_FLOAT_EQ(Q_100.diagonal().sum(), 8e-6);
-}
-
-TEST(SplineSplineInitialization, TestVectorizeBlendingMatrix) {
-    MatrixKd const blending_matrix{R3Spline::M_};
-    MatrixXd const vectorized{VectorizeBlendingMatrix(blending_matrix)};  // TODO(Jack): Better name than vectorized!
-
-    EXPECT_EQ(vectorized.rows(), 12);
-    EXPECT_EQ(vectorized.cols(), 12);
-    EXPECT_TRUE(vectorized.topLeftCorner(4, 1).isApprox(blending_matrix.row(0).transpose()));
-    EXPECT_TRUE(vectorized.bottomRightCorner(4, 1).isApprox(blending_matrix.row(3).transpose()));
-}
-
-TEST(SplineSplineInitialization, TestHankelMatrix) {
-    Vector3d const coefficients{1, 2, 3};
-    MatrixXd const hankel_matrix{HankelMatrix(coefficients)};
-    EXPECT_EQ(hankel_matrix.rows(), 2);
-    EXPECT_EQ(hankel_matrix.cols(), 2);
-    EXPECT_EQ(hankel_matrix(0, 1), 2);
-
-    // The type of hankel matrix actually used by the b-spline initialization - a hilbert matrix
-    MatrixXd const hilbert_matrix{HilbertMatrix(7)};
-    EXPECT_EQ(hilbert_matrix.rows(), 4);
-    EXPECT_EQ(hilbert_matrix.cols(), 4);
-    EXPECT_EQ(hilbert_matrix(0, 3), 0.25);
-}
-
-TEST(SplineSplineInitialization, TestDerivativeOperator) {
-    Matrix3d const D3{DerivativeOperator(3)};
-    EXPECT_TRUE(D3.diagonal(1).isApprox(Vector2d{1, 2}));
-
-    // This should be the size used for the cubic b-spline
-    MatrixKd const D4{DerivativeOperator(constants::order)};
-    EXPECT_TRUE(D4.diagonal(1).isApprox(Vector3d{1, 2, 3}));
-}
-
-TEST(SplineSplineInitialization, TestBuildAb) {
-    std::vector<C3Measurement> const measurements{{5000, {0, 0, 0}, DerivativeOrder::Null},  //
-                                                  {5100, {1, 1, 1}, DerivativeOrder::Null},
-                                                  {5200, {2, 2, 2}, DerivativeOrder::Null}};
-
-    int const num_segments{2};
-    TimeHandler const time_handler{5000, 100, constants::order};
-
-    auto const [A, b]{BuildAb(measurements, num_segments, time_handler)};
-    EXPECT_EQ(A.rows(), 9);
-    EXPECT_EQ(A.cols(), 15);
-    EXPECT_EQ(b.rows(), 9);
-
-    // TODO(Jack): At this point the actual time handling logic inside the function is not at all/or well tested.
-    // Can we test that from this view? Or is that already tested somehwere else?
-}
-
 TEST(SplineSplineInitialization, TestInitializeSpline) {
     std::vector<C3Measurement> const measurements{{5000, {0, 0, 0}, DerivativeOrder::Null},  //
                                                   {5100, {1, 1, 1}, DerivativeOrder::Null},
@@ -94,6 +34,23 @@ TEST(SplineSplineInitialization, TestInitializeSpline) {
     EXPECT_TRUE(two_segment_spline.control_points[4].isApprox(Vector3d{3.02269, 3.02269, 3.02269}, 1e-6));
 }
 
+TEST(SplineSplineInitialization, TestBuildAb) {
+    std::vector<C3Measurement> const measurements{{5000, {0, 0, 0}, DerivativeOrder::Null},  //
+                                                  {5100, {1, 1, 1}, DerivativeOrder::Null},
+                                                  {5200, {2, 2, 2}, DerivativeOrder::Null}};
+
+    int const num_segments{2};
+    TimeHandler const time_handler{5000, 100, constants::order};
+
+    auto const [A, b]{BuildAb(measurements, num_segments, time_handler)};
+    EXPECT_EQ(A.rows(), 9);
+    EXPECT_EQ(A.cols(), 15);
+    EXPECT_EQ(b.rows(), 9);
+
+    // TODO(Jack): At this point the actual time handling logic inside the function is not at all/or well tested.
+    // Can we test that from this view? Or is that already tested somehwere else?
+}
+
 TEST(SplineSplineInitialization, TestVectorizeWeights) {
     // At u=0 only the first three blocks will have weight - print out the weights to understand the values!
     ControlPointBlock const w_0{VectorizeWeights(0.0)};
@@ -104,4 +61,47 @@ TEST(SplineSplineInitialization, TestVectorizeWeights) {
     ControlPointBlock const w_1{VectorizeWeights(0.99999999999)};
     EXPECT_TRUE(w_1.block(0, 0, 3, 3).isZero());                  // First block is empty
     EXPECT_FLOAT_EQ(w_1.block(0, 6, 3, 3).sum(), 3 * (2.0 / 3));  // Third block has all the 2/3 weights
+}
+
+TEST(SplineSplineInitialization, TestBuildOmega) {
+    MatrixXd const Q_1{BuildOmega(1, 1)};
+    EXPECT_EQ(Q_1.rows(), 12);
+    EXPECT_EQ(Q_1.cols(), 12);
+    EXPECT_FLOAT_EQ(Q_1.diagonal().sum(), 8);  // Heuristic!
+
+    MatrixXd const Q_100{BuildOmega(100, 1)};
+    EXPECT_FLOAT_EQ(Q_100.diagonal().sum(), 8e-6);
+}
+
+TEST(SplineSplineInitialization, TestDerivativeOperator) {
+    Matrix3d const D3{DerivativeOperator(3)};
+    EXPECT_TRUE(D3.diagonal(1).isApprox(Vector2d{1, 2}));
+
+    // This should be the size used for the cubic b-spline
+    MatrixKd const D4{DerivativeOperator(constants::order)};
+    EXPECT_TRUE(D4.diagonal(1).isApprox(Vector3d{1, 2, 3}));
+}
+
+TEST(SplineSplineInitialization, TestHankelMatrix) {
+    Vector3d const coefficients{1, 2, 3};
+    MatrixXd const hankel_matrix{HankelMatrix(coefficients)};
+    EXPECT_EQ(hankel_matrix.rows(), 2);
+    EXPECT_EQ(hankel_matrix.cols(), 2);
+    EXPECT_EQ(hankel_matrix(0, 1), 2);
+
+    // The type of hankel matrix actually used by the b-spline initialization - a hilbert matrix
+    MatrixXd const hilbert_matrix{HilbertMatrix(7)};
+    EXPECT_EQ(hilbert_matrix.rows(), 4);
+    EXPECT_EQ(hilbert_matrix.cols(), 4);
+    EXPECT_EQ(hilbert_matrix(0, 3), 0.25);
+}
+
+TEST(SplineSplineInitialization, TestVectorizeBlendingMatrix) {
+    MatrixKd const blending_matrix{R3Spline::M_};
+    MatrixXd const vectorized{VectorizeBlendingMatrix(blending_matrix)};  // TODO(Jack): Better name than vectorized!
+
+    EXPECT_EQ(vectorized.rows(), 12);
+    EXPECT_EQ(vectorized.cols(), 12);
+    EXPECT_TRUE(vectorized.topLeftCorner(4, 1).isApprox(blending_matrix.row(0).transpose()));
+    EXPECT_TRUE(vectorized.bottomRightCorner(4, 1).isApprox(blending_matrix.row(3).transpose()));
 }
