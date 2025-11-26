df8b4aa4f78f27633ef478b101289db8671d1064
reprojection-calibration <reprojection.calibration@gmail.com>
2025-11-26 22:18:28 +0100

    add more checks to ParabolaLineInitialization

--- code/calibration/src/parabola_line_initialization.cpp
+++ code/calibration/src/parabola_line_initialization.cpp
@@ -11,6 +11,10 @@ namespace reprojection::calibration {
 // this becomes a problem in the initialization! My experience so far shows that all error conditions are actually
 // caught with the nxnx_nyny > 0.95 "non-radial" line check.
 std::optional<double> ParabolaLineInitialization(Vector2d const& principal_point, MatrixX2d const& pixels) {
+    if (pixels.rows() <= 4) {
+        return std::nullopt;
+    }
+
     MatrixX2d const pixels_c{pixels.rowwise() - principal_point.transpose()};
     Eigen::MatrixX4d P(pixels.rows(), 4);
     P.col(0) = pixels_c.col(0);
@@ -20,7 +24,6 @@ std::optional<double> ParabolaLineInitialization(Vector2d const& principal_point
 
     Eigen::JacobiSVD<Eigen::MatrixXd> svd;
     svd.compute(P, Eigen::ComputeThinU | Eigen::ComputeThinV);
-
     Eigen::Vector4d const C{svd.matrixV().col(3)};
 
     // We are not normalized so we do not yet refer to that variables (ex. c1 or c2) as part of the line normal N
@@ -30,8 +33,12 @@ std::optional<double> ParabolaLineInitialization(Vector2d const& principal_point
     double const& c4{C(3)};                       // b = nz / gamma --> c4
     double const t{c1 * c1 + c2 * c2 + c3 * c4};  // a * b = gamma * nz * nz / gamma = nz * nz --> c3 * c4
 
-    // WARN(Jack): In the reference paper there is a check here to make sure that t is positive! See note above. If it
-    // turns out we do get negative t here then the square root might cause problems...
+    // NOTE(Jack): I do not know how to manufacture test data to test this condition, but it is observed in the real
+    // world, therefore we need to suppress this from the code coverage, because it is not covered in unit testing!
+    if (t <= 0) {
+        return std::nullopt;
+    }
+
     double const d{1.0 / std::sqrt(t)};
     double const nx{d * c1};
     double const ny{d * c2};
@@ -40,7 +47,7 @@ std::optional<double> ParabolaLineInitialization(Vector2d const& principal_point
     // NOTE(Jack): This seems to be a pretty aggressive threshold. In some cases a line within 100 pixels of the origin
     // can be classified as "radial", that feels like too much. Testing with real data over time will tell if this
     // matters or not.
-    if (nxnx_nyny > 0.95) {
+    if (std::sqrt(nxnx_nyny) > 0.95) {
         return std::nullopt;
     }
 
