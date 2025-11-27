860d83a7e08b630db6ee3c9bb89b80dd6a6a79d8
reprojection-calibration <reprojection.calibration@gmail.com>
2025-11-27 22:25:42 +0100

    normalie dlt22

--- code/pnp/src/dlt.cpp
+++ code/pnp/src/dlt.cpp
@@ -1,4 +1,4 @@
-#include "dlt.hpp"
+#include "../include/pnp/dlt.hpp"
 
 #include "dlt_matrix_decompositions.hpp"
 #include "dlt_solving.hpp"
@@ -19,7 +19,7 @@ std::tuple<Isometry3d, Array4d> Dlt23(Bundle const& bundle) {
     Eigen::Matrix<double, 3, 4> const P_star{tf_pixels.inverse() * P * tf_points};  //  Denormalize
 
     // Extract camera parameters
-    auto [K, R]{DecomposeMIntoKr(P_star.leftCols(3))};
+    auto const [K, R]{DecomposeMIntoKr(P_star.leftCols(3))};
     Vector3d const t{CalculateCameraCenter(P_star)};
 
     return {ToIsometry3d(R, -R * t), eigen_utilities::FromK(K)};
@@ -30,14 +30,16 @@ Isometry3d Dlt22(Bundle const& bundle) {
     // ERROR(Jack): Always assumes we aligned with the Z dimension as the plane! CUTS OFF THE Z DIMENSION NO MATTER
     // WHAT!!!
     MatrixX2d const chopped_points{bundle.points(Eigen::all, {0, 1})};
+    auto const [normalized_points, tf_points]{NormalizeColumnWise(chopped_points)};
 
     // WARN(Jack): If we had exactly four correspondences, this means that A would be 8x9. For the svd that follows is
     // that ok? Do we need to zero pad anything or simply check that we have at least five points? Or is it no problem
     // at all?
-    auto const A{ConstructA<3>(bundle.pixels, chopped_points)};
-    auto H{SolveForH<3>(A)};
+    auto const A{ConstructA<3>(bundle.pixels, normalized_points)};
+    Matrix3d const H{SolveForH<3>(A)};
+    Matrix3d const H_star{H * tf_points};  //  Denormalize
 
-    auto const [R, t]{DecomposeHIntoRt(H)};
+    auto const [R, t]{DecomposeHIntoRt(H_star)};
 
     return ToIsometry3d(R, t);
 }
