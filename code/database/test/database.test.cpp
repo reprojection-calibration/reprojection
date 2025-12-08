8a033f2e9aa198f1626f38344dc0b0cb7d3a41b8
reprojection-calibration <reprojection.calibration@gmail.com>
2025-12-08 15:26:02 +0100

    add TestAddImuData

--- code/database/test/database.test.cpp
+++ code/database/test/database.test.cpp
@@ -18,6 +18,22 @@ struct ImuData {
     double linear_acceleration[3];
 };
 
+struct Sqlite3Tools {
+    [[nodiscard]] static bool Execute(std::string const& sql_statement, sqlite3* const db) {
+        char* errror_msg{0};
+        int const code{sqlite3_exec(db, sql_statement.c_str(), nullptr, nullptr, &errror_msg)};
+
+        if (code != static_cast<int>(SqliteFlag::Ok)) {
+            std::cerr << "SQL error: " << errror_msg << std::endl;
+            sqlite3_free(errror_msg);  // WARN(Jack): Violating RAII here! Should wrap errror_msg with class.
+
+            return false;
+        }
+
+        return true;
+    }
+};
+
 // NOTE(Jack): Technically which database we use DOES NOT matter to the calibration process (it is an implementation
 // detail only)! However instead of starting with a pure virtual interface base class to define the interface, we will
 // start with a concrete implementation and if we need generalization later we will refactor.
@@ -51,28 +67,38 @@ class CalibrationDatabase {
 
     ~CalibrationDatabase() { sqlite3_close(db_); }
 
-    // TODO(Jack): Should be private?
-    [[nodiscard]] bool Sqlite3Execute(std::string const& sql_statement) {
-        char* errror_msg{0};
-        int const code{sqlite3_exec(db_, sql_statement.c_str(), nullptr, nullptr, &errror_msg)};
-
-        if (code != static_cast<int>(SqliteFlag::Ok)) {
-            std::cerr << "SQL error: " << errror_msg << std::endl;
-            sqlite3_free(errror_msg);  // WARN(Jack): Violating RAII here! Should wrap errror_msg with class.
-
+    [[nodiscard]] bool AddImuData(uint64_t const timestamp_ns, std::string const& sensor_name, ImuData const& data) {
+        // TODO(Jack): This is hacky! We need to find a principle way to create all data tables, not recreate it
+        // everytime we want to add data!
+        // TODO(Jack): Is there any problem we will have storing our time type (uint64t) here as a signed integer type?
+        // TODO(Jack): Should index the sensor by an integer id or by a string?
+        std::string const init_imu_table{
+            "CREATE TABLE IF NOT EXISTS imu_data ("
+            "timestamp_ns INTEGER NOT NULL, "
+            "sensor_name TEXT NOT NULL, "
+            "omega_x REAL NOT NULL, "
+            "omega_y REAL NOT NULL, "
+            "omega_z REAL NOT NULL, "
+            "ax REAL NOT NULL, "
+            "ay REAL NOT NULL, "
+            "az REAL NOT NULL, "
+            "PRIMARY KEY (timestamp_ns, sensor_name)"
+            ");"};
+        bool success{Sqlite3Tools::Execute(init_imu_table, db_)};
+        if (not success) {
             return false;
         }
 
-        return true;
-    }
-
-    // uint64_t const timestamp_ns, std::string const& sensor_name, ImuData const& data
-    [[nodiscard]] bool AddImuData() {
-        std::string const sql_statement{
+        (void)data;
+        std::string const add_imu_row{
             "INSERT INTO imu_data (timestamp_ns, sensor_name, omega_x, omega_y, omega_z, ax, ay, az) "
-            "VALUES(0, 'abcdc', 0, 1, 2, 3, 4, 5);"};
+            "VALUES(" +
+            std::to_string(timestamp_ns) + ", '" + sensor_name + "', " + std::to_string(data.angular_velocity[0]) +
+            ", " + std::to_string(data.angular_velocity[1]) + ", " + std::to_string(data.angular_velocity[2]) + ", " +
+            std::to_string(data.linear_acceleration[0]) + ", " + std::to_string(data.linear_acceleration[1]) + ", " +
+            std::to_string(data.linear_acceleration[2]) + ");"};
 
-        return Sqlite3Execute(sql_statement);
+        return Sqlite3Tools::Execute(add_imu_row, db_);
     }
 
    private:
@@ -82,31 +108,6 @@ class CalibrationDatabase {
 }  // namespace reprojection::database
 
 using namespace reprojection;
-
-TEST(XXX, TestSqlite3Execute) {
-    std::string const record{"record_hhh.db3"};
-    database::CalibrationDatabase db{record, true};
-
-    // TODO(Jack): Is there any problem we will have storing our time type (uint64t) here as a signed integer type?
-    // TODO(Jack): Should index the sensor by an integer id or by a string?
-    std::string const sql_statement{
-        "CREATE TABLE IF NOT EXISTS imu_data ("
-        "timestamp_ns INTEGER NOT NULL, "
-        "sensor_name TEXT NOT NULL, "
-        "omega_x REAL NOT NULL, "
-        "omega_y REAL NOT NULL, "
-        "omega_z REAL NOT NULL, "
-        "ax REAL NOT NULL, "
-        "ay REAL NOT NULL, "
-        "az REAL NOT NULL, "
-        "PRIMARY KEY (timestamp_ns, sensor_name)"
-        ");"};
-    bool success{db.Sqlite3Execute(sql_statement)};
-    EXPECT_TRUE(success);
-
-    success = db.AddImuData();
-    EXPECT_TRUE(success);
-}
 
 // Test fixture used to facilitate isolated filesystem state. This is useful when testing database creation to prevent
 // artefacts from previous or parallel testing interfering with the system currently under test.
@@ -118,6 +119,25 @@ class TempFolder : public ::testing::Test {
 
     std::string database_path_{"sandbox"};
 };
+
+TEST_F(TempFolder, TestAddImuData) {
+    std::string const record{database_path_ + "/record_hhh.db3"};
+    database::CalibrationDatabase db{record, true};
+
+    bool success{db.AddImuData(0, "/imu/polaris/123", {})};
+    EXPECT_TRUE(success);
+    success = db.AddImuData(1, "/imu/polaris/123", {});
+    EXPECT_TRUE(success);
+
+    // Add second sensors data with same timestamp as a preexisting record - works because we use a compound primary key
+    // (timestamp_ns, sensor_name)
+    success = db.AddImuData(0, "/imu/polaris/456", {});
+    EXPECT_TRUE(success);
+
+    // Add a repeated record - this fails because the primary key must always be unique!
+    success = db.AddImuData(0, "/imu/polaris/456", {});
+    EXPECT_FALSE(success);
+}
 
 TEST_F(TempFolder, TestCreate) {
     std::string const record{database_path_ + "/record_xxx.db3"};
