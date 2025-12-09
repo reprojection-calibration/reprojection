a7649ab60cde711fe98f18ddb330ee1e05716460
reprojection-calibration <reprojection.calibration@gmail.com>
2025-12-09 08:32:53 +0100

    add TestInsertImuDataSql

--- code/database/src/database.cpp
+++ code/database/src/database.cpp
@@ -6,6 +6,7 @@
 #include <filesystem>
 #include <string>
 
+#include "sql.hpp"
 #include "sqlite3_helpers.hpp"
 
 namespace reprojection::database {
@@ -39,44 +40,22 @@ CalibrationDatabase::CalibrationDatabase(std::string const& db_path, bool const 
 CalibrationDatabase::~CalibrationDatabase() { sqlite3_close(db_); }
 
 [[nodiscard]] bool CalibrationDatabase::AddImuData(std::string const& sensor_name, ImuData const& data) {
-    // TODO(Jack): This is hacky! We need to find a principle way to create all data tables, not recreate it
+    // TODO(Jack): This is hacky! We need to find a principle way to create all data tables, not attempt to recreate it
     // everytime we want to add data!
-    // TODO(Jack): Is there any problem we will have storing our time type (uint64t) here as a signed integer type?
-    // TODO(Jack): Should index the sensor by an integer id or by a string?
-    std::string const init_imu_table{
-        "CREATE TABLE IF NOT EXISTS imu_data ("
-        "timestamp_ns INTEGER NOT NULL, "
-        "sensor_name TEXT NOT NULL, "
-        "omega_x REAL NOT NULL, "
-        "omega_y REAL NOT NULL, "
-        "omega_z REAL NOT NULL, "
-        "ax REAL NOT NULL, "
-        "ay REAL NOT NULL, "
-        "az REAL NOT NULL, "
-        "PRIMARY KEY (timestamp_ns, sensor_name)"
-        ");"};
-    bool success{Sqlite3Tools::Execute(init_imu_table, db_)};
+    bool success{Sqlite3Tools::Execute(imu_table_sql, db_)};
     if (not success) {
         return false;
     }
 
-    std::string const add_imu_row{
-        "INSERT INTO imu_data (timestamp_ns, sensor_name, omega_x, omega_y, omega_z, ax, ay, az) "
-        "VALUES (" +
-        std::to_string(data.timestamp_ns) + ", '" + sensor_name + "', " + std::to_string(data.angular_velocity[0]) +
-        ", " + std::to_string(data.angular_velocity[1]) + ", " + std::to_string(data.angular_velocity[2]) + ", " +
-        std::to_string(data.linear_acceleration[0]) + ", " + std::to_string(data.linear_acceleration[1]) + ", " +
-        std::to_string(data.linear_acceleration[2]) + ");"};
+    std::string const insert_imu_data_sql{
+        InsertImuDataSql(data.timestamp_ns, sensor_name, data.angular_velocity, data.linear_acceleration)};
 
-    return Sqlite3Tools::Execute(add_imu_row, db_);
+    return Sqlite3Tools::Execute(insert_imu_data_sql, db_);
 }
 
 // TODO(Jack): Return a variant here to indicate success or failure
 std::set<ImuData> CalibrationDatabase::GetImuData(std::string const& sensor_name) {
-    std::string const query_imu_sensor_data{
-        "SELECT timestamp_ns, omega_x, omega_y, omega_z, ax, ay, az "
-        "FROM imu_data WHERE sensor_name = '" +
-        sensor_name + "' ORDER BY timestamp_ns ASC;"};
+    std::string const select_imu_sensor_data_sql{SelectImuSensorDataSql(sensor_name)};
 
     auto callback = [](void* data, int, char** argv, char**) -> int {
         auto* set = reinterpret_cast<std::set<ImuData>*>(data);
@@ -89,7 +68,7 @@ std::set<ImuData> CalibrationDatabase::GetImuData(std::string const& sensor_name
     };
 
     std::set<ImuData> data;
-    (void)Sqlite3Tools::Execute(query_imu_sensor_data, db_, callback, &data);  // REMOVE CAST, RETURN VARIANT
+    (void)Sqlite3Tools::Execute(select_imu_sensor_data_sql, db_, callback, &data);  // REMOVE VOID CAST, RETURN VARIANT
 
     return data;
 }
