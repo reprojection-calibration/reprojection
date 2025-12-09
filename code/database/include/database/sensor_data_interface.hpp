c65c1a6011cd64c3c466720852897ed03e0d7685
reprojection-calibration <reprojection.calibration@gmail.com>
2025-12-09 18:10:21 +0100

    comment

--- code/database/include/database/sensor_data_interface.hpp
+++ code/database/include/database/sensor_data_interface.hpp
@@ -42,6 +42,8 @@ std::optional<std::set<ImuData>> GetImuData(std::shared_ptr<CalibrationDatabase 
 // we will run into problems here with memory. Therefore, we create this streamer class which loads the images one by
 // one from the database. We include the start_time parameter so that applications can skip loading images prior to that
 // timestamp (i.e. if the images before start_time were already processed).
+// WARN(Jack): We need to codify how we deal with const with the database. At this point I think there is no clear
+// strategy or we might think we have protection where we do not really.
 class ImageStreamer {
    public:
     ImageStreamer(std::shared_ptr<CalibrationDatabase const> const database, std::string const& sensor_name,
