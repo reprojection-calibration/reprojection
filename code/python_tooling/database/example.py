e7b068b9d611af194de65766076b9ac3c148981f
reprojection-calibration <reprojection.calibration@gmail.com>
2025-12-17 13:15:47 +0100

    working local db load

--- code/python_tooling/database/example.py
+++ code/python_tooling/database/example.py
@@ -1,2 +1,37 @@
+from database.extracted_target_pb2 import ExtractedTargetProto
+import sqlite3
+import numpy as np
+
+
 def add_one(number):
     return number + 1
+
+
+def load_extracted_targets(db_path):
+    conn = sqlite3.connect(db_path)
+    cur = conn.cursor()
+    # ERROR(Jack): Dangerous copy paste, see TODO below.
+    # TODO(Jack): Load this sql defintiion from the sql folder!
+    cur.execute(
+        "SELECT timestamp_ns, sensor_name, data "
+        "FROM extracted_targets ORDER BY timestamp_ns ASC"
+    )
+
+    targets = {}
+    for ts, sensor, blob in cur.fetchall():
+        msg = ExtractedTargetProto()
+        msg.ParseFromString(blob)
+
+        rows = msg.bundle.point_rows
+        data = np.array(msg.bundle.point_data, dtype=np.float64)
+
+        if rows == 0 or data.size == 0:
+            points = np.empty((0, 3))
+        else:
+            points = data.reshape(3, rows).transpose()  # Eigen::MatrixX3d
+
+        targets.setdefault(sensor, {})[ts] = points
+
+    conn.close()
+
+    return targets
--- code/python_tooling/tests/test_example.py
+++ code/python_tooling/tests/test_example.py
@@ -1,6 +1,7 @@
 import unittest
 
 from database.example import add_one
+from database.example import load_extracted_targets
 
 
 class TestStringMethods(unittest.TestCase):
@@ -18,6 +19,14 @@ class TestStringMethods(unittest.TestCase):
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
             s.split(2)
+
+
+class TestDatabaseConnections(unittest.TestCase):
+    def test_extracted_target_loading(self):
+        # WARN(Jack): Is there a better way to define this global value here? We might want to test inside of the
+        # container and sometimes outside.
+        db_path = "/home/stable-genius-gram/github/reprojection-calibration/reprojection/code/test_data/dataset-calib-imu4_512_16.db3"
+        load_extracted_targets(db_path)
 
 
 if __name__ == '__main__':
