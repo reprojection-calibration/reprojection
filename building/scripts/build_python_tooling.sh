b0bd70d3cb1b5bc835a10e9d86572ab4a7a937c6
reprojection-calibration <reprojection.calibration@gmail.com>
2025-12-17 10:59:23 +0100

    comments

--- building/scripts/build_python_tooling.sh
+++ building/scripts/build_python_tooling.sh
@@ -4,9 +4,11 @@ set -eoux pipefail
 
 build_directory=/buildroot/build/python
 
+# TODO(Jack): Can we do this all "out of source"? Or do we need to change the directory?
 cd /temporary/code/python_tooling
 
-python3 setup.py build --build-base "${build_directory}"
-
+# TODO(Jack): Is this the real best way to do this? Do the tests run the tests that we actually want to run in the build
+# folder? Or are they run from the soruce code?
+python3 setup.py build --build-base "${build_directory}" --verbose
 python3 -m unittest --verbose
 
