4de30e2ce9676d8b3a84d70cafa964597e81eb2c
reprojection-calibration <reprojection.calibration@gmail.com>
2025-12-17 13:19:45 +0100

    test test_extracted_target_loading

--- code/python_tooling/tests/test_example.py
+++ code/python_tooling/tests/test_example.py
@@ -26,7 +26,13 @@ class TestDatabaseConnections(unittest.TestCase):
         # WARN(Jack): Is there a better way to define this global value here? We might want to test inside of the
         # container and sometimes outside.
         db_path = "/home/stable-genius-gram/github/reprojection-calibration/reprojection/code/test_data/dataset-calib-imu4_512_16.db3"
-        load_extracted_targets(db_path)
+        loaded_data = load_extracted_targets(db_path)
+
+        # Two cameras
+        self.assertEqual(len(loaded_data), 2)
+        # Each with 879 extracted targets
+        self.assertEqual(len(loaded_data["/cam0/image_raw"]), 879)
+        self.assertEqual(len(loaded_data["/cam1/image_raw"]), 879)
 
 
 if __name__ == '__main__':
