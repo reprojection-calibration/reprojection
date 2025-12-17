3d6b4f5156a778c96fad4ce559b7862e7eaa6cec
reprojection-calibration <reprojection.calibration@gmail.com>
2025-12-17 14:34:53 +0100

    test loaded dimensions

--- code/python_tooling/tests/test_example.py
+++ code/python_tooling/tests/test_example.py
@@ -16,6 +16,12 @@ class TestDatabaseConnections(unittest.TestCase):
         self.assertEqual(len(loaded_data["/cam0/image_raw"]), 879)
         self.assertEqual(len(loaded_data["/cam1/image_raw"]), 879)
 
+        # Check that the dimensions of one of the loaded extracted targets matches our expectations (ex. 2,3,2)
+        extracted_target_i = loaded_data["/cam0/image_raw"][1520528314264184064]
+        self.assertEqual(extracted_target_i.pixels.shape, (144, 2))
+        self.assertEqual(extracted_target_i.points.shape, (144, 3))
+        self.assertEqual(extracted_target_i.indices.shape, (144, 2))
+
 
 if __name__ == '__main__':
     unittest.main()
