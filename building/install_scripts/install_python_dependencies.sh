a4c33a00bb1afeea66e03df852b6c6602053c07e
reprojection-calibration <reprojection.calibration@gmail.com>
2025-12-18 19:40:22 +0100

    use docker ENV

--- building/install_scripts/install_python_dependencies.sh
+++ building/install_scripts/install_python_dependencies.sh
@@ -8,15 +8,20 @@ apt-get install --no-install-recommends --yes \
     python3-venv \
     python3-pip
 
-VENV_DIR=~/.reprojection-venv
-python3 -m venv "$VENV_DIR"
-source "$VENV_DIR/bin/activate"
+python3 -m venv "${VENV_DIR}"
+source "${VENV_DIR}/bin/activate"
 
-# Build tooling
-pip install --upgrade build pip setuptools
 
-# General deps
-pip install numpy
+pip install --upgrade \
+ build \
+ numpy \
+ pip \
+ setuptools
 
 # Protobuf deps
-pip install grpcio grpcio-tools protobuf pyasn1 pyasn1-modules
+pip install --upgrade \
+  grpcio \
+  grpcio-tools \
+  protobuf \
+  pyasn1 \
+  pyasn1-modules
--- building/scripts/build_python_tooling.sh
+++ building/scripts/build_python_tooling.sh
@@ -2,15 +2,14 @@
 
 set -eoux pipefail
 
-# ERROR(Jack): Copy pasted from install_python_deps script!!!
-VENV_DIR=~/.reprojection-venv
-source "$VENV_DIR/bin/activate"
+source "${VENV_DIR}/bin/activate"
+
+proto_directory=/temporary/code/proto
+package_directory=/temporary/code/python_tooling
 
 # TODO(Jack): I would have liked to have the protobuf definition as an integrated part of the package (ex. in setup.py)
 #  but I could not get grpc_tools recognized in the setup.py script. Possibly to do with the fact that setup.py does not
-# have access to all the packages in the venv. But this is speculation and needs more reasearch.
-proto_directory=/temporary/code/proto
-package_directory=/temporary/code/python_tooling
+# have access to all the packages in the venv. But this is speculation and needs more research.
 python3 -m grpc_tools.protoc -I "${proto_directory}" --python_out="${package_directory}/generated" "${proto_directory}"/*.proto
 
 python3 -m pip install --editable "${package_directory}"
