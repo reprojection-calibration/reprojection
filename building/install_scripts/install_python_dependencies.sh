4db8a48597d08c64a001852fc4e5a619f918eaec
reprojection-calibration <reprojection.calibration@gmail.com>
2025-12-18 19:56:15 +0100

    comments

--- building/install_scripts/install_python_dependencies.sh
+++ building/install_scripts/install_python_dependencies.sh
@@ -2,15 +2,16 @@
 
 set -eoux pipefail
 
+# Required system dependencies for working with python in a venv
 apt-get update
 apt-get install --no-install-recommends --yes \
     python3 \
     python3-venv \
     python3-pip
 
+# Create and activate virtual environment
 python3 -m venv "${VENV_DIR}"
 source "${VENV_DIR}/bin/activate"
-
 
 pip install --upgrade \
  build \
