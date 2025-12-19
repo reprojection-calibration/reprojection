5a4e35d7621d9ca67d611ebc1829b2e71c9e0858
reprojection-calibration <reprojection.calibration@gmail.com>
2025-12-19 03:05:49 +0100

    working pyenv 3.12 install again

--- building/install_scripts/install_pyenv.sh
+++ building/install_scripts/install_pyenv.sh
@@ -2,9 +2,15 @@
 
 set -eoux pipefail
 
+# NOTE(Jack): This needs to be a separate script because we need to write the script to the .bash_profile and then
+# source that. Doing that in one single script
+
 wget -qO- https://pyenv.run | bash
 
+# NOTE(Jack): We create the .bashprofile because when we tried to use the .bashrc the other variables/script in the
+# .bashrc caused problems.
 touch ~/.bash_profile
+
 cat <<'EOF' | tee -a ~/.bash_profile > /dev/null
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
--- building/install_scripts/install_python_dependencies.sh
+++ building/install_scripts/install_python_dependencies.sh
@@ -5,8 +5,6 @@ set -eoux pipefail
 source ~/.bash_profile
 pyenv shell 3.12.12
 
-export VENV_DIR="~/.reprojection_venv"
-# Create and activate virtual environment
 python3.12 -m venv "${VENV_DIR}"
 source "${VENV_DIR}/bin/activate"
 
