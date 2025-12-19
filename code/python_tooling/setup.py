596ebd4a013eec4815d41aaa30f78c792a5fd5b7
reprojection-calibration <reprojection.calibration@gmail.com>
2025-12-19 12:08:18 +0100

    solve code checks

--- code/python_tooling/setup.py
+++ code/python_tooling/setup.py
@@ -4,7 +4,7 @@ setup(
     name="python_tooling",
     version="0.0.0",
     author="Jack Borer",
-    author_email="reprojection-calibration@gmail.com",
+    author_email="reprojection.calibration@gmail.com",
     description=(
         "A package for handling and visualization of calibration data."),
     license="GNU AGPL",
