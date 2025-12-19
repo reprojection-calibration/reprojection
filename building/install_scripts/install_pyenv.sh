4c59bb24c416c01bd0cb2dbcd1a7824131cc775a
reprojection-calibration <reprojection.calibration@gmail.com>
2025-12-19 12:14:34 +0100

    at python tooling to gha

--- building/install_scripts/install_pyenv.sh
+++ building/install_scripts/install_pyenv.sh
@@ -5,6 +5,20 @@ set -eoux pipefail
 # NOTE(Jack): This needs to be a separate script because we need to write the script to the .bash_profile and then
 # source that.
 
+# System deps required for pyenv to build python - I am not 100% sure we need all of these.
+apt-get update
+apt-get install --no-install-recommends --yes \
+    libbz2-dev \
+    libffi-dev \
+    libncursesw5-dev \
+    libreadline-dev \
+    libssl-dev \
+    libxmlsec1-dev \
+    libxml2-dev \
+    tk-dev
+
+rm --force --recursive /var/lib/apt/lists/*
+
 wget -qO- https://pyenv.run | bash
 
 # NOTE(Jack): We create the .bashprofile because when we tried to use the .bashrc the other variables/script in the
