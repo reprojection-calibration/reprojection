4ec402ac9b72d59fdcad73dd5409301105325862
reprojection-calibration <reprojection.calibration@gmail.com>
2025-12-20 15:16:42 +0100

    test_load_image_frame_data

--- code/python_tooling/database/load_image_frame_data.py
+++ code/python_tooling/database/load_image_frame_data.py
@@ -1,13 +1,45 @@
 from database.load_extracted_targets import load_all_extracted_targets
 from database.load_poses import load_poses
+from bisect import bisect_left
+
+
+def closest_timestamp(timestamps, timestamp_x):
+    if not timestamps:
+        return None
+
+    # Find the insertion point for timestamp_x
+    pos = bisect_left(timestamps, timestamp_x)
+
+    if pos == 0:
+        return timestamps[0]
+    if pos == len(timestamps):
+        return timestamps[-1]
+
+    before = timestamps[pos - 1]
+    after = timestamps[pos]
+
+    # Return whichever is closer
+    if abs(after - timestamp_x) < abs(timestamp_x - before):
+        return after
+    else:
+        return before
 
 
 def load_image_frame_data(db_path):
-    extracted_targets = load_all_extracted_targets(db_path)
+    # NOTE(Jack): In this function we take advantage of the fact that for image data the foreign key relationships force
+    # that any camera pose has to match an extracted target.
+    image_frames = load_all_extracted_targets(db_path)
 
-    # TODO(Jack): Just put external poses dict in core loading functions
+    # For external poses there is no timestamp matching requirement, so if they are available we just take the closest
+    # to the time from our extracted target frames.
     external_poses = load_poses(db_path, 'external', 'ground_truth')
+    if external_poses is not None:
+        # WARN(Jack): Hardcoded '/mocap0'
+        external_pose_times = sorted(external_poses['/mocap0'].keys())
 
+        for sensor, frames in image_frames.items():
+            for timestamp_i in frames:
+                match = closest_timestamp(external_pose_times, timestamp_i)
+                image_frames[sensor][timestamp_i]['external_pose'] = external_poses['/mocap0'][match]
 
-    # for sensor, frames in extracted_targets.items():
-     #   print(frames)
+    return image_frames
--- code/python_tooling/tests/test_database_connections.py
+++ code/python_tooling/tests/test_database_connections.py
@@ -15,8 +15,19 @@ class TestDatabaseConnections(unittest.TestCase):
             "DB_PATH", "/temporary/code/test_data/dataset-calib-imu4_512_16.db3")
 
     def test_load_image_frame_data(self):
-        data = load_image_frame_data(self.db_path)
-        print(data)
+        loaded_data = load_image_frame_data(self.db_path)
+        self.assertEqual(len(loaded_data), 2)
+        self.assertEqual(len(loaded_data["/cam0/image_raw"]), 879)
+        self.assertEqual(len(loaded_data["/cam1/image_raw"]), 879)
+
+        image_data_i = loaded_data["/cam0/image_raw"][1520528314264184064]
+        self.assertEqual(len(image_data_i['external_pose']), 7)
+
+        image_data_i = loaded_data["/cam1/image_raw"][1520528314264184064]
+        self.assertEqual(len(image_data_i['external_pose']), 7)
+
+
+
 
     def test_pose_loading(self):
         # At time of writing there is no camera pose data in the test_data database
