c5ef9e3a62a1494e3ff5699269893605f9d91772
reprojection-calibration <reprojection.calibration@gmail.com>
2025-12-20 21:50:23 +0100

    load database

--- code/python_tooling/dashboard/app.py
+++ code/python_tooling/dashboard/app.py
@@ -1,13 +1,14 @@
 from dash import Dash, dcc, html, Input, Output, callback
 import os
+from database.load_image_frame_data import load_image_frame_data
 
-DB_DIR = '../../test_data'
+DB_DIR = '../../test_data/'
 
 app = Dash()
 app.layout = html.Div([
     html.Button('Refresh Database List', id='refresh-database-list-button', n_clicks=0),
-    dcc.Dropdown(id='database-dropdown'),
-    html.Div(id='dd-output-container')  # REMOVE
+    dcc.Dropdown(id='database-dropdown', placeholder="Select a database", style={"width": "50%"}),
+    dcc.Store(id='database-store'),
 ])
 
 
@@ -20,6 +21,21 @@ def refresh_database_list(value):
         return []
 
     return sorted([f for f in os.listdir(DB_DIR) if f.endswith(".db3")])
+
+
+@callback(
+    Output('database-store', 'data'),
+    Input('database-dropdown', 'value')
+)
+def load_database_to_store(db_file):
+    if not db_file:
+        return None
+
+    full_path = DB_DIR + db_file
+    if not os.path.isfile(full_path):
+        return None
+
+    return load_image_frame_data(full_path)
 
 
 if __name__ == '__main__':
