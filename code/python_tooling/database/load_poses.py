5a0e92d7ef41eef80669623d9af6aa0214b3fc99
reprojection-calibration <reprojection.calibration@gmail.com>
2026-01-07 14:16:16 +0100

    remove external poses

--- code/python_tooling/database/load_poses.py
+++ code/python_tooling/database/load_poses.py
@@ -18,10 +18,6 @@ def load_poses_df(db_path, table, type):
                 df = pd.read_sql(
                     load_sql('camera_poses_select.sql'), conn, params=(type,)
                 )
-            elif table == "external":
-                df = pd.read_sql(
-                    load_sql('external_poses_select.sql'), conn, params=(type,)
-                )
     except Exception as e:
         print(f"Unexpected error in load_poses_df(db_path={db_path}, {table}, {type}): {e}")
         return None
@@ -29,11 +25,6 @@ def load_poses_df(db_path, table, type):
     return df
 
 
-# NOTE(Jack): In this function we take advantage of the fact that for image data the foreign key relationships force
-# that any camera pose has to match an extracted target - and we timestamp sync our external pose to that time
-# TODO(Jack): Only plot external targets if they are available
-# TODO(Jack): This function does not have the right name! Align what is actually being loaded here with the name. Just
-#  because we are loading data associated with an image frames does not mean it belongs in a data named load_image_frame_data
 def load_calibration_poses(db_path):
     df_initial_camera_poses = load_poses_df(db_path, 'camera', 'initial')
     if df_initial_camera_poses is not None:
@@ -43,8 +34,4 @@ def load_calibration_poses(db_path):
     if df_optimized_camera_poses is not None:
         df_optimized_camera_poses = df_optimized_camera_poses.sort_values("timestamp_ns")
 
-    df_external_poses = load_poses_df(db_path, 'external', 'ground_truth')
-    if df_external_poses is not None:
-        df_external_poses = df_external_poses.sort_values("timestamp_ns")
-
-    return df_initial_camera_poses, df_optimized_camera_poses, df_external_poses
+    return df_initial_camera_poses, df_optimized_camera_poses
