02af58a3d32799d9344384cb5c9292da65c72ec4
reprojection-calibration <reprojection.calibration@gmail.com>
2026-01-08 08:38:35 +0100

    refactor test to use LinearPoseInitialization with view

--- code/library/calibration/include/calibration/linear_pose_initialization.hpp
+++ code/library/calibration/include/calibration/linear_pose_initialization.hpp
@@ -8,11 +8,12 @@
 
 namespace reprojection::calibration {
 
-// TODO(Jack): The word "linear" comes more from the high order abstraction concept of the role this functions plays
+// NOTE(Jack): The word "linear" comes more from the high order abstraction concept of the role this functions plays
 //  in the top level calibration process. As the linear solution to initialize the nonlinear optimization problem.
 //  Therefore it feels like the name here is wrong and should not depend on a context from a higher level of
 //  abstraction. If we can think of a better name we can fix this!
 // TODO(Jack): How should we pass the view? Is by value ok here?
+// TODO(Jack): formalize handling of failed frames, use std optional?
 void LinearPoseInitialization(InitializationDataView data_view);
 
 }  // namespace reprojection::calibration
--- code/library/calibration/test/linear_pose_initialization.test.cpp
+++ code/library/calibration/test/linear_pose_initialization.test.cpp
@@ -23,37 +23,29 @@ TEST(CalibrationLinearPoseInitialization, TestLinearPoseInitialization) {
     int const num_frames{20};
     std::vector<Frame> const frames{generator.GenerateBatch(num_frames)};
 
-    // Convert the mvg data into pseudo extracted targets. Unlike real extracted targets these targets have 3D points
-    // (not 2D z=0 target points) and do not have any indices.
-    std::set<ExtractedTargetStamped> targets;
-    uint64_t pseudo_time{0};
-    for (auto const& frame : frames) {
-        // WARN(Jack): Indices is left blank even though technically the indices size should always match the bundle!!!
-        // Maybe we will add a constructor check to Target that enforces these sizes match and will need to fix this
-        // here.
-        targets.insert({{pseudo_time, "/cam/retro/123"}, {frame.bundle, ArrayX2i{}}});
+    CameraSensorData cam0{{"/cam/retro/123", CameraModel::DoubleSphere}, Array6d{600, 600, 360, 240, 0.1, 0.2}, {}, {}};
 
+    // TODO(Jack): Refactor mvg generator to use new calibration types??? Or does that not make any sense? At least
+    // provide an adaptor that converts frames into the data field of the dict.
+    uint64_t pseudo_time{0};
+    for (auto const& frame_i : frames) {
+        // Unlike real extracted targets this target has 3D points (not 2D z=0 target points) and does not have indices.
+        cam0.frames[pseudo_time].extracted_target.bundle = frame_i.bundle;
         pseudo_time += 1;
     }
 
     // NOTE(Jack): Because the mvg points are 3d (i.e. z!=0) the underlying pnp implementation tested here will use
     // Dlt23 and not Dlt22 which is what is normally used during the calibration process. Technically this does not make
     // a difference, but if we remove the Dlt23 at some later date we might need to change something here.
-    auto const poses{calibration::LinearPoseInitialization(
-        targets, std::unique_ptr<projection_functions::Camera>(
-                     new projection_functions::DoubleSphereCamera(double_sphere_intrinsics)))};
+    calibration::LinearPoseInitialization(InitializationDataView(cam0));
 
-    for (auto const& stamped_pose_i : poses) {
-        // WARN(Jack): We are assuming but not checking that all frames were successfully initialized. If this is no
-        // true then the way we abuse the timestamp to index into frames will cause a segfault. This abuse of the
-        // timestamp is not acceptable in any application code, and even here in the testing it will likely cause us
-        // pain in the future!
-        Isometry3d const gt_pose_i{frames[stamped_pose_i.header.timestamp_ns].pose};
+    for (auto const& frame_i : cam0.frames) {
+        Isometry3d const gt_pose_i{frames[frame_i.first].pose};
         Vector6d const se3_gt_pose_i{geometry::Log(gt_pose_i.inverse())};  // Note the inverse!
 
-        EXPECT_TRUE(stamped_pose_i.pose.isApprox(se3_gt_pose_i, 1e-6))
+        EXPECT_TRUE(frame_i.second.initial_pose.isApprox(se3_gt_pose_i, 1e-6))
             << "Linear pose initialization result:\n"
-            << stamped_pose_i.pose.transpose() << "\nGround truth:\n"
+            << frame_i.second.initial_pose.transpose() << "\nGround truth:\n"
             << se3_gt_pose_i.transpose();
     }
 }
