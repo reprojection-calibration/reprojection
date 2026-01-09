4b7979341f979d50de238828a1a121d55b947fe1
reprojection-calibration <reprojection.calibration@gmail.com>
2026-01-09 10:58:20 +0100

    annotations

--- code/library/demos/examples/pose_initialization.cpp
+++ code/library/demos/examples/pose_initialization.cpp
@@ -18,31 +18,34 @@ int main() {
     // can officially document?
     std::string const record_path{"/tmp/reprojection/code/test_data/dataset-calib-imu4_512_16.db3"};
     auto db{std::make_shared<database::CalibrationDatabase>(record_path, false, false)};
-    std::string const sensor_name{"/cam0/image_raw"};
 
+    std::string const sensor_name{"/cam0/image_raw"};
     CameraCalibrationData cam_data{
         {sensor_name, CameraModel::DoubleSphere},
         Array6d{156.82590211, 156.79756958, 254.99978685, 256.9744566, -0.17931409, 0.59133716},
         {},
         {}};
 
-    // Cam 0
+    // Load targets
     database::GetExtractedTargetData(db, cam_data);
 
+    // Linear initialization and save
     calibration::LinearPoseInitialization(InitializationDataView(cam_data));
-    (void)AddPoseData(cam_data, database::PoseType::Initial, db);
+    AddPoseData(cam_data, database::PoseType::Initial, db);
 
     // Artificially restrict ourselves to the first couple hundred frames where we converge successfully.
     auto it = cam_data.frames.upper_bound(1520528332714760192);
     cam_data.frames.erase(it, cam_data.frames.end());
 
+    // Nonlinear optimization and save
     optimization::CameraNonlinearRefinement(OptimizationDataView(cam_data));
+
     // WARN(Jack): At this time we have unsettled coordinate frame connventions. Because of this we need to invert the
     // poses here to match the initial poses. This is a well known problem!
     for (auto& frame_i : cam_data.frames) {
         frame_i.second.optimized_pose = geometry::Log(geometry::Exp(frame_i.second.optimized_pose).inverse());
     }
-    (void)AddPoseData(cam_data, database::PoseType::Optimized, db);
+    AddPoseData(cam_data, database::PoseType::Optimized, db);
 
     std::cout << cam_data.optimized_intrinsics.transpose() << std::endl;
 
