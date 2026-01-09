45a03d7d646fe946b208e07f56b17178cef32d49
reprojection-calibration <reprojection.calibration@gmail.com>
2026-01-09 06:41:37 +0100

    use sensor name

--- code/library/demos/examples/pose_initialization.cpp
+++ code/library/demos/examples/pose_initialization.cpp
@@ -17,16 +17,17 @@ int main() {
     // can officially document?
     std::string const record_path{"/tmp/reprojection/code/test_data/dataset-calib-imu4_512_16.db3"};
     auto db{std::make_shared<database::CalibrationDatabase>(record_path, false, false)};
+    std::string const sensor_name{"/cam0/image_raw"};
 
-    CameraSensorData cam0{{"/cam0/image_raw", CameraModel::DoubleSphere},
-                          Array6d{156.82590211, 156.79756958, 254.99978685, 256.9744566, -0.17931409, 0.59133716},
+    CameraSensorData cam0{{sensor_name, CameraModel::DoubleSphere},
+                          Array6d{156.82590211, 156.79756958, 154.99978685, 256.9744566, -0.17931409, 0.59133716},
                           {},
                           {}};
 
     // Cam 0
     // TODO LOAD THE TARGET DIRECTLY INTO THE NEW DATA STRUCTURE
     auto const cam0_targets_stamped{
-        database::GetExtractedTargetData(db, "/cam0/image_raw").value()};  // WARN UNPROTECTED OPTIONAL ACCESS
+        database::GetExtractedTargetData(db, sensor_name).value()};  // WARN UNPROTECTED OPTIONAL ACCESS
     for (auto const& target : cam0_targets_stamped) {
         cam0.frames[target.header.timestamp_ns].extracted_target = target.target;
     }
@@ -48,7 +49,8 @@ int main() {
 
     std::set<PoseStamped> cam0_poses;
     for (auto const& frame_i : cam0.frames) {
-        cam0_poses.insert({{frame_i.first, cam0.sensor.sensor_name}, frame_i.second.optimized_pose});
+        cam0_poses.insert({{frame_i.first, cam0.sensor.sensor_name},
+                           geometry::Log(geometry::Exp(frame_i.second.optimized_pose).inverse())});
     }
     (void)AddPoseData(cam0_poses, database::PoseType::Optimized, db);
 
