5f03f3e047dd18c71a8733170fd6088a3e060694
reprojection-calibration <reprojection.calibration@gmail.com>
2026-01-11 08:35:53 +0100

    coverage image

--- code/library/database/src/image_interface.cpp
+++ code/library/database/src/image_interface.cpp
@@ -49,11 +49,12 @@ ImageStreamer::ImageStreamer(std::shared_ptr<CalibrationDatabase const> const da
     try {
         Sqlite3Tools::Bind(statement_.stmt, 1, sensor_name);
         Sqlite3Tools::Bind(statement_.stmt, 2, static_cast<int64_t>(start_time));  // Warn cast!
-    } catch (std::runtime_error const& e) {
-        std::throw_with_nested(std::runtime_error(ErrorMessage("ImageStreamer::ImageStreamer()", sensor_name_,
-                                                               start_time, SqliteErrorCode::FailedBinding,
-                                                               std::string(sqlite3_errmsg(database_->db)))));
-    }
+    } catch (std::runtime_error const& e) {                                        // LCOV_EXCL_LINE
+        std::throw_with_nested(std::runtime_error(                                 // LCOV_EXCL_LINE
+            ErrorMessage("ImageStreamer::ImageStreamer()", sensor_name_,           // LCOV_EXCL_LINE
+                         start_time, SqliteErrorCode::FailedBinding,               // LCOV_EXCL_LINE
+                         std::string(sqlite3_errmsg(database_->db)))));            // LCOV_EXCL_LINE
+    }  // LCOV_EXCL_LINE
 }
 
 std::optional<ImageStamped> ImageStreamer::Next() {
@@ -61,10 +62,11 @@ std::optional<ImageStamped> ImageStreamer::Next() {
     if (code == static_cast<int>(SqliteFlag::Done)) {
         return std::nullopt;
     } else if (code != static_cast<int>(SqliteFlag::Row)) {
-        std::cerr << ErrorMessage("ImageStreamer::Next()", sensor_name_, 0, SqliteErrorCode::FailedStep,
-                                  std::string(sqlite3_errmsg(database_->db)))
-                  << "\n";
-        return std::nullopt;
+        std::cerr << ErrorMessage("ImageStreamer::Next()", sensor_name_, 0,    // LCOV_EXCL_LINE
+                                  SqliteErrorCode::FailedStep,                 // LCOV_EXCL_LINE
+                                  std::string(sqlite3_errmsg(database_->db)))  // LCOV_EXCL_LINE
+                  << "\n";                                                     // LCOV_EXCL_LINE
+        return std::nullopt;                                                   // LCOV_EXCL_LINE
     }
 
     uint64_t const timestamp_ns{std::stoull(reinterpret_cast<const char*>(sqlite3_column_text(statement_.stmt, 0)))};
@@ -72,19 +74,19 @@ std::optional<ImageStamped> ImageStreamer::Next() {
     uchar const* const blob{static_cast<uchar const*>(sqlite3_column_blob(statement_.stmt, 1))};
     int const blob_size{sqlite3_column_bytes(statement_.stmt, 1)};
     if (not blob or blob_size <= 0) {
-        std::cerr << "ImageStreamer::Next() blob reading failed for sensor: " + sensor_name_ +
-                         " at timestamp_ns: " + std::to_string(timestamp_ns)
-                  << "\n";
-        return std::nullopt;
+        std::cerr << "ImageStreamer::Next() blob reading failed for sensor: " + sensor_name_ +  // LCOV_EXCL_LINE
+                         " at timestamp_ns: " + std::to_string(timestamp_ns)                    // LCOV_EXCL_LINE
+                  << "\n";                                                                      // LCOV_EXCL_LINE
+        return std::nullopt;                                                                    // LCOV_EXCL_LINE
     }
 
     std::vector<uchar> const buffer(blob, blob + blob_size);
     cv::Mat const image{cv::imdecode(buffer, cv::IMREAD_UNCHANGED)};
     if (image.empty()) {
-        std::cerr << "ImageStreamer::Next() cv::imdecode() failed for sensor: " + sensor_name_ +
-                         " at timestamp_ns: " + std::to_string(timestamp_ns)
-                  << "\n";
-        return std::nullopt;
+        std::cerr << "ImageStreamer::Next() cv::imdecode() failed for sensor: " + sensor_name_ +  // LCOV_EXCL_LINE
+                         " at timestamp_ns: " + std::to_string(timestamp_ns)                      // LCOV_EXCL_LINE
+                  << "\n";                                                                        // LCOV_EXCL_LINE
+        return std::nullopt;                                                                      // LCOV_EXCL_LINE
     }
 
     return ImageStamped{{timestamp_ns, sensor_name_}, image};
