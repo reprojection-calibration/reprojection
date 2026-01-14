a548869552b173260db665d4d0dd702b821e67d5
reprojection-calibration <reprojection.calibration@gmail.com>
2026-01-14 19:05:25 +0100

    comments

--- code/python_tooling/tests/test_dashboard_callbacks_slider.py
+++ code/python_tooling/tests/test_dashboard_callbacks_slider.py
@@ -12,6 +12,7 @@ class TestDashboardCallbacksSlider(unittest.TestCase):
         output = update_slider_marks("", [None, {}])
         self.assertEqual(output, {})
 
+        # Happy path success
         selected_sensor = "/cam0/image_raw"
         timestamps_sorted = {selected_sensor: [int(i * 1e9) for i in range(20)]}
 
@@ -31,12 +32,14 @@ class TestDashboardCallbacksSlider(unittest.TestCase):
 
     def test_advance_slider(self):
         # Note that the Input("play-interval", "n_intervals") is only here to trigger this function! We do not use the
-        # provided n_intervals in any real way and instead use the State("frame-id-slider", "value") to track our frame idx.
+        # provided n_intervals (the first argument to this function) in any real way and instead use the
+        # State("frame-id-slider", "value") to track our frame idx.
         output = advance_slider(None, None, None)
         self.assertEqual(output, 0)
 
         output = advance_slider(None, 0, 10)
         self.assertEqual(output, 1)
 
+        # Loop back to the start
         output = advance_slider(None, 10, 10)
         self.assertEqual(output, 0)
