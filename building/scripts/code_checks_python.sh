23892d638e92339a34b4e69a736df6e7109eb38a
reprojection-calibration <reprojection.calibration@gmail.com>
2026-01-14 16:21:28 +0100

    add isort config

--- building/scripts/code_checks_python.sh
+++ building/scripts/code_checks_python.sh
@@ -4,4 +4,4 @@ set -eoux pipefail
 
 black --check --exclude '_pb2\.py$' --verbose '/temporary/code/python_tooling'
 
-isort --check --profile black -p dashboard,database,generated '/temporary/code/python_tooling'
+isort --check '/temporary/code/python_tooling'
