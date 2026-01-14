e487d47c37d85d828ca66334d0b8a4802cf75e25
reprojection-calibration <reprojection.calibration@gmail.com>
2026-01-14 19:37:26 +0100

    format

--- code/python_tooling/tests/test_dashboard_callbacks_statistics.py
+++ code/python_tooling/tests/test_dashboard_callbacks_statistics.py
@@ -10,7 +10,14 @@ class TestDashboardCallbacksStatistics(unittest.TestCase):
         self.assertRaises(PreventUpdate, update_statistics, None, None)
 
         sensor = "/cam0/image_raw"
-        statistics = {sensor: {'statistic_1': 0, 'statistic_2': 100, 'statistic_3': 101, 'statistic_4': 102}}
+        statistics = {
+            sensor: {
+                "statistic_1": 0,
+                "statistic_2": 100,
+                "statistic_3": 101,
+                "statistic_4": 102,
+            }
+        }
         output = update_statistics(sensor, [statistics, None])
 
         # Four statistics to display
@@ -18,7 +25,7 @@ class TestDashboardCallbacksStatistics(unittest.TestCase):
 
         def assert_statistic_row_element(div, color, value, label):
             div_colored_box = div.children[0]
-            self.assertEqual(div_colored_box.style['backgroundColor'], color)
+            self.assertEqual(div_colored_box.style["backgroundColor"], color)
 
             div_counter = div.children[1]
             self.assertEqual(div_counter.children, value)
@@ -29,9 +36,9 @@ class TestDashboardCallbacksStatistics(unittest.TestCase):
         # Check the first statistic which should display a red box because there are no frames for statistic_1
         div_statistic_1 = output[0]
         self.assertEqual(len(div_statistic_1.children), 3)
-        assert_statistic_row_element(div_statistic_1, 'red', 0, 'statistic_1')
+        assert_statistic_row_element(div_statistic_1, "red", 0, "statistic_1")
 
         # Check any of the other three statistics to make sure it is a green box
         div_statistic_2 = output[1]
         self.assertEqual(len(div_statistic_2.children), 3)
-        assert_statistic_row_element(div_statistic_2, 'green', 100, 'statistic_2')
+        assert_statistic_row_element(div_statistic_2, "green", 100, "statistic_2")
