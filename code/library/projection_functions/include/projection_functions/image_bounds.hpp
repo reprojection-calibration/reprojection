8a2e5997e0e529ebe507491f7795e6da2d5de5a9
reprojection-calibration <reprojection.calibration@gmail.com>
2026-01-17 10:28:58 +0100

    add image bounds and testing

---
+++ code/library/projection_functions/include/projection_functions/image_bounds.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "types/eigen_types.hpp"
+
+namespace reprojection::projection_functions {
+
+// TODO(Jack): Split between source and header?
+class ImageBounds {
+   public:
+    ImageBounds(Array4d const& image_dimension)
+        : ImageBounds(image_dimension[0], image_dimension[1], image_dimension[2], image_dimension[3]) {}
+
+    bool InBounds(double const u, double const v) const {
+        return u_min_ < u and u < u_max_ and v_min_ < v and v < v_max_;
+    }
+
+   private:
+    ImageBounds(double const min_width, double const max_width, double const min_height, double const max_height)
+        : u_min_{min_width}, u_max_{max_width}, v_min_{min_height}, v_max_{max_height} {}
+
+    double u_min_;
+    double u_max_;
+    double v_min_;
+    double v_max_;
+};
+
+}  // namespace reprojection::projection_functions
---
+++ code/library/projection_functions/test/image_bounds.test.cpp
@@ -0,0 +1,23 @@
+#include "projection_functions/image_bounds.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace reprojection;
+
+TEST(ProjectionFunctionsImageBounds, TestImageBounds) {
+    projection_functions::ImageBounds const image_bounds{{-1, 1, -1, 1}};
+
+    EXPECT_TRUE(image_bounds.InBounds(0, 0));
+    EXPECT_TRUE(image_bounds.InBounds(0.99, 0.99));
+    EXPECT_TRUE(image_bounds.InBounds(-0.99, 0.99));
+    EXPECT_TRUE(image_bounds.InBounds(0.99, -0.99));
+    EXPECT_TRUE(image_bounds.InBounds(-0.99, -0.99));
+
+    EXPECT_FALSE(image_bounds.InBounds(1.01, 1.01));
+    EXPECT_FALSE(image_bounds.InBounds(-1.01, 1.01));
+    EXPECT_FALSE(image_bounds.InBounds(1.01, -1.01));
+    EXPECT_FALSE(image_bounds.InBounds(-1.01, -1.01));
+
+    EXPECT_FALSE(image_bounds.InBounds(0, 1.01));
+    EXPECT_FALSE(image_bounds.InBounds(1.01, 0));
+}
