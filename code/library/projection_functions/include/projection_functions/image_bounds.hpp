2222e7fca0e1bfcdfcbe4396b20adfd481d55c54
reprojection-calibration <reprojection.calibration@gmail.com>
2026-01-17 11:47:28 +0100

    make limits clear

--- code/library/projection_functions/include/projection_functions/image_bounds.hpp
+++ code/library/projection_functions/include/projection_functions/image_bounds.hpp
@@ -4,9 +4,12 @@
 
 namespace reprojection::projection_functions {
 
+// NOTE(Jack): Our current definition of image bounds is [min, max). This means that an image bounds set with a width
+// dimension of 0 to 720 will consider 0 a valid dimension, but 720 will be out of bounds. The maximum valid dimension
+// would be 719.999999... If this is really algorithmically accurate, is not clear at this time.
 template <typename T>
 bool InBounds(ImageBounds const& bounds, T const u, T const v) {
-    return bounds.u_min < u and u < bounds.u_max and bounds.v_min < v and v < bounds.v_max;
+    return bounds.u_min <= u and u < bounds.u_max and bounds.v_min <= v and v < bounds.v_max;
 }
 
 }  // namespace reprojection::projection_functions
--- code/library/projection_functions/test/image_bounds.test.cpp
+++ code/library/projection_functions/test/image_bounds.test.cpp
@@ -7,6 +7,18 @@
 using namespace reprojection;
 
 TEST(ProjectionFunctionsImageBounds, TestImageBounds) {
+    ImageBounds const bounds{0, 720, 0, 420};
+
+    EXPECT_TRUE(projection_functions::InBounds(bounds, 0.0, 0.0));
+    EXPECT_TRUE(projection_functions::InBounds(bounds, 0.0, 419.999));
+    EXPECT_TRUE(projection_functions::InBounds(bounds, 719.999, 0.0));
+
+    EXPECT_FALSE(projection_functions::InBounds(bounds, -1e3, -1e3));
+    EXPECT_FALSE(projection_functions::InBounds(bounds, 0.0, 420.0));
+    EXPECT_FALSE(projection_functions::InBounds(bounds, 720.0, 0.0));
+}
+
+TEST(ProjectionFunctionsImageBounds, TestImageBoundsNegative) {
     ImageBounds const bounds{-1, 1, -1, 1};
 
     EXPECT_TRUE(projection_functions::InBounds(bounds, 0, 0));
