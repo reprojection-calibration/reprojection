34bbc6891117f6494e3912baded23a776c8f7ecc
reprojection-calibration <reprojection.calibration@gmail.com>
2026-01-19 19:59:33 +0100

    sort comments above method

--- code/library/optimization/src/nonlinear_refinement.cpp
+++ code/library/optimization/src/nonlinear_refinement.cpp
@@ -6,48 +6,46 @@
 namespace reprojection::optimization {
 
 // TODO(Jack): Return report summary of optimization.
+// TODO(Jack): Only provide valid frames in the data_view! Maybe even provide them pre initialized? Then the init pose
+//  or optimized pose does not even need to be optional maybe!?
+// TODO(Jack): Is there not a better way to describe this chained relationship between the initial pose and the
+//  following things like optimized pose etc. Because if there is not initial pose there cannot be anything else!
+//  Technically if there is no intial pose that frame should not even be visible in the OptimizationDataView I
+//  think...
+// TODO(Jack): Use the valid cost function mask to visualize only the valid pixels in the dashboard.
+//  Calculate initial reprojection error and get the valid cost function mask.
+// ERROR(Jack): What is a frame has too few valid pixels to actually constrain the pose? Should we entirely skip
+// that frame? Or what if in general we have a minimum required of points per frame threshold?
+// TODO(Jack): What should we do with the mask from the optimized reprojection error calculation? Does it have meaning
+//  for us?
 void CameraNonlinearRefinement(OptimizationDataView data_view) {
-    // TODO(Jack): When we construct the data view should this automatically happen? Or when we set the initial
-    // intrinsics? Same idea with the initial vs. optimized pose. But maybe it is better to be explicit here.
-    data_view.optimized_intrinsics() = data_view.initial_intrinsics();
-
     std::map<uint64_t, std::vector<std::unique_ptr<ceres::CostFunction>>> cost_functions;
     for (OptimizationFrameView frame_i : data_view) {
-        MatrixX2d const& pixels_i{frame_i.extracted_target().bundle.pixels};
-        MatrixX3d const& points_i{frame_i.extracted_target().bundle.points};
-
-        // TODO(Jack): Is there not a better way to describe this chained relationship between the initial pose and the
-        // following things like optimized pose etc. Because if there is not initial pose there cannot be anything else!
-        // Technically if there is no intial pose that frame should not even be visible in the OptimizationDataView I
-        // think...
         if (frame_i.initial_pose()) {
             frame_i.optimized_pose() = frame_i.initial_pose();
         } else {
-            frame_i.optimized_pose() = std::nullopt;  // LCOV_EXCL_LINE
-            continue;                                 // LCOV_EXCL_LINE
+            continue;  // LCOV_EXCL_LINE
         }
 
+        MatrixX2d const& pixels_i{frame_i.extracted_target().bundle.pixels};
+        MatrixX3d const& points_i{frame_i.extracted_target().bundle.points};
         for (Eigen::Index j{0}; j < pixels_i.rows(); ++j) {
             cost_functions[frame_i.timestamp_ns()].emplace_back(
                 Create(data_view.camera_model(), data_view.image_bounds(), pixels_i.row(j), points_i.row(j)));
         }
     }
 
+    data_view.optimized_intrinsics() = data_view.initial_intrinsics();
+
     ceres::Problem::Options problem_options;
     problem_options.cost_function_ownership = ceres::DO_NOT_TAKE_OWNERSHIP;
     ceres::Problem problem{problem_options};
     for (OptimizationFrameView frame_i : data_view) {
-        // TODO(Jack): This pose is not optimized yet! It not being std::nullopt here simply means that it was
-        // successfully initialized and is ready to be optimized.
-        if (not frame_i.optimized_pose()) {
+        if (not cost_functions.contains(frame_i.timestamp_ns())) {
             continue;  // LCOV_EXCL_LINE
         }
         auto const& cost_functions_i{cost_functions.at(frame_i.timestamp_ns())};
 
-        // TODO(Jack): Use the valid cost function mask to visualize only the valid pixels in the dashboard.
-        // Calculate initial reprojection error and get the valid cost function mask.
-        // ERROR(Jack): What is a frame has too few valid pixels to actually constrain the pose? Should we entirely skip
-        // that frame? Or what if in general we have a minimum required of points per frame threshold?
         ArrayXb mask;
         std::tie(frame_i.initial_reprojection_error(), mask) = EvaluateReprojectionResiduals(
             cost_functions_i, data_view.initial_intrinsics(), frame_i.initial_pose().value());
@@ -70,7 +68,6 @@ void CameraNonlinearRefinement(OptimizationDataView data_view) {
             continue;  // LCOV_EXCL_LINE
         }
 
-        // TODO(Jack): What should we do with this mask here? Does it have meaning for us?
         ArrayXb what_to_do;
         std::tie(frame_i.optimized_reprojection_error(), what_to_do) =
             EvaluateReprojectionResiduals(cost_functions.at(frame_i.timestamp_ns()), data_view.optimized_intrinsics(),
