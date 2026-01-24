632cc6a84c50064ff2b2c52fdc0d58ade9350d4d
reprojection-calibration <reprojection.calibration@gmail.com>
2026-01-24 08:35:13 +0100

    throw on left over keys

--- code/library/config/src/ceres_solver_options.test.cpp
+++ code/library/config/src/ceres_solver_options.test.cpp
@@ -11,19 +11,26 @@
 
 namespace reprojection::config {
 
-ceres::Solver::Options ParseSolverOptions(toml::table const& config) {
+ceres::Solver::Options ParseSolverOptions(toml::table config) {
     // NOTE(Jack): The options struct is initialized with default values. That means that if in the following code a
     // value is not explicitly set, that its value is preserved as the default.
     ceres::Solver::Options options;
 
-    auto const* solver_cfg{config["solver"].as_table()};
+    auto* const solver_cfg{config["solver"].as_table()};
     if (not solver_cfg) {
         return options;
     }
 
+    // TODO(Jack): We need a better policy here! We should inform the user if the type is wrong, not just silently fail.
     if (auto const value{solver_cfg->get_as<std::string>("minimizer_type")}) {
+        solver_cfg->erase("minimizer_type");
         options.minimizer_type =
             CeresEnumToString<ceres::MinimizerType, ceres::StringToMinimizerType>(value->as_string()->get());
+    }
+
+    if (solver_cfg->size() != 0) {
+        // TODO(Jack): Print the keys and values in the error message
+        throw std::runtime_error("Unread keys found in table xxxx!!!!");
     }
 
     return options;
@@ -54,4 +61,16 @@ TEST(ConfigCeresSolverOptions, TestLoadSolverOptionsMinimizerType) {
 
     auto const solver_options{config::ParseSolverOptions(config)};
     EXPECT_EQ(solver_options.minimizer_type, ceres::LINE_SEARCH);
+}
+
+// Given the wrong type this key/value will not be parsed and removed from the table, which means we will have a
+// leftover key at the end of parsing which is an error and we throw on.
+TEST(ConfigCeresSolverOptions, TestLoadSolverOptionsMinimizerTypeWrongType) {
+    static constexpr std::string_view config_file{R"(
+        [solver]
+        minimizer_type = 101.1
+    )"sv};
+    toml::table const config{toml::parse(config_file)};
+
+    EXPECT_THROW(config::ParseSolverOptions(config), std::runtime_error);
 }
