50e525bd1627c4765beb5c64a15eceec463c625f
reprojection-calibration <reprojection.calibration@gmail.com>
2026-01-26 08:01:22 +0100

    write the testing config at test time

--- code/library/config/include/config/load_configuration.hpp
+++ code/library/config/include/config/load_configuration.hpp
@@ -7,6 +7,7 @@
 namespace reprojection::config {
 
 // NOTE(Jack): As we add more configs we will probably return a tuple here or a struct with all the possible configs.
+// TODO(Jack): Should we use filesystem paths instead of strings? I think so...
 ceres::Solver::Options LoadConfiguration(std::string const& file);
 
 }  // namespace reprojection::config
--- code/library/config/test/load_configuration.test.cpp
+++ code/library/config/test/load_configuration.test.cpp
@@ -2,19 +2,55 @@
 
 #include <gtest/gtest.h>
 
+#include <filesystem>
+#include <fstream>
+
 using namespace reprojection;
 
+// TODO THIS BELONGS IN A COMMON TESTING PACKAGE!
+class TempFile {
+   public:
+    TempFile(std::string_view const& contents) {
+        // ERROR NAME IS NOT RANDOM WE WILL GET CONFLICTS!
+        path_ = std::filesystem::temp_directory_path() / "test_config_xxx.toml";
+
+        std::ofstream out(path_);
+        if (not out) {
+            throw std::runtime_error("Failed to create temp file at path: " + path_.string());
+        }
+        out << contents;
+    }
+
+    ~TempFile() { std::filesystem::remove(path_); }
+
+    std::filesystem::path Path() const { return path_; }
+
+   private:
+    std::filesystem::path path_;
+};
+
 TEST(ConfigLoadConfiguration, TestLoadConfiguration) {
-    // WARN(Jack): If you make a change to the config you need to regenerate the cmake!
-    //
-    // This is the classic "where is my test actually running from", and what is its absolute path problem, which makes
-    // it tricky to load files from the host system in a generic way. We solve this problem for the test_config.toml by
-    // copying it to the ${CMAKE_CURRENT_BINARY_DIR} during build configuration time. See the note in the cmakelists for
-    // more context.
-    std::string const config_file{"./test_config.toml"};
+    static constexpr std::string_view happy_path_config{R"(
+        [solver]
+        minimizer_type = "LINE_SEARCH"
+        min_line_search_step_size = 1e-6
+    )"};
+    TempFile const config_file{happy_path_config};
 
-    ceres::Solver::Options const config{config::LoadConfiguration(config_file)};
-
+    ceres::Solver::Options const config{config::LoadConfiguration(config_file.Path())};
     EXPECT_EQ(config.minimizer_type, ceres::LINE_SEARCH);
     EXPECT_EQ(config.min_line_search_step_size, 1e-6);
+}
+
+TEST(ConfigLoadConfiguration, TestLoadConfigurationEmptyConfig) {
+    static constexpr std::string_view happy_path_config{R"(
+        [some_other_config]
+        blah = 1
+    )"};
+    TempFile const config_file{happy_path_config};
+
+    // There is no [solver] config to load, so we will instead get a default initialized ceres::Solver::Options back :)
+    ceres::Solver::Options const config{config::LoadConfiguration(config_file.Path())};
+    EXPECT_EQ(config.minimizer_type, ceres::TRUST_REGION);
+    EXPECT_EQ(config.min_line_search_step_size, 1e-9);
 }
