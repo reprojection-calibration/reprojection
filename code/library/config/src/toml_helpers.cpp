00640e7c698be1fc07c60ff9877020c3c359b5ef
reprojection-calibration <reprojection.calibration@gmail.com>
2026-01-28 08:03:09 +0100

    split into files

---
+++ code/library/config/src/toml_helpers.cpp
@@ -0,0 +1,34 @@
+#include "toml_helpers.hpp"
+
+namespace reprojection::config {
+
+// TODO(Jack): I thought about using a variant here to return a bool or the ParseError, but std::optional code that idea
+//  of positive return or value in a simpler package. Is there a reason that we do need something more complicted than
+//  optional?
+// WARN(Jack): If for example you forgot the quotes around "TRUST_REGION" this code will
+// actually throw an uncontrolled error about misinterpreting a boolean value (because it begins with T). This is not
+// conform with our approach below and we should redesign this code to prevent that. Maybe we need to check the string
+// values first?
+// NOTE(Jack): It is valid to have more keys, this function only checks that certain required keys are present. If there
+// are more that is no problem.
+std::optional<ParseError> ValidateToml(toml::table const& table, std::map<std::string, DataType> const& required_keys) {
+    for (auto const& [key, type] : required_keys) {
+        if (auto const node{table.at_path(key)}) {
+            if ((type == DataType::Array and not node.is_array()) or
+                (type == DataType::FloatingPoint and not node.is_floating_point()) or
+                (type == DataType::Integer and not node.is_integer()) or
+                (type == DataType::String and not node.is_string()) or
+                (type == DataType::Table and not node.is_table())) {
+                return ParseError{ParseErrorType::IncorrectType,
+                                  "Configuration key: " + key + " is not of expected type: " + ToString(type)};
+            }
+        } else {
+            return ParseError{ParseErrorType::UnknownKey,
+                              "Configuration does not contain required key: " + key + " of type: " + ToString(type)};
+        }
+    }
+
+    return std::nullopt;
+}
+
+}  // namespace reprojection::config
---
+++ code/library/config/src/toml_helpers.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <optional>
+#include <string>
+#include <toml++/toml.hpp>
+
+// TODO MOVE TO TYPES
+namespace reprojection {
+
+enum class DataType {
+    Array,
+    FloatingPoint,
+    Integer,
+    String,
+    Table,
+};
+
+std::string ToString(DataType const value) {
+    if (value == DataType::Array) {
+        return "array";
+    } else if (value == DataType::FloatingPoint) {
+        return "floating_point";
+    } else if (value == DataType::Integer) {
+        return "integer";
+    } else if (value == DataType::String) {
+        return "string";
+    } else if (value == DataType::Table) {
+        return "table";
+    } else {
+        throw std::runtime_error("DataType enum ToString function has not implemented this type yet!");
+    }
+}
+
+enum class ParseErrorType {
+    IncorrectType,
+    UnknownKey,
+};
+
+struct ParseError {
+    ParseErrorType error;
+    std::string msg;
+};
+
+}  // namespace reprojection
+
+namespace reprojection::config {
+
+std::optional<ParseError> ValidateToml(toml::table const& table, std::map<std::string, DataType> const& required_keys);
+
+}  // namespace reprojection::config
