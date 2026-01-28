220aec83f0e1f5771368770bf7b8b2405f2f0fd1
reprojection-calibration <reprojection.calibration@gmail.com>
2026-01-28 07:49:57 +0100

    TestValidateTomlBadConfigs

--- code/library/config/src/toml_helpers.test.cpp
+++ code/library/config/src/toml_helpers.test.cpp
@@ -90,14 +90,38 @@ TEST(ConfigTomlHelpers, TestValidateTomlHappyPath) {
 
     // Example use case #1 - check that a config has specific table sections.
     std::map<std::string, DataType> const required_keys{{"target", DataType::Table}, {"solver", DataType::Table}};
-    auto const error_msg{config::ValidateToml(config, required_keys)};
-    EXPECT_FALSE(error_msg.has_value());
+    EXPECT_FALSE(config::ValidateToml(config, required_keys).has_value());
 
     // Example use case #2 - check that a config has specific keys.
     std::map<std::string, DataType> const required_keys_1{{"target.pattern_size", DataType::Array},
                                                           {"target.unit_dimension", DataType::FloatingPoint},
                                                           {"solver.num_threads", DataType::Integer},
                                                           {"solver.minimizer_type", DataType::String}};
-    auto const error_msg_1{config::ValidateToml(config, required_keys_1)};
-    EXPECT_FALSE(error_msg_1.has_value());
+    EXPECT_FALSE(config::ValidateToml(config, required_keys_1).has_value());
+}
+
+TEST(ConfigTomlHelpers, TestValidateTomlIncorrectType) {
+    static constexpr std::string_view config_file{R"(
+        # A data time is currently not supported so it will trigger an error for all requested type checking.
+        config_key = 1979-05-27T00:32:00.999999-07:00
+    )"sv};
+    toml::table const config{toml::parse(config_file)};
+
+    std::map<std::string, DataType> required_keys{{"config_key", DataType::Array}};
+    auto const error_msg{config::ValidateToml(config, required_keys)};
+    ASSERT_TRUE(error_msg.has_value());
+    EXPECT_EQ(error_msg->error, ParseErrorType::IncorrectType);
+    EXPECT_EQ(error_msg->msg, "Configuration key: config_key is not of expected type: array");
+
+    required_keys["config_key"] = DataType::FloatingPoint;
+    EXPECT_TRUE(config::ValidateToml(config, required_keys).has_value());
+
+    required_keys["config_key"] = DataType::Integer;
+    EXPECT_TRUE(config::ValidateToml(config, required_keys).has_value());
+
+    required_keys["config_key"] = DataType::String;
+    EXPECT_TRUE(config::ValidateToml(config, required_keys).has_value());
+
+    required_keys["config_key"] = DataType::Table;
+    EXPECT_TRUE(config::ValidateToml(config, required_keys).has_value());
 }
