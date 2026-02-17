fc0fe1058a3414af88dd3c1b3e683bca32ab60a0
reprojection-calibration <reprojection.calibration@gmail.com>
2026-02-17 16:05:33 +0100

    update testing

--- code/library/pnp/src/dlt.test.cpp
+++ code/library/pnp/src/dlt.test.cpp
@@ -12,40 +12,42 @@
 using namespace reprojection;
 
 TEST(PnpDlt, TestDlt23) {
-    CameraCalibrationData const data{testing_mocks::GenerateMvgData(
+    CameraCalibrationData const gt_data{testing_mocks::GenerateMvgData(
         20, 1e9, CameraModel::Pinhole, testing_utilities::pinhole_intrinsics, testing_utilities::image_bounds, false)};
 
-    for (auto const& [timestamp_ns, frame_i] : data.frames) {
-        auto const [tf, K]{pnp::Dlt23(frame_i.extracted_target.bundle)};
+    for (auto const& [timestamp_ns, frame_i] : gt_data.frames) {
+        auto const [tf_co_w, K]{pnp::Dlt23(frame_i.extracted_target.bundle)};
 
         // WARN(Jack): Unprotected optional access! Do we need a better strategy here? The mvg test data should
         // definitely have filled out this value!
-        Isometry3d const gt_pose_i{geometry::Exp(data.frames.at(timestamp_ns).initial_pose.value())};
+        Isometry3d const gt_tf_w_co{geometry::Exp(gt_data.frames.at(timestamp_ns).initial_pose.value())};
+        Isometry3d const gt_tf_co_w{gt_tf_w_co.inverse()};
 
-        EXPECT_TRUE(tf.isApprox(gt_pose_i)) << "Result:\n"
-                                            << tf.matrix() << "\nexpected result:\n"
-                                            << gt_pose_i.matrix();
-        EXPECT_FLOAT_EQ(tf.linear().determinant(), 1);  // Property of rotation matrix - positive one determinant
-        EXPECT_TRUE(K.isApprox(data.initial_intrinsics));
+        EXPECT_TRUE(tf_co_w.isApprox(gt_tf_co_w)) << "Result:\n"
+                                                  << tf_co_w.matrix() << "\nexpected result:\n"
+                                                  << gt_tf_co_w.matrix();
+        EXPECT_FLOAT_EQ(tf_co_w.linear().determinant(), 1);  // Property of rotation matrix - positive one determinant
+        EXPECT_TRUE(K.isApprox(gt_data.initial_intrinsics));
     }
 }
 
 TEST(PnpDlt, TestDlt22) {
     // Points must have Z=0 (flat = true) for Dlt22
-    CameraCalibrationData const data{testing_mocks::GenerateMvgData(20, 1e9, CameraModel::Pinhole,
-                                                                    testing_utilities::unit_pinhole_intrinsics,
-                                                                    testing_utilities::unit_image_bounds, true)};
+    CameraCalibrationData const gt_data{testing_mocks::GenerateMvgData(20, 1e9, CameraModel::Pinhole,
+                                                                       testing_utilities::unit_pinhole_intrinsics,
+                                                                       testing_utilities::unit_image_bounds, true)};
 
-    for (auto const& [timestamp_ns, frame_i] : data.frames) {
-        auto const tf{pnp::Dlt22(frame_i.extracted_target.bundle)};
+    for (auto const& [timestamp_ns, frame_i] : gt_data.frames) {
+        auto const tf_co_w{pnp::Dlt22(frame_i.extracted_target.bundle)};
 
         // WARN(Jack): Unprotected optional access! Do we need a better strategy here? The mvg test data should
         // definitely have filled out this value!
-        Isometry3d const gt_pose_i{geometry::Exp(data.frames.at(timestamp_ns).initial_pose.value())};
+        Isometry3d const gt_tf_w_co{geometry::Exp(gt_data.frames.at(timestamp_ns).initial_pose.value())};
+        Isometry3d const gt_tf_co_w{gt_tf_w_co.inverse()};
 
-        EXPECT_TRUE(tf.isApprox(gt_pose_i)) << "Result:\n"
-                                            << tf.matrix() << "\nexpected result:\n"
-                                            << gt_pose_i.matrix();
-        EXPECT_FLOAT_EQ(tf.linear().determinant(), 1);  // Property of rotation matrix - positive one determinant
+        EXPECT_TRUE(tf_co_w.isApprox(gt_tf_co_w)) << "Result:\n"
+                                                  << tf_co_w.matrix() << "\nexpected result:\n"
+                                                  << gt_tf_co_w.matrix();
+        EXPECT_FLOAT_EQ(tf_co_w.linear().determinant(), 1);  // Property of rotation matrix - positive one determinant
     }
 }
