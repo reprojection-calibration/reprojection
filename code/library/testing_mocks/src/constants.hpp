38ab00d0ca0d4dc3c680e31d9923ce3878adc463
reprojection-calibration <reprojection.calibration@gmail.com>
2026-02-18 09:35:59 +0100

    remove unused delta_t constant

--- code/library/testing_mocks/src/constants.hpp
+++ code/library/testing_mocks/src/constants.hpp
@@ -11,8 +11,6 @@ int const num_loops{4};
 
 // Starting time of the SE3 spline
 std::uint64_t const t0_ns{0};
-// Time increment between each control point (sphere trajectory pose) of the SE3 spline
-std::uint64_t const delta_t_ns{1000000};  // 1ms
 
 // WARN(Jack): This be getting a little hacky/globally, but hey it's at least const :) But the reason we need this is
 // because we need to have the same trajectory geometry for both the mvg_generator and imu_generator. If the best way to
