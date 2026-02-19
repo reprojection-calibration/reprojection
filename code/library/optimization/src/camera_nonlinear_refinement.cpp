cf09a68882923d8e1afa29c343076c69a1f5d9ad
reprojection-calibration <reprojection.calibration@gmail.com>
2026-02-19 17:47:45 +0100

    CameraNonlinearRefinement

--- code/library/optimization/src/camera_nonlinear_refinement.cpp
+++ code/library/optimization/src/camera_nonlinear_refinement.cpp
@@ -6,52 +6,37 @@
 
 namespace reprojection::optimization {
 
+// TODO(Jack): Confirm that OptimizationState initialization is a deep copy!
 // ERROR(Jack): What is a frame has too few valid pixels to actually constrain the pose? Should we entirely skip
 // that frame? Or what if in general we have a minimum required of points per frame threshold?
-CeresState CameraNonlinearRefinement(OptimizationDataView data_view) {
-    // Assemble all cost functions.
-    std::map<uint64_t, std::vector<std::unique_ptr<ceres::CostFunction>>> cost_functions;
-    for (OptimizationFrameView const& frame_i : data_view) {
-        MatrixX2d const& pixels_i{frame_i.extracted_target().bundle.pixels};
-        MatrixX3d const& points_i{frame_i.extracted_target().bundle.points};
-
-        for (Eigen::Index j{0}; j < pixels_i.rows(); ++j) {
-            cost_functions[frame_i.timestamp_ns()].emplace_back(
-                Create(data_view.camera_model(), data_view.image_bounds(), pixels_i.row(j), points_i.row(j)));
-        }
-    }
-
-    // Create the ceres problem and configure it - one day this will happen from a config file!
-    CeresState ceres_state{ceres::DO_NOT_TAKE_OWNERSHIP, ceres::DENSE_SCHUR};
+std::tuple<OptimizationState, CeresState> CameraNonlinearRefinement(CameraInfo const& sensor,
+                                                                    CameraMeasurements const& data,
+                                                                    OptimizationState const& initial_state) {
+    // Create the ceres problem and configure it - one day the ceres state will be set from a config file!
+    CeresState ceres_state{ceres::TAKE_OWNERSHIP, ceres::DENSE_SCHUR};
     ceres::Problem problem{ceres_state.problem_options};
 
-    // Calculate the reprojection errors before optimization and then add all cost functions to the problem.
-    for (OptimizationFrameView frame_i : data_view) {
-        auto const& cost_functions_i{cost_functions.at(frame_i.timestamp_ns())};
+    OptimizationState optimized_state{initial_state};
+    for (auto const& [timestamp_ns, target] : data) {
+        MatrixX2d const& pixels_i{target.bundle.pixels};
+        MatrixX3d const& points_i{target.bundle.points};
 
-        frame_i.initial_reprojection_error() =
-            EvaluateReprojectionResiduals(cost_functions_i, data_view.initial_intrinsics(), frame_i.initial_pose());
+        for (Eigen::Index j{0}; j < pixels_i.rows(); ++j) {
+            ceres::CostFunction* const cost_function{
+                Create(sensor.camera_model, sensor.bounds, pixels_i.row(j), points_i.row(j))};
 
-        for (auto const& cost_function : cost_functions_i) {
-            problem.AddResidualBlock(cost_function.get(), nullptr, data_view.optimized_intrinsics().data(),
-                                     frame_i.optimized_pose().value().data());
+            problem.AddResidualBlock(cost_function, nullptr, optimized_state.camera_state.intrinsics.data(),
+                                     optimized_state.frames.at(timestamp_ns).pose.data());
         }
     }
 
     // Optimize!!!
     ceres::Solve(ceres_state.solver_options, &problem, &ceres_state.solver_summary);
 
-    // Calculate the reprojection errors after optimization.
-    for (OptimizationFrameView frame_i : data_view) {
-        // cppcheck-suppress unreadVariable
-        frame_i.optimized_reprojection_error() =
-            EvaluateReprojectionResiduals(cost_functions.at(frame_i.timestamp_ns()), data_view.optimized_intrinsics(),
-                                          frame_i.optimized_pose().value());
-    }
-
-    return ceres_state;
+    return {optimized_state, ceres_state};
 }
 
+// TODO CALCULATE THE REPROJECTION ERROR OUTSIDE OF THE MAIN OPTIMIZATiON FUNCTION!!!!!
 ArrayX2d EvaluateReprojectionResiduals(std::vector<std::unique_ptr<ceres::CostFunction>> const& cost_functions,
                                        ArrayXd const& intrinsics, Array6d const& pose) {
     std::vector<double const*> parameter_blocks;
