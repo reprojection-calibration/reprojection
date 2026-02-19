bf2bb2d68d8116db5df84795277567a28b58f8e9
reprojection-calibration <reprojection.calibration@gmail.com>
2026-02-19 22:47:04 +0100

    noisy nonlinear opt

--- code/library/optimization/test/camera_nonlinear_refinement.test.cpp
+++ code/library/optimization/test/camera_nonlinear_refinement.test.cpp
@@ -43,20 +43,22 @@ TEST(OptimizationCameraNonlinearRefinement, TestCameraNonlinearRefinementBatch) 
 // Given a noisy initial pose but perfect bundle (i.e. no noise in the pixels or points), we then get perfect poses
 // and intrinsic back.
 TEST(OptimizationCameraNonlinearRefinement, TestNoisyCameraNonlinearRefinement) {
-    CameraCalibrationData const gt_data{testing_mocks::GenerateMvgData(
-        20, 1e9, CameraModel::Pinhole, testing_utilities::pinhole_intrinsics, testing_utilities::image_bounds, false)};
-    CameraCalibrationData data{gt_data};
+    CameraInfo const sensor{"", CameraModel::Pinhole, testing_utilities::image_bounds};
+    CameraState const gt_intrinsics{testing_utilities::pinhole_intrinsics};
+    auto const [targets, gt_frames]{testing_mocks::GenerateMvgData(sensor, gt_intrinsics, 50, 1e9, false)};
 
     // Add gaussian noise to the initial poses
-    for (auto& [_, frame_i] : data.frames) {
-        Isometry3d const SE3_i{geometry::Exp(frame_i.initial_pose.value())};
-        frame_i.initial_pose = geometry::Log(testing_mocks::AddGaussianNoise(0.5, 0.5, SE3_i));
+    Frames noisy_frames{gt_frames};
+    for (auto& [_, frame_i] : noisy_frames) {
+        Isometry3d const SE3_i{geometry::Exp(frame_i.pose)};
+        frame_i.pose = geometry::Log(testing_mocks::AddGaussianNoise(0.5, 0.5, SE3_i));
     }
 
-    optimization::CeresState const state{optimization::CameraNonlinearRefinement(OptimizationDataView(data))};
-    EXPECT_EQ(state.solver_summary.termination_type, ceres::TerminationType::CONVERGENCE);
+    OptimizationState const initial_state{gt_intrinsics, noisy_frames};
+    auto const [optimized_state, diagnostics]{optimization::CameraNonlinearRefinement(sensor, targets, initial_state)};
+    EXPECT_EQ(diagnostics.solver_summary.termination_type, ceres::TerminationType::CONVERGENCE);
 
-    for (auto const& [timestamp_ns, frame_i] : data.frames) {
+    for (auto const& [timestamp_ns, frame_i] : optimized_state.frames) {
         // WARN(Jack): Clearly I do not understand the axis-angle representation... And here something frustrating
         // happened that I will explain. This test using noisy poses had been working for months, no problems to report.
         // Comparing the Vector6d se3 poses directly worked perfectly and the optimization returned the ground truth
@@ -67,25 +69,18 @@ TEST(OptimizationCameraNonlinearRefinement, TestNoisyCameraNonlinearRefinement) 
         // test to instead compare the 4x4 SE3 transformation  matrices. Now it passes again, essentially the same as
         // before, but now working in the matrix space. Why all of a sudden the optimized poses start flipping, I cannot
         // explain.
-        // WARN(Jack): Unprotected optional access! Do we need a better strategy here? The mvg test data should
-        // definitely have filled out this value!
-        Isometry3d const gt_tf_co_w{geometry::Exp(gt_data.frames.at(timestamp_ns).initial_pose.value())};
-
-        ASSERT_TRUE(frame_i.optimized_pose.has_value());
-        Isometry3d const tf_co_w{geometry::Exp(frame_i.optimized_pose.value())};
+        Isometry3d const gt_tf_co_w{geometry::Exp(gt_frames.at(timestamp_ns).pose)};
+        Isometry3d const tf_co_w{geometry::Exp(frame_i.pose)};
 
         EXPECT_TRUE(tf_co_w.isApprox(gt_tf_co_w, 1e-6)) << "Result:\n"
                                                         << tf_co_w.matrix() << "\nexpected result:\n"
                                                         << gt_tf_co_w.matrix();
-
-        ASSERT_TRUE(frame_i.optimized_reprojection_error.has_value());
-        EXPECT_LT(frame_i.optimized_reprojection_error.value().mean(), 1e-6);
     }
 
-    EXPECT_TRUE(data.optimized_intrinsics.isApprox(gt_data.initial_intrinsics, 1e-6))
+    EXPECT_TRUE(optimized_state.camera_state.intrinsics.isApprox(gt_intrinsics.intrinsics, 1e-6))
         << "Result:\n"
-        << data.optimized_intrinsics << "\nexpected result:\n"
-        << gt_data.initial_intrinsics;
+        << optimized_state.camera_state.intrinsics.transpose() << "\nexpected result:\n"
+        << gt_intrinsics.intrinsics.transpose();
 }
 
 TEST(OptimizationCameraNonlinearRefinement, TestEvaluateReprojectionResiduals) {
