45cf82f24ff2305a7c76124eb1941fdc9ad82b12
reprojection-calibration <reprojection.calibration@gmail.com>
2026-02-19 07:48:32 +0100

    add InvertSe3

---
+++ code/python_tooling/tests/test_database_geometry.py
@@ -0,0 +1,24 @@
+import unittest
+
+import numpy as np
+
+from database.geometry import InvertSe3
+
+
+class TestDatabaseGeometry(unittest.TestCase):
+    def test_invert_se3(self):
+        se3 = np.array([0, 0, 0, 0, 0, 0])
+        se3_inv = InvertSe3(se3)
+        self.assertEqual(se3_inv, [0, 0, 0, 0, 0, 0])
+
+        se3 = np.array([0, 0, 0, 1, 2, 3])
+        se3_inv = InvertSe3(se3)
+        self.assertEqual(se3_inv, [0, 0, 0, -1, -2, -3])
+
+        se3 = np.array([np.pi / 2, 0, 0, 0, 0, 0])
+        se3_inv = InvertSe3(se3)
+        np.testing.assert_almost_equal(se3_inv, [-np.pi / 2.0, 0, 0, 0, 0, 0])
+
+        se3 = np.array([np.pi / 2, 0, 0, 1, 2, 3])
+        se3_inv = InvertSe3(se3)
+        np.testing.assert_almost_equal(se3_inv, [-np.pi / 2.0, 0, 0, -1, -3, 2])
