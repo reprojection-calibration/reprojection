5a020c20e444c13e4573d0ac4cbf7fce5105b640
reprojection-calibration <reprojection.calibration@gmail.com>
2026-02-20 10:03:43 +0100

    outdated comment

--- code/library/types/include/types/sensor_types.hpp
+++ code/library/types/include/types/sensor_types.hpp
@@ -4,10 +4,6 @@
 
 #include "types/algorithm_types.hpp"
 
-// NOTE(Jack): We use the word "sensor" to indicate the idea that at this point we are making the relationship between
-// data and a specific named data source and world timestamp concrete. These types should, in the best case, stay close
-// to the application logic periphery and not penetrate deep into the core code base.
-
 namespace reprojection {
 
 // TODO(Jack): Use concept to check that the first type for StampedMap is the proper expected stamp type.
