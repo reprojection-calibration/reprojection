20b80a5037f37e3bbf4b5c63e02e789914b801dc
reprojection-calibration <reprojection.calibration@gmail.com>
2026-02-21 12:40:13 +0100

    refactor into files

---
+++ code/library/spline/src/sparse_utilities.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <Eigen/SparseCore>
+
+#include "types/eigen_types.hpp"
+
+namespace reprojection::spline {
+
+// See section "Filling a sparse matrix" - https://libeigen.gitlab.io/eigen/docs-nightly/group__TutorialSparse.html
+Eigen::SparseMatrix<double> DiagonalSparseMatrix(MatrixXd const& block, int const stride, size_t const count);
+
+}  // namespace reprojection::spline
--- code/library/spline/src/sparse_utilities.test.cpp
+++ code/library/spline/src/sparse_utilities.test.cpp
@@ -1,39 +1,8 @@
+#include "sparse_utilities.hpp"
+
 #include <gtest/gtest.h>
 
-#include <Eigen/SparseCore>
-
 #include "types/eigen_types.hpp"
-
-namespace reprojection::spline {
-
-// See section "Filling a sparse matrix" - https://libeigen.gitlab.io/eigen/docs-nightly/group__TutorialSparse.html
-Eigen::SparseMatrix<double> DiagonalSparseMatrix(MatrixXd const& block, int const stride, size_t const count) {
-    if (not(block.rows() == block.cols())) {
-        throw std::runtime_error("Only accepts square blocks");
-    }
-
-    std::vector<Eigen::Triplet<double>> triplets;
-    triplets.reserve(block.size() * count);
-
-    for (size_t n{0}; n < count; n++) {
-        int const start_index{static_cast<int>(n * stride)};
-
-        for (Eigen::Index row{0}; row < block.rows(); row++) {
-            for (Eigen::Index col{0}; col < block.cols(); col++) {
-                triplets.push_back(
-                    {static_cast<int>(start_index + row), static_cast<int>(start_index + col), block(row, col)});
-            }
-        }
-    }
-
-    size_t const size{(block.rows() * count) + (stride - block.rows()) * (count - 1)};
-    Eigen::SparseMatrix<double> mat(size, size);
-    mat.setFromTriplets(std::cbegin(triplets), std::cend(triplets));
-
-    return mat;
-}
-
-}  // namespace reprojection::spline
 
 using namespace reprojection;
 
