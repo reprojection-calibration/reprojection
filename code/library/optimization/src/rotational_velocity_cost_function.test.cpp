0d9564a58ad602d7748516b7cef8dcb59bb84eb4
reprojection-calibration <reprojection.calibration@gmail.com>
2026-02-22 11:57:22 +0100

    TestRotationalVelocityCostFunction

--- code/library/optimization/src/rotational_velocity_cost_function.test.cpp
+++ code/library/optimization/src/rotational_velocity_cost_function.test.cpp
@@ -4,6 +4,20 @@
 
 using namespace reprojection;
 
+TEST(OptimizationProjectionCostFunction, TestRotationalVelocityCostFunction) {
+    Array3d const aa_a_b{0, 0, 0};
+    Array3d residual{-1, -1, -1};
+
+    Vector3d const omega_a{Vector3d::Zero()};
+    Vector3d const omega_b{Vector3d::Zero()};
+    optimization::RotationalVelocityCostFunction const cost_function{omega_a, omega_b};
+    bool const success{cost_function(aa_a_b.data(), residual.data())};
+    EXPECT_TRUE(success);
+    EXPECT_FLOAT_EQ(residual[0], 0.0);
+    EXPECT_FLOAT_EQ(residual[1], 0.0);
+    EXPECT_FLOAT_EQ(residual[2], 0.0);
+}
+
 TEST(OptimizationRotationalVelocityCostFunction, TestRotationalVelocityCostFunctionCreate) {
     Vector3d const omega_a{Vector3d::Zero()};
     Vector3d const omega_b{Vector3d::Zero()};
