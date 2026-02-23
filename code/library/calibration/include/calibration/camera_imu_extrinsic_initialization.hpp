8dd1b2257a7f61bd8338be4bf6687f9f63bf56eb
reprojection-calibration <reprojection.calibration@gmail.com>
2026-02-23 16:54:38 +0100

    document EstimateCameraImuRotation

--- code/library/calibration/include/calibration/camera_imu_extrinsic_initialization.hpp
+++ code/library/calibration/include/calibration/camera_imu_extrinsic_initialization.hpp
@@ -10,6 +10,18 @@ namespace reprojection::calibration {
 std::tuple<std::tuple<Matrix3d, CeresState>, Vector3d> EstimateCameraImuRotationAndGravity(
     Frames const& camera_poses, ImuMeasurements const& imu_data);
 
+/**
+ * \brief Estimate the approximate extrinsic rotation matrix between the IMU and camera optical frame (R_co_imu).
+ *
+ * Given the camera orientation spline we can differentiate it to get the camera's angular velocity (omega_co). Assuming
+ * zero translation between the camera and IMU we can then use optimization::AngularVelocityAlignment() to estimate the
+ * rotation matrix which aligns the camera's angular velocity to the IMU gyroscope angular velocity. Of course for any
+ * real sensor pair there is translation and this will introduce an error, but for an initialization this method is
+ * acceptable.
+ *
+ * Note that if not all axes of the camera-IMU have sufficient rotational velocity excitement then the returned solution
+ * will be degenerate.
+ */
 std::tuple<Matrix3d, CeresState> EstimateCameraImuRotation(spline::CubicBSplineC3 const& camera_orientation_spline,
                                                            ImuMeasurements const& imu_data);
 
@@ -22,7 +34,7 @@ std::tuple<Matrix3d, CeresState> EstimateCameraImuRotation(spline::CubicBSplineC
  *
  * This function takes the entire set of imu linear acceleration data and transforms it into the camera's world frame
  * using the camera_orientation_spline and R_imu_co, and then sums it up. The "zero mean acceleration" principle means
- * that the acceleration that is "left over" after the summing is the contribution from gravity. Of course in real world
+ * that the acceleration that is left over after the summing is the contribution from gravity. Of course in real world
  * data with noise and biases this will never be perfect, however, we can use this value to initialize the full
  * camera-imu extrinsic calibration.
  *
