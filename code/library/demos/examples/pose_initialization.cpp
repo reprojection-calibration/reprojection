aa7d92fea2cb4812893bac2ab1a029e7f669ebcb
reprojection-calibration <reprojection.calibration@gmail.com>
2026-02-23 07:18:49 +0100

    keys view

--- code/library/demos/examples/pose_initialization.cpp
+++ code/library/demos/examples/pose_initialization.cpp
@@ -22,9 +22,13 @@ using namespace reprojection;
 // WARN(Jack): This is a hack that we need to do so that the spline initialization does not have any massive
 // discontinuties or sudden jumps. But there is some bigger problem here that we are missing and need to solve long
 // term.
+// WARN(Jack): Also note that we do not save aligned_initial_state to the database, we save plain old initial_state and
+// use that to calculate the reprojection errors, but use aligned_initial_state to initialize the nonlinear
+// optimization. This means that what we are doing here and what we are visualizing in the database are starting to
+// diverge. Not nice!
 OptimizationState AlignRotations(OptimizationState state) {
     Vector3d so3_i_1{std::cbegin(state.frames)->second.pose.topRows(3)};
-    for (auto& [timestamp_ns, frame_i] : state.frames) {
+    for (auto& frame_i : state.frames | std::views::values) {
         Vector3d so3_i{frame_i.pose.topRows(3)};
         double const dp{so3_i_1.dot(so3_i)};
 
