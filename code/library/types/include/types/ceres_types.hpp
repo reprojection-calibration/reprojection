a6f36b09f86c9551796b6dfef90c5ba3f4616ce0
reprojection-calibration <reprojection.calibration@gmail.com>
2026-02-24 12:23:57 +0100

    add     CeresState() = default;

--- code/library/types/include/types/ceres_types.hpp
+++ code/library/types/include/types/ceres_types.hpp
@@ -6,6 +6,8 @@
 namespace reprojection {
 
 struct CeresState {
+    CeresState() = default;
+
     CeresState(ceres::Ownership const ownership, ceres::LinearSolverType const linear_solver) {
         problem_options.cost_function_ownership = ownership;
         solver_options.linear_solver_type = linear_solver;
