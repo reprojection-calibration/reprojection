ec9fde455b7d3f01a30ace93545fb7953ac29b0f
reprojection-calibration <reprojection.calibration@gmail.com>
2026-02-24 21:17:39 +0100

    TestInitializeSpline

--- code/library/spline/test/spline_initialization.test.cpp
+++ code/library/spline/test/spline_initialization.test.cpp
@@ -29,9 +29,9 @@ TEST(SplineSplineInitialization, TestInitializeSpline) {
     // NOTE(Jack): At this point this and below are canary in the coal mine tests, to make sure nothing changes as we
     // refactor. An unsolved problem is the time handling, and this is the reason why these values are not exact values
     // on the integers, which given the test data they should be.
-    EXPECT_TRUE(one_segment_spline.control_points[0].isApprox(
+    EXPECT_TRUE(one_segment_spline.ControlPoints().col(0).isApprox(
         Vector3d{-2.0352118285642522, -2.0352118285642522, -2.0352118285642522}));
-    EXPECT_TRUE(one_segment_spline.control_points[3].isApprox(
+    EXPECT_TRUE(one_segment_spline.ControlPoints().col(3).isApprox(
         Vector3d{4.0556875027223356, 4.0556875027223356, 4.0556875027223356}));
 
     CubicBSplineC3 const two_segment_spline{spline::InitializeC3Spline(measurements, 2)};
@@ -39,8 +39,8 @@ TEST(SplineSplineInitialization, TestInitializeSpline) {
     EXPECT_EQ(two_segment_spline.time_handler_.delta_t_ns_, 100);
     EXPECT_EQ(two_segment_spline.Size(), 5);
     // See note above on canary coal mine.
-    EXPECT_TRUE(two_segment_spline.control_points[0].isApprox(
+    EXPECT_TRUE(two_segment_spline.ControlPoints().col(0).isApprox(
         Vector3d{-1.0133795604206075, -1.0133795604206075, -1.0133795604206075}));
-    EXPECT_TRUE(two_segment_spline.control_points[4].isApprox(
+    EXPECT_TRUE(two_segment_spline.ControlPoints().col(4).isApprox(
         Vector3d{3.0268874400439727, 3.0268874400439727, 3.0268874400439727}, 1e-6));
 }
