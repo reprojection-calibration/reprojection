9683e26c445e2b53772967f485e1d5ef2446bd4d
reprojection-calibration <reprojection.calibration@gmail.com>
2026-02-24 13:51:18 +0100

    add SplineProjectionCostFunction_T

---
+++ code/library/optimization/src/spline_projection_cost_function.test.cpp
@@ -0,0 +1,7 @@
+#include "spline_projection_cost_function.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace reprojection;
+
+TEST(OptimizationSplineProjectionCostFunction, TestXxxx) { EXPECT_EQ(1, 2); }
