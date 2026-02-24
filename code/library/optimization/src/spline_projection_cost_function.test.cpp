8b46fa72f2584439e789707e195c70b3b89d5704
reprojection-calibration <reprojection.calibration@gmail.com>
2026-02-24 14:47:56 +0100

    TestSplineProjectionCostFunction_T

--- code/library/optimization/src/spline_projection_cost_function.test.cpp
+++ code/library/optimization/src/spline_projection_cost_function.test.cpp
@@ -7,6 +7,22 @@
 
 using namespace reprojection;
 
+TEST(OptimizationSplineProjectionCostFunction, TestSplineProjectionCostFunction_T) {
+    Array2d const pixel{testing_utilities::pinhole_intrinsics[2], testing_utilities::pinhole_intrinsics[3]};
+    Array3d const point{0, 0, 10};
+    optimization::SplineProjectionCostFunction_T<projection_functions::Pinhole> const cost_function{
+        pixel, point, testing_utilities::image_bounds, 0, 1};
+
+    // Test with four of the same identity control points. This should give us an identity SE3 pose.
+    Array6d const control_point{Array6d::Zero()};
+    Array2d residual{-1, -1};
+    bool success{cost_function(testing_utilities::pinhole_intrinsics.data(), control_point.data(), control_point.data(),
+                               control_point.data(), control_point.data(), residual.data())};
+    EXPECT_TRUE(success);
+    EXPECT_FLOAT_EQ(residual[0], 0.0);
+    EXPECT_FLOAT_EQ(residual[1], 0.0);
+}
+
 TEST(OptimizationSplineProjectionCostFunction, TestSplineProjectionCostFunction_TCreate) {
     Array2d const pixel{360, 240};
     Array3d const point{0, 0, 600};
