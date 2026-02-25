b220ab15b25f61fdb41fa53ab6282108498e8903
reprojection-calibration <reprojection.calibration@gmail.com>
2026-02-25 09:44:51 +0100

    TestSplineStateHelperMethods

--- code/library/spline/test/spline_state.test.cpp
+++ code/library/spline/test/spline_state.test.cpp
@@ -18,5 +18,22 @@ TEST(SplineSplineState, TestSplineStateConstruction) {
     std::vector<Vector3d> const control_points_vector{{0, 0, 0}, {1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
     spline::CubicBSplineC3 const vector_constructed{control_points_vector, time_handler};
     EXPECT_TRUE(vector_constructed.ControlPoints().isApprox(control_points_matrix));
+}
 
+TEST(SplineSplineState, TestSplineStateHelperMethods) {
+    spline::TimeHandler const time_handler{100, 5};
+    spline::MatrixNXd const control_points_matrix{
+        {0, 1, 2, 3},  //
+        {0, 1, 2, 3},
+        {0, 1, 2, 3},
+    };
+    spline::CubicBSplineC3 const spline{control_points_matrix, time_handler};
+
+    EXPECT_EQ(spline.DeltaTNs(), time_handler.delta_t_ns_);
+    EXPECT_EQ(spline.Size(), control_points_matrix.cols());
+
+    // Only one segment in the spline.
+    EXPECT_TRUE(spline.Position(100));
+    EXPECT_TRUE(spline.Position(104));
+    EXPECT_FALSE(spline.Position(105));
 }
