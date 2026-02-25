dae5cddd45352f48ab8877e95abd65e39e96acf9
reprojection-calibration <reprojection.calibration@gmail.com>
2026-02-25 07:51:20 +0100

    reorg types

--- code/library/spline/include/spline/types.hpp
+++ code/library/spline/include/spline/types.hpp
@@ -5,21 +5,21 @@
 
 namespace reprojection::spline {
 
-// Holds the full se3 state which is then split up into two MatrixNXd matrices as it is passed to the separate rotation
-// and translation splines.
-using Matrix2NXd = Eigen::Matrix<double, 2 * constants::states, Eigen::Dynamic>;
-
+// NOTE(Jack): Technically we could also just use Matrix6Xd (combined se3 spline) or Eigen::Matrix3Xd (so3 and r3
+// splines), but I want to try to communicate that we are actually working with the state here of size N, which for our
+// case is length 3 for both splines. Maybe this is dumb and I regret it one day.
 // NOTE(Jack): We need to provide these templated types so that we can also use them for the parts where we need
 // to handle ceres autodiff Jet types too.
+
+// Matrix2NXd the full se3 state (size = 2 * constants::states) which is then split up into two MatrixNXd matrices as it
+// is passed to the separate rotation and translation splines.
+using Matrix2NXd = Eigen::Matrix<double, 2 * constants::states, Eigen::Dynamic>;
+using MatrixKd = Eigen::Matrix<double, constants::order, constants::order>;
 template <typename T>
 using MatrixNK = Eigen::Matrix<T, constants::states, constants::order>;
 using MatrixNKd = MatrixNK<double>;
-
-// TODO(Jack): Technically we could also just use Eigen::MatrixNXd, but I want to try to communicate that we are
-//  actually working with the state here of size N, which for our case is length 3 for both splines. Maybe this is
-//  dumb.
 using MatrixNXd = Eigen::Matrix<double, constants::states, Eigen::Dynamic>;
-using MatrixKd = Eigen::Matrix<double, constants::order, constants::order>;
+
 using VectorKd = Eigen::Vector<double, constants::order>;
 
 // For both the r3 and so3 spline we can evaluate either the position, velocity, or acceleration.
