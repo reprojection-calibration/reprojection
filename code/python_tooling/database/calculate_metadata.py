ad721a01398b2377989f9345bdd87eb2b2754846
reprojection-calibration <reprojection.calibration@gmail.com>
2026-02-28 08:36:31 +0100

    metadata

--- code/python_tooling/database/calculate_metadata.py
+++ code/python_tooling/database/calculate_metadata.py
@@ -1,8 +1,30 @@
-def count_structure(d):
+def traverse_timeseries_data(d, apply):
     if not isinstance(d, dict):
         return None
 
     if d and all(isinstance(k, int) for k in d.keys()):
-        return len(d)
+        return apply(d)
 
-    return {k: count_structure(v) for k, v in d.items()}
+    return {k: traverse_timeseries_data(v, apply) for k, v in d.items()}
+
+
+def count_data(d):
+    def process(timestamped_dict):
+        return len(timestamped_dict)
+
+    return traverse_timeseries_data(d, process)
+
+
+# NOTE(Jack): We store the reference timestamps as strings because otherwise dash, when it json serializes this data on
+# its way to the browse, will itself cast it to a string. During that process, because we have really large ints here we
+# loose some precision which makes correspondence impossible. Therefore, we preempt this mess by ourselves storing the
+# the timestamps as strings directly.
+def reference_timestamps(d):
+    def process(timestamped_dict):
+        timestamps = list(timestamped_dict.keys())
+        timestamps = sorted(timestamps)
+        timestamps = [str(t) for t in timestamps]
+
+        return timestamps
+
+    return traverse_timeseries_data(d, process)
--- code/python_tooling/tests/database/test_calculate_metadata.py
+++ code/python_tooling/tests/database/test_calculate_metadata.py
@@ -1,12 +1,10 @@
 import os
-
 import unittest
 
+from database.calculate_metadata import count_data, reference_timestamps
 from database.data_formatting import (
     load_data,
 )
-
-from database.calculate_metadata import count_structure
 from database.types import SensorType
 
 
@@ -17,12 +15,12 @@ class TestDatabaseCalculateMetadata(unittest.TestCase):
             "DB_PATH", "/temporary/code/test_data/dataset-calib-imu4_512_16.db3"
         )
 
-    def test_count_structure(self):
-        metadata = count_structure(None)
+    def test_count_data(self):
+        metadata = count_data(None)
         self.assertIsNone(metadata)
 
         data = load_data(self.db_path)
-        metadata = count_structure(data)
+        metadata = count_data(data)
 
         gt_metadata = {
             SensorType.Camera: {
@@ -33,3 +31,18 @@ class TestDatabaseCalculateMetadata(unittest.TestCase):
         }
 
         self.assertEqual(metadata, gt_metadata)
+
+    def test_reference_timestamps(self):
+        metadata = reference_timestamps(None)
+        self.assertIsNone(metadata)
+
+        data = load_data(self.db_path)
+        metadata = reference_timestamps(data)
+
+        # Check arbitrarily that its sorted and the first and last timestamp of one of the entries
+        cam0_image_timestamps = metadata[SensorType.Camera]["/cam0/image_raw"][
+            "measurements"
+        ]["images"]
+        self.assertEqual(cam0_image_timestamps, sorted(cam0_image_timestamps))
+        self.assertEqual(cam0_image_timestamps[0], "1520528314264184064")
+        self.assertEqual(cam0_image_timestamps[-1], "1520528358165555968")
