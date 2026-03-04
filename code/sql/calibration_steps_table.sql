fb68fc274a14d0a9b57b1bd2ee2859e82b1946d7
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-04 11:57:46 +0100

    add focal_length_initialization step

--- code/sql/calibration_steps_table.sql
+++ code/sql/calibration_steps_table.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS calibration_steps
 (
-    step_name  TEXT PRIMARY KEY CHECK ( step_name IN ('linear_pose_initialization',
+    step_name  TEXT PRIMARY KEY CHECK ( step_name IN ('focal_length_initialization',
+                                                      'linear_pose_initialization',
                                                       'nonlinear_refinement',
                                                       'spline_initialization',
                                                       'spline_nonlinear_refinement')),
+    cache_key  TEXT, -- TODO(jack): Can we make this have a not null constraint?
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
