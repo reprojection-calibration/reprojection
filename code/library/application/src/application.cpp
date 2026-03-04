5c02603d47c7e0e9f52baad923f39b84666499bb
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-04 19:51:45 +0100

    load poses for lpi

--- code/library/application/src/application.cpp
+++ code/library/application/src/application.cpp
@@ -17,13 +17,15 @@ std::pair<CacheStatus, CameraState> FocalLengthInitialization(
 
     // TODO(Jack): This logic of handling the step table cache key update should be generic for all cases!
     if (status == CacheStatus::StepNameMiss or status == CacheStatus::CacheKeyMiss) {
+        std::cout << "calculate the focal length initialization! - YOU STILL NEED TO IMPLEMENT THIS!!!" << std::endl;
+
+
         if (status == CacheStatus::StepNameMiss) {
             database::AddCalibrationStep(step_name, cache_key, database);
         } else {
             std::cout << "cache key update - YOU STILL NEED TO IMPLEMENT THIS!!!" << std::endl;
         }
 
-        std::cout << "calculate the focal length initialization! - YOU STILL NEED TO IMPLEMENT THIS!!!" << std::endl;
 
         // ERROR WE JUST RETURN BLANK!
         return {status, {}};
@@ -48,17 +50,16 @@ std::pair<CacheStatus, OptimizationState> LinearPoseInitialization(
     CacheStatus const status{CacheState(database, step_name, cache_key)};
 
     if (status == CacheStatus::StepNameMiss or status == CacheStatus::CacheKeyMiss) {
+        auto const initial_state{calibration::LinearPoseInitialization(camera_info, targets, camera_state)};
+        ReprojectionErrors const initial_error{
+            optimization::ReprojectionResiduals(camera_info, targets, initial_state)};
+
         if (status == CacheStatus::StepNameMiss) {
             database::AddCalibrationStep(step_name, cache_key, database);
         } else {
             std::cout << "cache key update - YOU STILL NEED TO IMPLEMENT THIS!!!" << std::endl;
             exit(1);
         }
-
-        auto const initial_state{calibration::LinearPoseInitialization(camera_info, targets, camera_state)};
-        ReprojectionErrors const initial_error{
-            optimization::ReprojectionResiduals(camera_info, targets, initial_state)};
-
         database::AddPoseData(initial_state.frames, "linear_pose_initialization", camera_info.sensor_name, database);
         database::AddReprojectionError(initial_error, "linear_pose_initialization", camera_info.sensor_name, database);
 
@@ -68,9 +69,9 @@ std::pair<CacheStatus, OptimizationState> LinearPoseInitialization(
         // TODO(Jack): Theoretically we could also load the intrinsics, but as they are passed in via camera_state we
         //  will just use those instead. Does that violate the principle of least suprise? I mean the
         //  calibration::LinearPoseInitialization also just copies camera_state from the input.
+        Frames const frames{database::GetPoses(database, step_name, camera_info.sensor_name)};
 
-        // TODO LOAD POSES AND BUILD OPTIMIZAZTIONSTATE!!!
-        return {status, {}};
+        return {status, {camera_state, frames}};
     } else {
         throw std::runtime_error("Library implementation error - unknown CacheStatus!!!!");
     }
