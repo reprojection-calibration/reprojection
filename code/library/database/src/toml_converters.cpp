c2a8798b0756c5bb7e9122a769e9dd721cb8496c
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-04 17:02:55 +0100

    add pinhole to toml converters

--- code/library/database/src/toml_converters.cpp
+++ code/library/database/src/toml_converters.cpp
@@ -1,6 +1,6 @@
-#include <toml++/toml.hpp>
-
 #include "toml_converters.hpp"
+
+#include <toml++/toml.hpp>
 
 namespace reprojection::database {
 // TODO(Jack): Add error handling if for example the length of intrinsics does not match the length expected for that
@@ -11,6 +11,8 @@ std::string ToToml(CameraModel const type, ArrayXd const& intrinsics) {
     if (type == CameraModel::DoubleSphere) {
         tbl = toml::table{{"fx", intrinsics[0]}, {"fy", intrinsics[1]}, {"cx", intrinsics[2]},
                           {"cy", intrinsics[3]}, {"xi", intrinsics[4]}, {"alpha", intrinsics[5]}};
+    } else if (type == CameraModel::Pinhole) {
+        tbl = toml::table{{"fx", intrinsics[0]}, {"fy", intrinsics[1]}, {"cx", intrinsics[2]}, {"cy", intrinsics[3]}};
     } else {
         throw std::runtime_error("Implement ToToml for other types!");
     }
@@ -30,6 +32,14 @@ ArrayXd FromToml(CameraModel const type, std::string const& toml_str) {
         intrinsics[3] = tbl["cy"].value<double>().value();
         intrinsics[4] = tbl["xi"].value<double>().value();
         intrinsics[5] = tbl["alpha"].value<double>().value();
+        return intrinsics;
+    } else if (type == CameraModel::Pinhole) {
+        auto tbl = toml::parse(toml_str);
+        ArrayXd intrinsics(4);
+        intrinsics[0] = tbl["fx"].value<double>().value();
+        intrinsics[1] = tbl["fy"].value<double>().value();
+        intrinsics[2] = tbl["cx"].value<double>().value();
+        intrinsics[3] = tbl["cy"].value<double>().value();
         return intrinsics;
     } else {
         throw std::runtime_error("Implement FromToml for other types!");
