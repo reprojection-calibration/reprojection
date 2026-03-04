ea1e8cd95780d68de7a12a3a86b30f487fe2a49e
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-04 17:19:05 +0100

    TEST(ApplicationApplication, TestFocalLengthInitialization)

--- code/library/application/test/application.test.cpp
+++ code/library/application/test/application.test.cpp
@@ -14,38 +14,53 @@ namespace reprojection::application {
 
 enum class CacheStatus {
     CacheHit,
-    CacheKeyCacheMiss,
-    StepNameCacheMiss,
+    CacheKeyMiss,
+    StepNameMiss,
 };
 
 // TODO NAMING!!!!
+
+CacheStatus CacheState(std::shared_ptr<database::CalibrationDatabase const> const database, std::string_view step_name,
+                       std::string_view cache_key) {
+    auto const db_cache_key{database::GetCacheKey(database, step_name)};
+
+    if (not db_cache_key) {
+        return CacheStatus::StepNameMiss;
+    } else if (db_cache_key.value() == cache_key) {
+        return CacheStatus::CacheHit;
+    } else {
+        return CacheStatus::CacheKeyMiss;
+    }
+}
 
 // TODO(Jack): Right now there are two cache miss categories with similar logic! Eliminate the repetition!
 std::pair<CacheStatus, CameraState> FocalLengthInitialization(
     CameraMeasurements const& targets, CameraInfo const& camera_info,
     std::shared_ptr<database::CalibrationDatabase> const database) {
-    auto const existing_cache_key{database::GetCacheKey(database, "focal_length_initialization")};
+    std::string const cache_key{caching::CacheKey(camera_info, targets)};
+    CacheStatus const status{CacheState(database, "focal_length_initialization", cache_key)};
 
-    if (not existing_cache_key.has_value()) {
-        std::cout << "step_name cache miss - YOU STILL NEED TO IMPLEMENT THIS!!!" << std::endl;
-        // here we will make a new calibration step entry
-        return {CacheStatus::StepNameCacheMiss, {}};
-    }
+    if (status == CacheStatus::StepNameMiss or status == CacheStatus::CacheKeyMiss) {
+        std::cout << "cache miss - YOU STILL NEED TO IMPLEMENT THIS!!!" << std::endl;
 
-    std::string const new_cache_key{caching::CacheKey(camera_info, targets)};
-    if (new_cache_key == existing_cache_key) {
+        if (status == CacheStatus::StepNameMiss) {
+            database::AddCalibrationStep("focal_length_initialization", cache_key, database);
+        } else {
+            // TODO(Jack): Update the key of step with the new key!
+            std::cout << "cache key update - YOU STILL NEED TO IMPLEMENT THIS!!!" << std::endl;
+        }
+
+        return {status, {}};
+    } else if (status == CacheStatus::CacheHit) {
         auto const result{GetIntrinsic(database, "focal_length_initialization", camera_info.sensor_name)};
         if (not result.has_value()) {
             throw std::runtime_error("We need a real error handling strategy here!");
         }
-
         auto const [_, camera_state]{result.value()};
 
-        return {CacheStatus::CacheHit, camera_state};
+        return {status, camera_state};
     } else {
-        std::cout << "key cache miss - YOU STILL NEED TO IMPLEMENT THIS!!!" << std::endl;
-        // here we will update an existing calibration step entry
-        return {CacheStatus::CacheKeyCacheMiss, {}};
+        throw std::runtime_error("Library implementation error - unknown CacheStatus!!!!");
     }
 }
 
@@ -56,14 +71,35 @@ using namespace reprojection;
 TEST(ApplicationApplication, TestFocalLengthInitialization) {
     auto db{std::make_shared<database::CalibrationDatabase>(":memory:", true, false)};
 
-    std::string const sensor_name{"/cam/retro/123"};
-    uint64_t const timestamp_ns{0};
-    CameraMeasurements const camera_measurements{{timestamp_ns, {}}};
-    CameraInfo const camera_info{sensor_name, CameraModel::Pinhole, testing_utilities::image_bounds};
+    CameraMeasurements const camera_measurements{{0, {}}};
+    CameraInfo const camera_info{"/cam/retro/123", CameraModel::Pinhole, testing_utilities::image_bounds};
+    CameraState const gt_camera_state{testing_utilities::pinhole_intrinsics};
 
-    auto const [cache_status, intrinsics]{application::FocalLengthInitialization(camera_measurements, camera_info, db)};
+    // Artificially construct a representative calibration step entry with corresponding cache key.
+    std::string const cache_key{caching::CacheKey(camera_info, camera_measurements)};
+    database::AddCalibrationStep("focal_length_initialization", cache_key, db);
+    AddIntrinsics(camera_info, gt_camera_state, "focal_length_initialization", db);
 
-    EXPECT_EQ(cache_status, application::CacheStatus::StepNameCacheMiss);
+    auto const [cache_status,
+                camera_state]{application::FocalLengthInitialization(camera_measurements, camera_info, db)};
+    EXPECT_EQ(cache_status, application::CacheStatus::CacheHit);
+    EXPECT_TRUE(camera_state.intrinsics.isApprox(gt_camera_state.intrinsics));
+}
 
-    EXPECT_EQ(1, 2);
+// NOTE(Jack): We use local namespaces and "in memory" databases to achieve complete isolation between the tests cases.
+TEST(ApplicationApplication, TestFocalLengthInitializationCacheMisses) {
+    CameraMeasurements const camera_measurements{{0, {}}};
+    CameraInfo const camera_info{"/cam/retro/123", CameraModel::Pinhole, testing_utilities::image_bounds};
+    {
+        auto db{std::make_shared<database::CalibrationDatabase>(":memory:", true, false)};
+        auto const [cache_status, _]{application::FocalLengthInitialization(camera_measurements, camera_info, db)};
+        EXPECT_EQ(cache_status, application::CacheStatus::StepNameMiss);
+    }
+    {
+        auto db{std::make_shared<database::CalibrationDatabase>(":memory:", true, false)};
+        database::AddCalibrationStep("focal_length_initialization", "nonexistent_cache_key", db);
+
+        auto const [cache_status, _]{application::FocalLengthInitialization(camera_measurements, camera_info, db)};
+        EXPECT_EQ(cache_status, application::CacheStatus::CacheKeyMiss);
+    }
 }
