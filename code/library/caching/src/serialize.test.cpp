8136eafd8176739c75cd1743c169356531e3f9f3
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-05 12:48:22 +0100

    refactor to use test fixture

--- code/library/caching/src/serialize.test.cpp
+++ code/library/caching/src/serialize.test.cpp
@@ -6,22 +6,27 @@
 
 using namespace reprojection;
 
-TEST(CachingSerialize, CameraInfo) {
-    CameraInfo const data{"/cam/retro/123", CameraModel::DoubleSphere, testing_utilities::image_bounds};
+// TODO(Jack): Fixture is copy and pasted
+class CachingFixture : public ::testing::Test {
+   protected:
+    CameraInfo camera_info{"/cam/retro/123", CameraModel::Pinhole, testing_utilities::image_bounds};
+    ExtractedTarget target{
+        Bundle{MatrixX2d{{1.23, 1.43}, {2.75, 2.35}}, MatrixX3d{{3.25, 3.45, 5.43}, {6.18, 6.78, 4.56}}},
+        {{5, 6}, {2, 3}}};
+    CameraMeasurements camera_measurements{{0, target}, {1, target}};
+    CameraState camera_state{testing_utilities::pinhole_intrinsics};
+    Frames frames{{0, {Array6d::Ones()}}, {1, {2 * Array6d::Ones()}}};
+};
 
-    std::string const result{caching::Serialize(data)};
-    std::string const gt_result{"/cam/retro/123|double_sphere|0.000,720.000,0.000,480.000|"};
+TEST_F(CachingFixture, CameraInfo) {
+    std::string const result{caching::Serialize(camera_info)};
+    std::string const gt_result{"/cam/retro/123|pinhole|0.000,720.000,0.000,480.000|"};
 
     EXPECT_EQ(result, gt_result);
 }
 
-TEST(CachingSerialize, CameraMeasurements) {
-    ExtractedTarget const target{
-        Bundle{MatrixX2d{{1.23, 1.43}, {2.75, 2.35}}, MatrixX3d{{3.25, 3.45, 5.43}, {6.18, 6.78, 4.56}}},
-        {{5, 6}, {2, 3}}};
-    CameraMeasurements const data{{0, target}, {1, target}};
-
-    std::string const result{caching::Serialize(data)};
+TEST_F(CachingFixture, CameraMeasurements) {
+    std::string const result{caching::Serialize(camera_measurements)};
     std::string const gt_result{
         "0|1.230,1.430;2.750,2.350;|3.250,3.450,5.430;6.180,6.780,4.560;|5,6;2,3;|1|1.230,1.430;2.750,2.350;|3.250,3."
         "450,5.430;6.180,6.780,4.560;|5,6;2,3;|"};
@@ -29,18 +34,14 @@ TEST(CachingSerialize, CameraMeasurements) {
     EXPECT_EQ(result, gt_result);
 }
 
-TEST(CachingSerialize, CameraState) {
-    CameraState const camera_state{testing_utilities::pinhole_intrinsics};
-
+TEST_F(CachingFixture, CameraState) {
     std::string const result{caching::Serialize(camera_state)};
     std::string const gt_result{"600.000;600.000;360.000;240.000;|"};
 
     EXPECT_EQ(result, gt_result);
 }
 
-TEST(CachingSerialize, Frames) {
-    Frames const frames{{0, {Array6d::Ones()}}, {1, {2*Array6d::Ones()}}};
-
+TEST_F(CachingFixture, Frames) {
     std::string const result{caching::Serialize(frames)};
     std::string const gt_result{"0|1.000;1.000;1.000;1.000;1.000;1.000;|1|2.000;2.000;2.000;2.000;2.000;2.000;|"};
 
--- code/library/caching/test/cache_keys.test.cpp
+++ code/library/caching/test/cache_keys.test.cpp
@@ -6,46 +6,36 @@
 
 using namespace reprojection;
 
-TEST(CachingCacheKeys, FocalLengthInitialization) {
-    CameraInfo const camera_info{"/cam/retro/123", CameraModel::DoubleSphere, testing_utilities::image_bounds};
-    ExtractedTarget const target{
+// TODO(Jack): Fixture is copy and pasted
+class CachingFixture : public ::testing::Test {
+   protected:
+    CameraInfo camera_info{"/cam/retro/123", CameraModel::Pinhole, testing_utilities::image_bounds};
+    ExtractedTarget target{
         Bundle{MatrixX2d{{1.23, 1.43}, {2.75, 2.35}}, MatrixX3d{{3.25, 3.45, 5.43}, {6.18, 6.78, 4.56}}},
         {{5, 6}, {2, 3}}};
-    CameraMeasurements const camera_measurements{{0, target}, {1, target}};
+    CameraMeasurements camera_measurements{{0, target}, {1, target}};
+    CameraState camera_state{testing_utilities::pinhole_intrinsics};
+    Frames frames{{0, {Array6d::Ones()}}, {1, {2 * Array6d::Ones()}}};
+    OptimizationState optimization_state{camera_state, frames};
+};
 
+TEST_F(CachingFixture, FocalLengthInitialization) {
     std::string const result{caching::CacheKey(camera_info, camera_measurements)};
-    std::string const gt_result{"696a97934101f9d594dd9645ef1d68455920eca08c8b9b940ef5e77b7e984b51"};
+    std::string const gt_result{"f4fa69df1a139cf559ab0e423312ecbcb1afe1fff9fffe0782da63a7c22e1b51"};
 
     EXPECT_EQ(result, gt_result);
 }
 
-TEST(CachingCacheKeys, LinearPoseInitialization) {
-    CameraInfo const camera_info{"/cam/retro/123", CameraModel::DoubleSphere, testing_utilities::image_bounds};
-    ExtractedTarget const target{
-        Bundle{MatrixX2d{{1.23, 1.43}, {2.75, 2.35}}, MatrixX3d{{3.25, 3.45, 5.43}, {6.18, 6.78, 4.56}}},
-        {{5, 6}, {2, 3}}};
-    CameraMeasurements const camera_measurements{{0, target}, {1, target}};
-    CameraState const camera_state{testing_utilities::pinhole_intrinsics};
-
+TEST_F(CachingFixture, LinearPoseInitialization) {
     std::string const result{caching::CacheKey(camera_info, camera_measurements, camera_state)};
-    std::string const gt_result{"a1f9e0653a6eb00e3ea0d1eb94287563799524ad3938388569560a832f8ffcf2"};
+    std::string const gt_result{"947b431f312e71ed52b4e85b84ae36e34c62ac1dd749f3988f631d23f3c029b9"};
 
     EXPECT_EQ(result, gt_result);
 }
 
-// TODO(Jack): We need some test fixtures! The testing boilerplate is getting out of hand!
-TEST(CachingCacheKeys, CameraNonlinearRefinement) {
-    CameraInfo const camera_info{"/cam/retro/123", CameraModel::DoubleSphere, testing_utilities::image_bounds};
-    ExtractedTarget const target{
-        Bundle{MatrixX2d{{1.23, 1.43}, {2.75, 2.35}}, MatrixX3d{{3.25, 3.45, 5.43}, {6.18, 6.78, 4.56}}},
-        {{5, 6}, {2, 3}}};
-    CameraMeasurements const camera_measurements{{0, target}, {1, target}};
-    CameraState const camera_state{testing_utilities::pinhole_intrinsics};
-    Frames const frames{{0, {Array6d::Ones()}}, {1, {2 * Array6d::Ones()}}};
-    OptimizationState const optimization_state{camera_state, frames};
-
+TEST_F(CachingFixture, CameraNonlinearRefinement) {
     std::string const result{caching::CacheKey(camera_info, camera_measurements, optimization_state)};
-    std::string const gt_result{"e41c8d6411b5fbcac3b5e1b7ed66c33959cd13b20a9e48a6b86eeca5cf86530a"};
+    std::string const gt_result{"2a49e05662f0f402884fcb00bbe9b3388979381e9324181e513c56edd255ced7"};
 
     EXPECT_EQ(result, gt_result);
 }
