0e1060fa0cddd6aa609fcf8a01573c0f8010b624
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-05 13:09:42 +0100

    code checks

--- code/library/caching/src/serialize.test.cpp
+++ code/library/caching/src/serialize.test.cpp
@@ -7,8 +7,9 @@
 using namespace reprojection;
 
 // TODO(Jack): Fixture is copy and pasted
-class CachingFixture : public ::testing::Test {
+class SerializeFixture : public ::testing::Test {
    protected:
+    // cppcheck-suppress-begin unusedStructMember
     CameraInfo camera_info{"/cam/retro/123", CameraModel::Pinhole, testing_utilities::image_bounds};
     ExtractedTarget target{
         Bundle{MatrixX2d{{1.23, 1.43}, {2.75, 2.35}}, MatrixX3d{{3.25, 3.45, 5.43}, {6.18, 6.78, 4.56}}},
@@ -16,16 +17,17 @@ class CachingFixture : public ::testing::Test {
     CameraMeasurements camera_measurements{{0, target}, {1, target}};
     CameraState camera_state{testing_utilities::pinhole_intrinsics};
     Frames frames{{0, {Array6d::Ones()}}, {1, {2 * Array6d::Ones()}}};
+    // cppcheck-suppress-end unusedStructMember
 };
 
-TEST_F(CachingFixture, CameraInfo) {
+TEST_F(SerializeFixture, CameraInfo) {
     std::string const result{caching::Serialize(camera_info)};
     std::string const gt_result{"/cam/retro/123|pinhole|0.000,720.000,0.000,480.000|"};
 
     EXPECT_EQ(result, gt_result);
 }
 
-TEST_F(CachingFixture, CameraMeasurements) {
+TEST_F(SerializeFixture, CameraMeasurements) {
     std::string const result{caching::Serialize(camera_measurements)};
     std::string const gt_result{
         "0|1.230,1.430;2.750,2.350;|3.250,3.450,5.430;6.180,6.780,4.560;|5,6;2,3;|1|1.230,1.430;2.750,2.350;|3.250,3."
@@ -34,14 +36,14 @@ TEST_F(CachingFixture, CameraMeasurements) {
     EXPECT_EQ(result, gt_result);
 }
 
-TEST_F(CachingFixture, CameraState) {
+TEST_F(SerializeFixture, CameraState) {
     std::string const result{caching::Serialize(camera_state)};
     std::string const gt_result{"600.000;600.000;360.000;240.000;|"};
 
     EXPECT_EQ(result, gt_result);
 }
 
-TEST_F(CachingFixture, Frames) {
+TEST_F(SerializeFixture, Frames) {
     std::string const result{caching::Serialize(frames)};
     std::string const gt_result{"0|1.000;1.000;1.000;1.000;1.000;1.000;|1|2.000;2.000;2.000;2.000;2.000;2.000;|"};
 
--- code/library/caching/test/cache_keys.test.cpp
+++ code/library/caching/test/cache_keys.test.cpp
@@ -7,8 +7,9 @@
 using namespace reprojection;
 
 // TODO(Jack): Fixture is copy and pasted
-class CachingFixture : public ::testing::Test {
+class CacheKeysFixture : public ::testing::Test {
    protected:
+    // cppcheck-suppress-begin unusedStructMember
     CameraInfo camera_info{"/cam/retro/123", CameraModel::Pinhole, testing_utilities::image_bounds};
     ExtractedTarget target{
         Bundle{MatrixX2d{{1.23, 1.43}, {2.75, 2.35}}, MatrixX3d{{3.25, 3.45, 5.43}, {6.18, 6.78, 4.56}}},
@@ -17,23 +18,24 @@ class CachingFixture : public ::testing::Test {
     CameraState camera_state{testing_utilities::pinhole_intrinsics};
     Frames frames{{0, {Array6d::Ones()}}, {1, {2 * Array6d::Ones()}}};
     OptimizationState optimization_state{camera_state, frames};
+    // cppcheck-suppress-end unusedStructMember
 };
 
-TEST_F(CachingFixture, FocalLengthInitialization) {
+TEST_F(CacheKeysFixture, FocalLengthInitialization) {
     std::string const result{caching::CacheKey(camera_info, camera_measurements)};
     std::string const gt_result{"f4fa69df1a139cf559ab0e423312ecbcb1afe1fff9fffe0782da63a7c22e1b51"};
 
     EXPECT_EQ(result, gt_result);
 }
 
-TEST_F(CachingFixture, LinearPoseInitialization) {
+TEST_F(CacheKeysFixture, LinearPoseInitialization) {
     std::string const result{caching::CacheKey(camera_info, camera_measurements, camera_state)};
     std::string const gt_result{"947b431f312e71ed52b4e85b84ae36e34c62ac1dd749f3988f631d23f3c029b9"};
 
     EXPECT_EQ(result, gt_result);
 }
 
-TEST_F(CachingFixture, CameraNonlinearRefinement) {
+TEST_F(CacheKeysFixture, CameraNonlinearRefinement) {
     std::string const result{caching::CacheKey(camera_info, camera_measurements, optimization_state)};
     std::string const gt_result{"2a49e05662f0f402884fcb00bbe9b3388979381e9324181e513c56edd255ced7"};
 
