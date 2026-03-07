84bad05633dee2dc96f882b3c3639733fae6c3e0
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-07 07:34:15 -0500

    rename step to "camera_nonlinear_refinement"

--- code/sql/calibration_steps_table.sql
+++ code/sql/calibration_steps_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS calibration_steps
 (
     step_name  TEXT PRIMARY KEY CHECK ( step_name IN ('linear_pose_initialization',
-                                                      'nonlinear_refinement',
+                                                      'camera_nonlinear_refinement',
                                                       'spline_interpolation',
                                                       'spline_nonlinear_refinement')),
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
