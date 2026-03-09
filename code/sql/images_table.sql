c8bba4538a0feb8637e4cc9afa10eb031f1be98a
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-09 11:00:32 -0400

    remove images table foreign key constraint

--- code/sql/images_table.sql
+++ code/sql/images_table.sql
@@ -3,7 +3,5 @@ CREATE TABLE IF NOT EXISTS images
     sensor_name  TEXT    NOT NULL,
     timestamp_ns INTEGER NOT NULL,
     data         BLOB    NULL,
-    PRIMARY KEY (sensor_name, timestamp_ns),
-    FOREIGN KEY (sensor_name)
-        REFERENCES camera_info (sensor_name)
+    PRIMARY KEY (sensor_name, timestamp_ns)
 );
