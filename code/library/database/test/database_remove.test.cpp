eef774dd386f1c23a2660e0b98f12aef42a25421
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-09 22:13:34 -0400

    DatabaseRemoveFixture

--- code/library/database/test/database_remove.test.cpp
+++ code/library/database/test/database_remove.test.cpp
@@ -9,23 +9,32 @@
 
 using namespace reprojection;
 
-TEST(DatabaseDatabaseRemove, TestRemoveFromDbStep) {
-    auto const db{std::make_shared<database::CalibrationDatabase>(":memory:", true, false)};
+class DatabaseRemoveFixture : public ::testing::Test {
+   protected:
+    void SetUp() override {
+        db = std::make_shared<database::CalibrationDatabase>(":memory:", true, false);
 
+        database::WriteToDb(camera_info, db);
+    }
+
+    std::shared_ptr<database::CalibrationDatabase> db;
+    CameraInfo camera_info{"/cam/retro/123", CameraModel::Pinhole, testing_utilities::image_bounds};
+};
+
+TEST_F(DatabaseRemoveFixture, TestRemoveFromDbStep) {
     // If there is no step to remove it is just silent
     EXPECT_NO_THROW(database::RemoveFromDb(CalibrationStep::Lpi, "", db));
 
     // Write a step to the database and load its cache key to check its there.
-    database::WriteToDb(CameraInfo{"/cam/retro/123", CameraModel::Pinhole, testing_utilities::image_bounds}, db);
-    database::WriteToDb(CalibrationStep::Lpi, "cache_key", "/cam/retro/123", db);
+    database::WriteToDb(CalibrationStep::Lpi, "cache_key", camera_info.sensor_name, db);
 
-    auto cache_key{database::ReadCacheKey(db, CalibrationStep::Lpi, "/cam/retro/123")};
+    auto cache_key{database::ReadCacheKey(db, CalibrationStep::Lpi, camera_info.sensor_name)};
     ASSERT_TRUE(cache_key.has_value());
 
     // Remove the step and then try to load the cache key - but the cache key should be std::nullopt because the step
     // has been removed.
-    EXPECT_NO_THROW(database::RemoveFromDb(CalibrationStep::Lpi, "/cam/retro/123", db));
+    EXPECT_NO_THROW(database::RemoveFromDb(CalibrationStep::Lpi, camera_info.sensor_name, db));
 
-    cache_key = database::ReadCacheKey(db, CalibrationStep::Lpi, "/cam/retro/123");
+    cache_key = database::ReadCacheKey(db, CalibrationStep::Lpi, camera_info.sensor_name);
     EXPECT_FALSE(cache_key.has_value());
 }
