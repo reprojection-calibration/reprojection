a6da518ba818c6aab7e4d1232464b34c0b121024
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-10 08:54:07 -0400

    remove duplication ExtractBundlesByDimension

--- code/library/calibration/src/utilities.cpp
+++ code/library/calibration/src/utilities.cpp
@@ -8,31 +8,26 @@
 namespace reprojection::calibration {
 
 std::pair<std::vector<Bundle>, std::vector<Bundle>> SortIntoRowsAndCols(ExtractedTarget const& target) {
-    // Get rows
-    // See https://stackoverflow.com/questions/14740867/find-unique-numbers-in-array
-    std::set<int> const row_ids{target.indices.col(0).cbegin(), target.indices.col(0).cend()};
-    std::vector<Bundle> rows;
-    for (auto const id : row_ids) {
-        ArrayXb const row_mask{
-            (target.indices.col(0).unaryExpr([id](int const x) { return x == id; }) == true)};
-        ArrayXi const indices{eigen_utilities::MaskToRowId(row_mask)};
-
-        rows.push_back(target.bundle(indices));
-    }
-
-    // TODO(Jack): Can we avoid the copy and paste?
-    // Get cols
-    std::set<int> const col_ids{target.indices.col(1).cbegin(), target.indices.col(1).cend()};
-    std::vector<Bundle> cols;
-    for (auto const id : col_ids) {
-        ArrayXb const col_mask{
-            (target.indices.col(1).unaryExpr([id](int const x) { return x == id; }) == true)};
-        ArrayXi const indices{eigen_utilities::MaskToRowId(col_mask)};
-
-        cols.push_back(target.bundle(indices));
-    }
+    auto const rows{ExtractBundlesByDimension(target, Dimension::Row)};
+    auto const cols{ExtractBundlesByDimension(target, Dimension::Col)};
 
     return {rows, cols};
+}
+
+// See https://stackoverflow.com/questions/14740867/find-unique-numbers-in-array
+std::vector<Bundle> ExtractBundlesByDimension(ExtractedTarget const& target, Dimension dim) {
+    auto const column{target.indices.col(static_cast<int>(dim))};
+    std::set<int> const ids{std::cbegin(column), std::cend(column)};
+
+    std::vector<Bundle> bundles;
+    for (auto const id : ids) {
+        ArrayXb const mask{(column.unaryExpr([id](int const x) { return x == id; }) == true)};
+
+        ArrayXi const indices{eigen_utilities::MaskToRowId(mask)};
+        bundles.push_back(target.bundle(indices));
+    }
+
+    return bundles;
 }
 
 }  // namespace reprojection::calibration
--- code/library/calibration/src/utilities.hpp
+++ code/library/calibration/src/utilities.hpp
@@ -6,6 +6,10 @@
 
 namespace reprojection::calibration {
 
+enum class Dimension { Row = 0, Col = 1 };
+
 std::pair<std::vector<Bundle>, std::vector<Bundle>> SortIntoRowsAndCols(ExtractedTarget const& target);
+
+std::vector<Bundle> ExtractBundlesByDimension(ExtractedTarget const& target, Dimension dim);
 
 }  // namespace reprojection::calibration
