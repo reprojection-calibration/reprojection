3697f379563c61d3049994bca56fd48c2b35f9a4
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-14 09:42:36 -0400

    add MakeCamera helper

--- code/library/projection_functions/include/projection_functions/intialize_camera.hpp
+++ code/library/projection_functions/include/projection_functions/intialize_camera.hpp
@@ -8,4 +8,17 @@ namespace reprojection::projection_functions {
 // TODO(Jack): Test!
 std::unique_ptr<Camera> InitializeCamera(CameraModel const model, ArrayXd const& intrinsics, ImageBounds const& bounds);
 
+template <typename T_Model>
+    requires ProjectionClass<T_Model>
+std::unique_ptr<Camera> MakeCamera(Eigen::VectorXd const& intrinsics, ImageBounds const& bounds) {
+    if (intrinsics.rows() != T_Model::Size) {
+        throw std::runtime_error("Intrinsic size mismatch - wanted " + std::to_string(T_Model::Size) + " but got " +
+                                 std::to_string(intrinsics.rows()));  // LCOV_EXCL_LINE
+    }
+
+    using CameraType = Camera_T<T_Model>;
+
+    return std::make_unique<CameraType>(intrinsics, bounds);
+}
+
 }  // namespace reprojection::projection_functions
--- code/library/projection_functions/src/intialize_camera.cpp
+++ code/library/projection_functions/src/intialize_camera.cpp
@@ -1,42 +1,22 @@
-
+#include "projection_functions/intialize_camera.hpp"
 
 #include "projection_functions/camera_model.hpp"
+#include "types/calibration_types.hpp"
 
 namespace reprojection::projection_functions {
 
-// TODO(Jack): Add all other camera models and check the above listed TODO points.
 std::unique_ptr<Camera> InitializeCamera(CameraModel const model, ArrayXd const& intrinsics,
                                          ImageBounds const& bounds) {
     if (model == CameraModel::DoubleSphere) {
-        if (not(intrinsics.rows() == DoubleSphere::Size)) {
-            throw std::runtime_error(
-                "InitializeCamera() requested CameraModel::DoubleSphere but got intrinsics with length: " +  // LCOV_EXCL_LINE
-                std::to_string(intrinsics.rows()));  // LCOV_EXCL_LINE
-        }
-        return std::unique_ptr<Camera>(new DoubleSphereCamera(intrinsics, bounds));
+        return MakeCamera<DoubleSphere>(intrinsics, bounds);
     } else if (model == CameraModel::Pinhole) {
-        if (not(intrinsics.rows() == Pinhole::Size)) {
-            throw std::runtime_error(
-                "InitializeCamera() requested CameraModel::Pinhole but got intrinsics with length: " +  // LCOV_EXCL_LINE
-                std::to_string(intrinsics.rows()));  // LCOV_EXCL_LINE
-        }
-        return std::unique_ptr<Camera>(new PinholeCamera(intrinsics, bounds));
+        return MakeCamera<Pinhole>(intrinsics, bounds);
     } else if (model == CameraModel::PinholeRadtan4) {
-        if (not(intrinsics.rows() == PinholeRadtan4::Size)) {
-            throw std::runtime_error(
-                "InitializeCamera() requested CameraModel::PinholeRadtan4 but got intrinsics with length: " +  // LCOV_EXCL_LINE
-                std::to_string(intrinsics.rows()));  // LCOV_EXCL_LINE
-        }
-        return std::unique_ptr<Camera>(new PinholeRadtan4Camera(intrinsics, bounds));
+        return MakeCamera<PinholeRadtan4>(intrinsics, bounds);
     } else if (model == CameraModel::UnifiedCameraModel) {
-        if (not(intrinsics.rows() == UnifiedCameraModel::Size)) {
-            throw std::runtime_error(
-                "InitializeCamera() requested CameraModel::UnifiedCameraModel but got intrinsics with length: " +  // LCOV_EXCL_LINE
-                std::to_string(intrinsics.rows()));  // LCOV_EXCL_LINE
-        }
-        return std::unique_ptr<Camera>(new UcmCamera(intrinsics, bounds));
+        return MakeCamera<UnifiedCameraModel>(intrinsics, bounds);
     } else {
-        throw std::runtime_error("invalid camera model");  // LCOV_EXCL_LINE
+        throw std::runtime_error("LIBRARY IMPLEMENTATION ERROR - invalid camera model");  // LCOV_EXCL_LINE
     }
 }
 
