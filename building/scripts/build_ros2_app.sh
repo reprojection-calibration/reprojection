bce0a15cbb5224db32e1706a9a0f47b9c4e63783
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-20 12:12:43 +0100

    remove ament dependecy in ros2

--- building/scripts/build_ros2_app.sh
+++ building/scripts/build_ros2_app.sh
@@ -2,13 +2,35 @@
 
 set -eox pipefail
 
-# shellcheck disable=SC1091
+# TODO(Jack): This script is almost exactly the same as the build_library.sh one!
+
+build_directory=/buildroot/build-ros2
+cmake_build_type=Debug
+
+# Adopted from https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+for i in "$@"; do
+  case $i in
+      --cmake_build_type=*)
+      cmake_build_type="${i#*=}"
+      shift
+      ;;
+    -*)
+      echo "Unknown option $i"
+      exit 1
+      ;;
+    *)
+      ;;
+  esac
+done
+
+# NOTE(Jack): We have to source the ros setup script here because rclcpp depends on some ament cmake functionality that
+# is delivered by ros.
 source /opt/ros/jazzy/setup.bash
 
-set -eoux pipefail
+# TODO(Jack): Make release build by default! Note that release builds cannot be built with code coverage enabled!
+cmake -B "${build_directory}" -DCMAKE_BUILD_TYPE="${cmake_build_type}"  -G Ninja -S /temporary/code/application_ros2 -DCMAKE_PREFIX_PATH=/opt/ros/jazzy
+cmake --build "${build_directory}"
 
-cd /temporary/code/ros2_ws
 
-colcon build
-colcon test
-colcon test-result --verbose --all
+cd "${build_directory}"
+ctest --output-on-failure --progress
