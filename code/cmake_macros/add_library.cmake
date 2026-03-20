189d5d671be2cc59e4a72cac2f854c030280e331
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-20 22:48:28 +0100

    TASK: Package library and install into ROS applications (#129)
    * ADD: Cmake installation logic (libs default ON, headers default OFF)
    * ADD: $<INSTALL_INTERFACE:include> to AddLibrary() macro
    * REMOVE: database_proto library and use source code directly instead
    * REFACTOR: ROS base stages to inherit from apt-base-stage
    * REFACTOR: Python tooling to inherit from apt-base-stage

--- code/cmake_macros/add_library.cmake
+++ code/cmake_macros/add_library.cmake
@@ -4,6 +4,7 @@ macro(AddLibrary)
     )
     target_include_directories(${LIBRARY_NAME} PUBLIC
             $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include/>
+            $<INSTALL_INTERFACE:include>
     )
     target_include_directories(${LIBRARY_NAME} PRIVATE
             $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/src/>
@@ -12,6 +13,32 @@ macro(AddLibrary)
     target_link_libraries(${LIBRARY_NAME} PRIVATE
             ${LINK_LIBRARIES}
     )
+
+    # Install shared libs default ON
+    if (NOT DEFINED INSTALL_SO)
+        set(INSTALL_SO ON)
+    endif()
+
+    # Install headers default OFF
+    if (NOT DEFINED INSTALL_HEADERS)
+        set(INSTALL_HEADERS OFF)
+    endif()
+
+    if (INSTALL_SO)
+        install(TARGETS ${LIBRARY_NAME}
+                EXPORT reprojectionTargets
+                LIBRARY DESTINATION lib
+                ARCHIVE DESTINATION lib
+                RUNTIME DESTINATION bin
+        )
+    endif()
+
+    if (INSTALL_HEADERS)
+        install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/include/
+                DESTINATION include
+        )
+    endif()
+
 
     if (CMAKE_BUILD_TYPE STREQUAL "Debug")
         target_compile_options(${LIBRARY_NAME} PRIVATE --coverage)
