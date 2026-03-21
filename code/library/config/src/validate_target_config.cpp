51e6ed4a11cd537fac75d361e92107174cd26ba0
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-21 19:58:22 +0100

    constify ValidateTargetConfig

--- code/library/config/src/validate_target_config.cpp
+++ code/library/config/src/validate_target_config.cpp
@@ -7,23 +7,20 @@
 namespace reprojection::config {
 
 std::optional<ParserErrorMsg> ValidateTargetConfig(toml::table const& target_cfg) {
-    // TODO(Jack): Why does my use of .merge() below not allow this to be const? Makes no sense.
-    // TODO(Jack): If this could be const then it should also be static. Also below for the optional_keys.
-    std::map<std::string, TomlType> required_keys{{"pattern_size", TomlType::Array}, {"type", TomlType::String}};
-
+    std::map<std::string, TomlType> const required_keys{{"pattern_size", TomlType::Array}, {"type", TomlType::String}};
     if (auto const error_msg{ValidateRequiredKeys(target_cfg, required_keys)}) {
         return error_msg;
     }
 
-    // TODO(Jack): I wish there was a better way to merge the maps here and express the semantics of what is required,
-    //  what is optional, and how the combination of the two is what is possible.
     // TODO(Jack): NO IDEA why I need to supress code coverage here. Is something wrong with the maps or is my code here
-    // really not executed in testing?
-    std::map<std::string, TomlType> optional_keys{{"circle_grid", TomlType::Table},               // LCOV_EXCL_LINE
-                                                  {"circle_grid.asymmetric", TomlType::Boolean},  // LCOV_EXCL_LINE
-                                                  {"unit_dimension", TomlType::FloatingPoint}};
-    optional_keys.merge(required_keys);
-    std::map<std::string, TomlType> const possible_keys{optional_keys};
+    //  really not executed in testing?
+    std::map<std::string, TomlType> const optional_keys{
+        {"circle_grid", TomlType::Table},               // LCOV_EXCL_LINE
+        {"circle_grid.asymmetric", TomlType::Boolean},  // LCOV_EXCL_LINE
+        {"unit_dimension", TomlType::FloatingPoint}};
+
+    std::map<std::string, TomlType> possible_keys{required_keys};
+    possible_keys.insert(optional_keys.begin(), optional_keys.end());
 
     if (auto const error_msg{ValidatePossibleKeys(target_cfg, possible_keys)}) {
         return error_msg;
