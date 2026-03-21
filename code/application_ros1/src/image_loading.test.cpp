3fccb99f60e8ab47298b47ad3001293cba0e2a01
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-21 09:41:49 +0100

    add comment context

--- code/application_ros1/src/image_loading.test.cpp
+++ code/application_ros1/src/image_loading.test.cpp
@@ -9,9 +9,14 @@
 
 using namespace reprojection;
 
-TEST(Ros1ImageLoading, TestToCvMat) {
-    rosbag::Bag bag;
+TEST(Ros1ImageLoading, TestRawAndCompressedToCvMat) {
+    // NOTE(Jack): Our ros1::ToCvMat() function works directly on the rosbag::MessageInstance. This type, as far as I
+    // understand, cannot really be created as normal standalone objects. The only place that you can really get them is
+    // by iterating a open bag via a rosbag::View. Therefore, in this test we need to write a bag and then process it to
+    // get the test data in the required format.
     ros1::ScopedBagPath const temp_bag;
+
+    rosbag::Bag bag;
     bag.open(temp_bag.path, rosbag::bagmode::Write);
     bag.write("/raw_image_topic", ros::Time(1), ros1::DummyImage());
     bag.write("/compressed_image_topic", ros::Time(1), ros1::DummyCompressedImage());
