569476733e21f0c269639347332c4cb73600d2d4
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-21 22:18:00 +0100

    TestValidateDataConfig

--- code/library/config/test/validate_config.test.cpp
+++ code/library/config/test/validate_config.test.cpp
@@ -19,9 +19,46 @@ static constexpr std::string_view minimum_config{R"(
         type = "circle_grid"
     )"};
 
-TEST(XXX, YYY) {
+TEST(ConfigValidateConfig, TestValidateCalibrationConfigHappyPath) {
     toml::table const toml{toml::parse(minimum_config)};
 
     auto const result{config::ValidateCalibrationConfig(toml)};
     EXPECT_FALSE(result.has_value());
+}
+
+TEST(ConfigValidateConfig, TestValidateDataConfig) {
+    static constexpr std::string_view good_config{R"(
+        [data]
+        file = "/data/TUM-Visual-Inertial-Dataset/dataset-calib-imu4.bag"
+    )"};
+    toml::table toml{toml::parse(good_config)};
+    auto result{config::ValidateDataConfig(*toml["data"].as_table())};
+    EXPECT_FALSE(result.has_value());
+
+    static constexpr std::string_view incorrect_type{R"(
+        [data]
+        file = 123
+    )"};
+    toml = toml::parse(incorrect_type);
+    result = config::ValidateDataConfig(*toml["data"].as_table());
+    ASSERT_TRUE(result.has_value());
+    EXPECT_EQ(result->error, config::TomlParseError::IncorrectType);
+
+    static constexpr std::string_view missing_key{R"(
+        [data]
+    )"};
+    toml = toml::parse(missing_key);
+    result = config::ValidateDataConfig(*toml["data"].as_table());
+    ASSERT_TRUE(result.has_value());
+    EXPECT_EQ(result->error, config::TomlParseError::MissingKey);
+
+    static constexpr std::string_view unknown_key{R"(
+        [data]
+        file = "/data/TUM-Visual-Inertial-Dataset/dataset-calib-imu4.bag"
+        unknown_key = 0
+    )"};
+    toml = toml::parse(unknown_key);
+    result = config::ValidateDataConfig(*toml["data"].as_table());
+    ASSERT_TRUE(result.has_value());
+    EXPECT_EQ(result->error, config::TomlParseError::UnknownKey);
 }
