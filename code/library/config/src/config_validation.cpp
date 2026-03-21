0fa2a13a94c4829fa5d8f176c364741ff3e06e30
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-21 20:43:23 +0100

    ValidateCalibrationConfig

--- code/library/config/src/config_validation.cpp
+++ code/library/config/src/config_validation.cpp
@@ -7,9 +7,38 @@
 namespace reprojection::config {
 
 std::optional<ParserErrorMsg> ValidateCalibrationConfig(toml::table const& calibration_cfg) {
-    std::map<std::string, TomlType> const required_keys{{"data", TomlType::Table}, {"sensor", TomlType::Table},{"target", TomlType::Table}};
+    std::map<std::string, TomlType> const required_tables{
+            {"data", TomlType::Table}, {"sensor", TomlType::Table}, {"target", TomlType::Table}};
 
-    return ValidateConfigKeys(calibration_cfg, required_keys, {});
+    if (auto const error_msg{ValidateConfigKeys(calibration_cfg, required_tables)}) {
+        return error_msg;
+    }
+
+    if (auto const error_msg{ValidateDataConfig(*calibration_cfg["data"].as_table())}) {
+        return error_msg;
+    }
+
+    if (auto const error_msg{ValidateSensorConfig(*calibration_cfg["sensor"].as_table())}) {
+        return error_msg;
+    }
+
+    if (auto const error_msg{ValidateTargetConfig(*calibration_cfg["target"].as_table())}) {
+        return error_msg;
+    }
+
+    return std::nullopt;
+}
+
+std::optional<ParserErrorMsg> ValidateDataConfig(toml::table const& data_cfg) {
+    std::map<std::string, TomlType> const required_keys{{"file", TomlType::String}};
+
+    return ValidateConfigKeys(data_cfg, required_keys);
+}
+
+std::optional<ParserErrorMsg> ValidateSensorConfig(toml::table const& sensor_cfg){
+    std::map<std::string, TomlType> const required_keys{{"camera_name", TomlType::String}, {"camera_model", TomlType::String}};
+
+    return ValidateConfigKeys(sensor_cfg, required_keys);
 }
 
 std::optional<ParserErrorMsg> ValidateTargetConfig(toml::table const& target_cfg) {
@@ -20,7 +49,5 @@ std::optional<ParserErrorMsg> ValidateTargetConfig(toml::table const& target_cfg
 
     return ValidateConfigKeys(target_cfg, required_keys, optional_keys);
 }
-
-
 
 }  // namespace reprojection::config
--- code/library/config/test/validate_config.test.cpp
+++ code/library/config/test/validate_config.test.cpp
@@ -3,8 +3,6 @@
 #include "config/config_validation.hpp"
 #include "testing_utilities/temporary_file.hpp"
 
-
-
 using namespace reprojection;
 using TemporaryFile = testing_utilities::TemporaryFile;
 
@@ -24,7 +22,6 @@ static constexpr std::string_view minimum_config{R"(
 TEST(XXX, YYY) {
     toml::table const toml{toml::parse(minimum_config)};
 
-   auto const result{ config::ValidateCalibrationConfig(toml)};
-
-    std::cout << result->msg << std::endl;
+    auto const result{config::ValidateCalibrationConfig(toml)};
+    EXPECT_FALSE(result.has_value());
 }
