f9e1ca1db78a86b9bf708795a0d034c36e0b4965
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-22 10:30:55 +0100

    add ExtractValue and ThrowIfUnexpectedKeys - eliminate copy paste

--- code/library/config/src/config_parsing.cpp
+++ code/library/config/src/config_parsing.cpp
@@ -2,55 +2,45 @@
 
 namespace reprojection::config {
 
+template <typename T>
+T ExtractValue(std::string_view key, toml::table& cfg) {
+    T value;
+    if (auto const node{cfg.get(key)}) {
+        value = node->as<T>()->get();
+        cfg.erase(key);
+    }
+
+    return value;
+}
+
+void ThrowIfUnexpectedKeys(toml::table const& cfg, std::string_view section) {
+    if (cfg.empty()) {
+        return;
+    }
+
+    std::ostringstream oss;
+    oss << "Unexpected parameters found in the " << section << " configuration, are you sure they are correct?\n";
+    for (const auto& [key, _] : cfg) {
+        oss << "  - " << key.str() << "\n";
+    }
+
+    throw std::runtime_error(oss.str());
+}
+
 // TODO USE FILESYSTEM PATH?
 std::string ParseDataConfig(toml::table data_cfg) {
-    // TODO MAKE FUNCTION TO ELIMINATE COPY PASTE
-    std::string file;
-    if (auto const value{data_cfg.get("file")}) {
-        file = value->as<std::string>()->get();
-        data_cfg.erase("file");
-    }
+    std::string const file{ExtractValue<std::string>("file", data_cfg)};
 
-
-    // TODO MAKE FUNCTION TO ELIMINATE COPY PASTE
-    if (not data_cfg.empty()) {
-        std::ostringstream oss;
-        oss << "Unexpected parameters found in the sensor configuration, are you sure they are correct?\n";
-        for (const auto& [key, _] : data_cfg) {
-            oss << "  - " << key.str() << "\n";
-        }
-
-        throw std::runtime_error(oss.str());
-    }
+    ThrowIfUnexpectedKeys(data_cfg, "data");
 
     return file;
 }
 
 std::pair<std::string, CameraModel> ParseSensorConfig(toml::table sensor_cfg) {
-    // TODO MAKE FUNCTION TO ELIMINATE COPY PASTE
-    std::string camera_name;
-    if (auto const value{sensor_cfg.get("camera_name")}) {
-        camera_name = value->as<std::string>()->get();
-        sensor_cfg.erase("camera_name");
-    }
+    std::string camera_name{ExtractValue<std::string>("camera_name", sensor_cfg)};
+    std::string camera_model{ExtractValue<std::string>("camera_model", sensor_cfg)};
 
-    // TODO MAKE FUNCTION TO ELIMINATE COPY PASTE
-    std::string camera_model;
-    if (auto const value{sensor_cfg.get("camera_model")}) {
-        camera_model = value->as<std::string>()->get();
-        sensor_cfg.erase("camera_model");
-    }
-
-    // TODO MAKE FUNCTION TO ELIMINATE COPY PASTE
-    if (not sensor_cfg.empty()) {
-        std::ostringstream oss;
-        oss << "Unexpected parameters found in the sensor configuration, are you sure they are correct?\n";
-        for (const auto& [key, _] : sensor_cfg) {
-            oss << "  - " << key.str() << "\n";
-        }
-
-        throw std::runtime_error(oss.str());
-    }
+    ThrowIfUnexpectedKeys(sensor_cfg, "sensor");
 
     return {camera_name, ToCameraModel(camera_model)};
 }
