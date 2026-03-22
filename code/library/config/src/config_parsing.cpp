c6d19d5da8af1eb1b577b2ea2f22f104b7200f25
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-22 11:20:54 +0100

    refactor parsing helpers into header

--- code/library/config/src/config_parsing.cpp
+++ code/library/config/src/config_parsing.cpp
@@ -1,33 +1,8 @@
 #include "config/config_parsing.hpp"
+#include "parsing_helpers.hpp"
 
 namespace reprojection::config {
 
-template <typename T>
-T ExtractValue(std::string_view key, toml::table& cfg) {
-    T value;
-    if (auto const node{cfg.get(key)}) {
-        value = node->as<T>()->get();
-        cfg.erase(key);
-    }
-
-    return value;
-}  // LCOV_EXCL_LINE
-
-void ThrowIfUnexpectedKeys(toml::table const& cfg, std::string_view section) {
-    if (cfg.empty()) {
-        return;
-    }
-
-    std::ostringstream oss;
-    oss << "Unexpected parameters found in the " << section << " configuration, are you sure they are correct?\n";
-    for (const auto& [key, _] : cfg) {
-        oss << "  - " << key.str() << "\n";
-    }
-
-    throw std::runtime_error(oss.str());
-}
-
-// TODO USE FILESYSTEM PATH?
 std::string ParseDataConfig(toml::table data_cfg) {
     std::string const file{ExtractValue<std::string>("file", data_cfg)};
 
