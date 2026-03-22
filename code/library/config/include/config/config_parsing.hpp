0c6615cefb9f50283cb70b15cf02a7e353cb1c25
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-22 10:21:48 +0100

    TestParseDataConfig TestParseSensorConfig

--- code/library/config/include/config/config_parsing.hpp
+++ code/library/config/include/config/config_parsing.hpp
@@ -8,6 +8,11 @@
 
 namespace reprojection::config {
 
+std::string ParseDataConfig(toml::table data_cfg);
+
+// TODO(Jack): Returning just a pair here is not so nive, because there is no semantic type system enforcement of what
+//  these represent. We could return a CameraInfo type from here (which we will probably do one day), but the fact that
+//  we do not have the image bounds here too means I do not want to partially construct it and have it be invalid.
 std::pair<std::string, CameraModel> ParseSensorConfig(toml::table solver_cfg);
 
 ceres::Solver::Options ParseSolverConfig(toml::table solver_cfg);
--- code/library/config/src/config_parsing.cpp
+++ code/library/config/src/config_parsing.cpp
@@ -1,8 +1,58 @@
 #include "config/config_parsing.hpp"
 
-
-
 namespace reprojection::config {
 
-// NOTE(Jack): As we add more configs we will probably return a tuple here or a struct with all the possible configs
+// TODO USE FILESYSTEM PATH?
+std::string ParseDataConfig(toml::table data_cfg) {
+    // TODO MAKE FUNCTION TO ELIMINATE COPY PASTE
+    std::string file;
+    if (auto const value{data_cfg.get("file")}) {
+        file = value->as<std::string>()->get();
+        data_cfg.erase("file");
+    }
+
+
+    // TODO MAKE FUNCTION TO ELIMINATE COPY PASTE
+    if (not data_cfg.empty()) {
+        std::ostringstream oss;
+        oss << "Unexpected parameters found in the sensor configuration, are you sure they are correct?\n";
+        for (const auto& [key, _] : data_cfg) {
+            oss << "  - " << key.str() << "\n";
+        }
+
+        throw std::runtime_error(oss.str());
+    }
+
+    return file;
+}
+
+std::pair<std::string, CameraModel> ParseSensorConfig(toml::table sensor_cfg) {
+    // TODO MAKE FUNCTION TO ELIMINATE COPY PASTE
+    std::string camera_name;
+    if (auto const value{sensor_cfg.get("camera_name")}) {
+        camera_name = value->as<std::string>()->get();
+        sensor_cfg.erase("camera_name");
+    }
+
+    // TODO MAKE FUNCTION TO ELIMINATE COPY PASTE
+    std::string camera_model;
+    if (auto const value{sensor_cfg.get("camera_model")}) {
+        camera_model = value->as<std::string>()->get();
+        sensor_cfg.erase("camera_model");
+    }
+
+    // TODO MAKE FUNCTION TO ELIMINATE COPY PASTE
+    if (not sensor_cfg.empty()) {
+        std::ostringstream oss;
+        oss << "Unexpected parameters found in the sensor configuration, are you sure they are correct?\n";
+        for (const auto& [key, _] : sensor_cfg) {
+            oss << "  - " << key.str() << "\n";
+        }
+
+        throw std::runtime_error(oss.str());
+    }
+
+    return {camera_name, ToCameraModel(camera_model)};
+}
+
 }  // namespace reprojection::config
