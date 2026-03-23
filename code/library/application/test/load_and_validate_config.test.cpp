87f93e63af2b69c4998789c3f99992bce5e2d181
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-23 07:21:32 +0100

    TestHappyPath TestBadFile, TestBadValidation

--- code/library/application/test/load_and_validate_config.test.cpp
+++ code/library/application/test/load_and_validate_config.test.cpp
@@ -2,12 +2,47 @@
 
 #include <gtest/gtest.h>
 
+#include "testing_utilities/temporary_file.hpp"
+
 using namespace reprojection;
+using TemporaryFile = testing_utilities::TemporaryFile;
 
-TEST(ApplicationLoadAndValidateConfig, TestYYYY) {
-    //std::variant<toml::table, config::TomlErrorMsg>
+TEST(ApplicationLoadAndValidateConfig, TestHappyPath) {
+    // TODO(Jack): This is now copy and pasted in two places, should we make one common def in the testing utils?
+    static constexpr std::string_view minimum_config{R"(
+        [data]
+        file = "/data/TUM-Visual-Inertial-Dataset/dataset-calib-imu4.bag"
 
-    auto const result{application::LoadAndValidateConfig("")};
+        [sensor]
+        camera_name = "/cam0/image_raw"
+        camera_model = "double_sphere"
 
-    EXPECT_EQ(1,2);
+        [target]
+        pattern_size = [3,4]
+        type = "circle_grid"
+    )"};
+    TemporaryFile const config_file{".toml", minimum_config};
+
+    auto const result{application::LoadAndValidateConfig(config_file.Path())};
+    ASSERT_TRUE(std::holds_alternative<toml::table>(result));
+    toml::table const gt_result{toml::parse(minimum_config)};
+    EXPECT_EQ(std::get<toml::table>(result), gt_result);
+}
+
+TEST(ApplicationLoadAndValidateConfig, TestBadLoad) {
+    auto const result{application::LoadAndValidateConfig("nonexistent.toml")};
+    ASSERT_TRUE(std::holds_alternative<config::TomlErrorMsg>(result));
+    EXPECT_EQ(std::get<config::TomlErrorMsg>(result).error, config::TomlError::FailedLoad);
+    EXPECT_EQ(std::get<config::TomlErrorMsg>(result).msg, "Error parsing file 'nonexistent.toml' - File could not be opened for reading on line (0)");
+}
+
+TEST(ApplicationLoadAndValidateConfig, TestBadValidate) {
+    static constexpr std::string_view empty_config{R"(
+    )"};
+    TemporaryFile const config_file{".toml", empty_config};
+
+    auto const result{application::LoadAndValidateConfig(config_file.Path())};
+    ASSERT_TRUE(std::holds_alternative<config::TomlErrorMsg>(result));
+    EXPECT_EQ(std::get<config::TomlErrorMsg>(result).error, config::TomlError::MissingKey);
+    EXPECT_EQ(std::get<config::TomlErrorMsg>(result).msg, "Configuration does not contain required key: data of type: table");
 }
