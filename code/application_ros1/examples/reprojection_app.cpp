3c1947abfb2ec9fc3754f409cc1d5669a11115d7
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-23 11:08:57 +0100

    error handling ros1

--- code/application_ros1/examples/reprojection_app.cpp
+++ code/application_ros1/examples/reprojection_app.cpp
@@ -1,3 +1,7 @@
+#include <cstdlib>
+#include <iostream>
+
+#include <reprojection/application/cli_utils.hpp>
 #include <reprojection/application/load_and_validate_config.hpp>
 
 #include "reprojection/bag_wrapper.hpp"
@@ -5,16 +9,28 @@
 
 using namespace reprojection;
 
-int main() {
-    try {
-        auto const [bag_file, image_topic]{ros1::DummyLoadConfig()};
-        ros1::BagWrapper const bag{bag_file, rosbag::bagmode::Read};
+int main(int argc, char* argv[]) {
+    auto const config_path{application::GetCommandOption(argv, argv + argc, "--config")};
+    if (not config_path) {
+        // TODO(Jack): What is a long term strategy to guarantee that these error messages stay consistent across all
+        //  applications?
+        std::cout << "\n\tMissing --config flag\n" << std::endl;
 
-        auto const cache_string{TopicCacheString(bag, image_topic)};
-        std::cout << cache_string.value() << std::endl;
-    } catch (rosbag::BagException& e) {
-        return 1;
+        return EXIT_FAILURE;
     }
+
+    auto const config{application::LoadAndValidateConfig(*config_path)};
+    if (std::holds_alternative<TomlErrorMsg>(config)) {
+        std::cout << "\n\t" + std::get<TomlErrorMsg>(config).msg + "\n" << std::endl;
+
+        return EXIT_FAILURE;
+    }
+
+    auto const [bag_file, image_topic]{ros1::DummyLoadConfig()};
+    ros1::BagWrapper const bag{bag_file, rosbag::bagmode::Read};
+
+    auto const cache_string{TopicCacheString(bag, image_topic)};
+    std::cout << cache_string.value() << std::endl;
 
     return 0;
 }
