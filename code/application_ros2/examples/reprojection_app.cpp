125fd616ad12ef52f34f2d5b774822ac45474a6a
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-23 14:08:33 +0100

    add data path as command line arg

--- code/application_ros2/examples/reprojection_app.cpp
+++ code/application_ros2/examples/reprojection_app.cpp
@@ -7,20 +7,25 @@
 
 using namespace reprojection;
 
+// TODO(Jack): What is a long term strategy to guarantee that these error messages stay consistent across all
+//  applications?
+
 int main(int argc, char* argv[]) {
     auto const config_path{application::GetCommandOption(argv, argv + argc, "--config")};
     if (not config_path) {
-        // TODO(Jack): What is a long term strategy to guarantee that these error messages stay consistent across all
-        //  applications?
         std::cout << "Missing --config flag" << "\n";
+        return EXIT_FAILURE;
+    }
 
+    auto const data_path{application::GetCommandOption(argv, argv + argc, "--data")};
+    if (not data_path) {
+        std::cout << "Missing --data flag" << "\n";
         return EXIT_FAILURE;
     }
 
     auto const config{application::LoadAndValidateConfig(*config_path)};
     if (std::holds_alternative<TomlErrorMsg>(config)) {
         std::cout << std::get<TomlErrorMsg>(config).msg << "\n";
-
         return EXIT_FAILURE;
     }
 
