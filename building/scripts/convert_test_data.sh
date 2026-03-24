7c909dfd45b6da309d9466b8a388e3ac621306d7
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-24 14:24:35 +0100

    use python:3.12-slim!!!!!!

--- building/scripts/convert_test_data.sh
+++ building/scripts/convert_test_data.sh
@@ -2,19 +2,7 @@
 
 set -eoux pipefail
 
-# NOTE(Jack): Technically when working with python we should use a venv to prevent us from interfering/breaking the
-# system packages. But our use case here is so small and discrete that it is not worth the overhead. Therefore we simply
-# install python and pip via apt-get. Please note that because we just do a system install we have to pass the
-# '--break-system-packages' to the pip3 install command. If this causes trouble one day, or we want to be able to
-# execute this locally one day, then we can refactor, but for now it gets the job done!
-
-apt-get update
-apt-get install --no-install-recommends --yes \
-    python3 \
-    python3-pip
-rm --force --recursive /var/lib/apt/lists/*
-
-pip3 install --break-system-packages rosbags
+pip3 install rosbags
 
 # TODO(Jack): Something that we need to be careful about is that we now have these test data paths and names copied all
 #  over the place! It would be nice if we designed the system such that they are only specified in one place.
