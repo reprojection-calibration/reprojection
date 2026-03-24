f0e79f16ded797ca9f899ed6a98ecf8751224227
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-24 15:28:54 +0100

    code checks

--- code/application_ros1/test/reprojection.test.cpp
+++ code/application_ros1/test/reprojection.test.cpp
@@ -12,13 +12,13 @@ TEST(Ros1Reprojection, TestSerializeBagTopic) {
     ros1::ScopedBagPath const temp_bag;
     {
         rosbag::Bag bag;
-        bag.open(temp_bag.path, rosbag::bagmode::Write );
+        bag.open(temp_bag.path, rosbag::bagmode::Write);
         bag.write("/raw_image_topic", ros::Time(1), ros1::DummyImage());
     }
 
     auto const reader_result{ros1::SingleTopicBagReader::Create(temp_bag.path, "/raw_image_topic")};
     ASSERT_TRUE(std::holds_alternative<ros1::SingleTopicBagReader>(reader_result));
-    auto const& reader { std::get<ros1::SingleTopicBagReader>(reader_result)};
+    auto const& reader{std::get<ros1::SingleTopicBagReader>(reader_result)};
 
     auto result{ros1::SerializeBagTopic(reader)};
     ASSERT_TRUE(result.has_value());
