7727dd89134774004b588d92c41d519fc3ee0b84
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-24 13:51:15 +0100

    data conversion test-data

--- building/install_scripts/download_test_data.sh
+++ building/install_scripts/download_test_data.sh
@@ -6,5 +6,6 @@ apt-get update
 apt-get install --no-install-recommends --yes \
     ca-certificates \
     wget
+rm --force --recursive /var/lib/apt/lists/*
 
 wget --directory-prefix="/data" "https://vision.in.tum.de/tumvi/calibrated/512_16/dataset-calib-imu4_512_16.bag"
