67f4076bfba8203b939c15364a4e476fabbeb315
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-24 12:33:27 +0100

    download ros1 bag

---
+++ building/install_scripts/download_test_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eoux pipefail
+
+apt-get update
+apt-get install --no-install-recommends --yes \
+    ca-certificates \
+    wget
+
+wget --directory-prefix="/data" "https://vision.in.tum.de/tumvi/calibrated/512_16/dataset-calib-imu4_512_16.bag"
