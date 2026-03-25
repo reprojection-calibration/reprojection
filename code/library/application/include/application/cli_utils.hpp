9e239c432d7c298d6c2d941c46643825c6a33176
reprojection-calibration <reprojection.calibration@gmail.com>
2026-03-25 19:22:20 +0100

    TEST(ApplicationCliUtils, TestParseCommandLineInput)

--- code/library/application/include/application/cli_utils.hpp
+++ code/library/application/include/application/cli_utils.hpp
@@ -1,10 +1,22 @@
 #pragma once
 
 #include <algorithm>
+#include <filesystem>
 #include <optional>
 #include <string>
+#include <variant>
 
 namespace reprojection::application {
+
+struct PathConfig {
+    std::filesystem::path config_path;
+    std::filesystem::path data_path;
+    std::filesystem::path workspace_dir;
+};
+
+struct CliErrorMsg {
+    std::string msg;
+};
 
 // Adopted from https://stackoverflow.com/questions/865668/parsing-command-line-arguments-in-c
 inline std::optional<std::string> GetCommandOption(char const* const* const begin, char const* const* const end,
@@ -15,6 +27,27 @@ inline std::optional<std::string> GetCommandOption(char const* const* const begi
     }
 
     return std::nullopt;
+}
+
+inline std::variant<PathConfig, CliErrorMsg> ParseCommandLineInput(int const argc, char const* const argv[]) {
+    auto const config_path{GetCommandOption(argv, argv + argc, "--config")};
+    if (not config_path) {
+        return CliErrorMsg{"Missing --config flag"};
+    }
+
+    auto const data_path{GetCommandOption(argv, argv + argc, "--data")};
+    if (not data_path) {
+        return CliErrorMsg{"Missing --data flag"};
+    }
+
+    PathConfig path_config{*config_path, *data_path, ""};
+
+    // If a workspace directory is provided use it, otherwise just use the directory where the data is.
+    auto const workspace_dir{GetCommandOption(argv, argv + argc, "--workspace")};
+    path_config.workspace_dir =
+        workspace_dir ? std::filesystem::path{*workspace_dir} : path_config.data_path.parent_path();
+
+    return path_config;
 }
 
 }  // namespace reprojection::application
--- code/library/application/test/cli_utils.test.cpp
+++ code/library/application/test/cli_utils.test.cpp
@@ -6,6 +6,29 @@
 
 using namespace reprojection;
 
+TEST(ApplicationCliUtils, TestParseCommandLineInput) {
+    auto result{application::ParseCommandLineInput(0, nullptr)};
+    ASSERT_TRUE(std::holds_alternative<application::CliErrorMsg>(result));
+    EXPECT_EQ(std::get<application::CliErrorMsg>(result).msg, "Missing --config flag");
+
+    char const arg0[]{"program"};
+    char const arg1[]{"--config"};
+    char const arg2[]{"tmp/config.toml"};
+    char const arg3[]{"--data"};
+    char const arg4[]{"tmp/data.bag"};
+
+    char const* const argv[]{arg0, arg1, arg2, arg3, arg4};
+    int const argc{5};
+
+    result = application::ParseCommandLineInput(argc,argv);
+    ASSERT_TRUE(std::holds_alternative<application::PathConfig>(result));
+
+    auto const paths{std::get<application::PathConfig>(result)};
+    EXPECT_EQ(paths.config_path, "tmp/config.toml");
+    EXPECT_EQ(paths.data_path, "tmp/data.bag");
+    EXPECT_EQ(paths.workspace_dir, "tmp");
+}
+
 TEST(ApplicationCliUtils, TestGetCommandOption) {
     auto result{application::GetCommandOption(nullptr, nullptr, "")};
     EXPECT_FALSE(result.has_value());
