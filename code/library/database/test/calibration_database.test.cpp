dc940939d065015a547ecafee848c1d188d9883c
reprojection.calibration <reprojection.calibration@gmail.com>
2026-04-13 10:24:32 -0400

    DatabaseCalibrationDatabase

--- code/library/database/test/calibration_database.test.cpp
+++ code/library/database/test/calibration_database.test.cpp
@@ -14,26 +14,26 @@ TEST(DatabaseCalibrationDatabase, TestCreate) {
     TemporaryFile const temp_file{".db3"};
 
     // Cannot create a database when read_only is true - creating a database requires writing to it!
-    EXPECT_THROW(database::CalibrationDatabase(temp_file.Path(), true, true), std::runtime_error);
+    EXPECT_THROW(database::OpenCalibrationDatabase(temp_file.Path(), true, true), std::runtime_error);
 
     // Cannot open a non-existent database
-    EXPECT_THROW(database::CalibrationDatabase(temp_file.Path(), false), std::runtime_error);
+    EXPECT_THROW(database::OpenCalibrationDatabase(temp_file.Path(), false), std::runtime_error);
 
     // Create a database (found on the filesystem) and then check that we can open it
-    database::CalibrationDatabase(temp_file.Path(), true);
-    EXPECT_NO_THROW(database::CalibrationDatabase(temp_file.Path(), false));
+    database::OpenCalibrationDatabase(temp_file.Path(), true);
+    EXPECT_NO_THROW(database::OpenCalibrationDatabase(temp_file.Path(), false));
 }
 
 TEST(DatabaseCalibrationDatabase, TestReadWrite) {
     TemporaryFile const temp_file{".db3"};
-    database::CalibrationDatabase(temp_file.Path(), true);
+    database::OpenCalibrationDatabase(temp_file.Path(), true);
 
-    EXPECT_NO_THROW(database::CalibrationDatabase(temp_file.Path(), false, false));
+    EXPECT_NO_THROW(database::OpenCalibrationDatabase(temp_file.Path(), false, false));
 }
 
 TEST(DatabaseCalibrationDatabase, TestReadOnly) {
     TemporaryFile const temp_file{".db3"};
-    database::CalibrationDatabase(temp_file.Path(), true);
+    database::OpenCalibrationDatabase(temp_file.Path(), true);
 
-    EXPECT_NO_THROW(database::CalibrationDatabase(temp_file.Path(), false, true));
+    EXPECT_NO_THROW(database::OpenCalibrationDatabase(temp_file.Path(), false, true));
 }
