eb3875498243dc8a4b3f3cbf5cf0f450afa1760a
reprojection.calibration <reprojection.calibration@gmail.com>
2026-04-16 11:53:32 +0200

    initial ImageLoadingStep commit

---
+++ code/library/steps/include/steps/image_loading.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "database/calibration_database.hpp"
+#include "database/image_interface.hpp"
+#include "types/calibration_types.hpp"
+#include "types/io.hpp"
+
+namespace reprojection::steps {
+
+struct ImageLoadingStep {
+    std::string sensor_name;
+    std::string cache_key;
+    ImageSource image_source;
+
+    CalibrationStep step_type{CalibrationStep::ImageLoading};
+
+    std::string SensorName() const { return sensor_name; }
+
+    std::string CacheKey() const;
+
+    EncodedImages Compute() const;
+
+    EncodedImages Load(SqlitePtr const db) const;
+
+    void Save(EncodedImages const& images, SqlitePtr const db) const;
+};
+
+}  // namespace reprojection::steps
---
+++ code/library/steps/src/image_loading.cpp
@@ -0,0 +1,41 @@
+#include "steps/image_loading.hpp"
+
+#include "caching/cache_keys.hpp"
+
+namespace reprojection::steps {
+
+// TODO(Jack): The name of the class variable "cache_key" is misleading because it is not a cache key but really a
+// serialized data signature. We should fix this name to clarify its purpose and use.
+std::string ImageLoadingStep::CacheKey() const { return caching::CacheKey(cache_key); }
+
+EncodedImages ImageLoadingStep::Compute() const {
+    EncodedImages encoded_images;
+    while (auto const data{image_source()}) {
+        auto const& [timestamp_ns, img]{*data};
+
+        std::vector<uchar> buffer;
+        if (not cv::imencode(".png", img, buffer)) {
+            throw std::runtime_error("cv::imencode() failed for " + std::string(sensor_name));  // LCOV_EXCL_LINE
+        }
+
+        encoded_images.insert({timestamp_ns, ImageBuffer{buffer}});  // LCOV_EXCL_LINE
+    }
+
+    return encoded_images;
+}
+
+// IMPLEMENT!!!
+// IMPLEMENT!!!
+EncodedImages ImageLoadingStep::Load(SqlitePtr const db) const {
+    (void)db;
+    return {};
+}
+
+// IMPLEMENT!!!
+// IMPLEMENT!!!
+void ImageLoadingStep::Save(EncodedImages const& encoded_images, SqlitePtr const db) const {
+    (void)encoded_images;
+    (void)db;
+}
+
+}  // namespace reprojection::steps
