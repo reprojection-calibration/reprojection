53eec7229fa1fc26ffcf592df3ca2cb2b3d3a39a
reprojection.calibration <reprojection.calibration@gmail.com>
2026-04-16 12:47:11 +0200

    make images select all default

--- code/sql/images_select.sql
+++ code/sql/images_select.sql
@@ -2,5 +2,4 @@ SELECT timestamp_ns,
        data
 FROM images
 WHERE sensor_name = ?
-  AND timestamp_ns >= ?
 ORDER BY timestamp_ns ASC;
--- code/sql/images_select_all.sql
+++
@@ -1,5 +0,0 @@
-SELECT sensor_name,
-       timestamp_ns,
-       data
-FROM images
-ORDER BY timestamp_ns ASC;
