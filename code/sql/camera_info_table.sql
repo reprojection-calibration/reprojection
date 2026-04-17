33b770e8a31ec31687c598f9527a268fb56e2874
reprojection-calibration <reprojection.calibration@gmail.com>
2026-04-17 16:35:29 +0200

    undoo camera info foreign key cascade delete table refactor

--- code/sql/camera_info_table.sql
+++ code/sql/camera_info_table.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS camera_info
 (
-    step_name    TEXT CHECK ( step_name IN ('camera_info')),
     sensor_name  TEXT UNIQUE NOT NULL,
     camera_model TEXT CHECK ( camera_model IN ('double_sphere',
                                                'pinhole',
@@ -8,7 +7,5 @@ CREATE TABLE IF NOT EXISTS camera_info
                                                'unified_camera_model')),
     height       INTEGER     NOT NULL,
     width        INTEGER     NOT NULL,
-    PRIMARY KEY (sensor_name, camera_model),
-    FOREIGN KEY (step_name, sensor_name) REFERENCES calibration_steps (step_name, sensor_name)
-        ON DELETE CASCADE
+    PRIMARY KEY (sensor_name, camera_model)
 );
