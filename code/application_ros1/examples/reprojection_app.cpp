a0a8952c12924e0e71c15c447f5f5fd021af01f7
reprojection.calibration <reprojection.calibration@gmail.com>
2026-04-21 12:28:52 +0200

    refactor ros1 app to compile and run tests

--- code/application_ros1/examples/reprojection_app.cpp
+++ code/application_ros1/examples/reprojection_app.cpp
@@ -1,11 +1,10 @@
 #include <cstdlib>
 #include <iostream>
 
-// WARN(Jack): We should be able to include this like #include <reprojection/application/io.hpp> but the install paths
+// WARN(Jack): We should be able to include this like #include <reprojection/application/*.hpp> but the install paths
 // are not working like we want! We need to take a look at this. We want this so we can prevent file name collisions and
 // simply make it more clear where this comes from.
-#include <application/calibrate.hpp>
-#include <application/io.hpp>
+#include <application/reprojection_calibration.hpp>
 
 #include "application_ros1/bag_wrapper.hpp"
 #include "application_ros1/reprojection.hpp"
@@ -13,26 +12,15 @@
 using namespace reprojection;
 
 int main(int argc, char* argv[]) {
-    auto const paths{application::ParseCommandLineInput(argc, argv)};
-    if (not paths) {
+    auto const app_args{application::ParseArgs(argc, argv)};
+    if (not app_args) {
         return EXIT_FAILURE;
     }
 
-    auto const config{application::LoadAndValidateConfig(paths->config_path)};
-    if (not config) {
-        return EXIT_FAILURE;
-    }
-
-    auto const db{application::Open(paths->workspace_dir, paths->data_path)};
-    if (not db) {
-        return EXIT_FAILURE;
-    }
-
-    toml::table const config_table{*config};
     // TODO(Jack): Should we use the generic templated key access function found in the library?
-    std::string const camera_topic{*config_table["sensor"]["camera_name"].value<std::string>()};
+    std::string const camera_topic{*app_args->config["sensor"]["camera_name"].value<std::string>()};
 
-    auto const reader_result{ros1::SingleTopicBagReader::Create(paths->data_path, camera_topic)};
+    auto const reader_result{ros1::SingleTopicBagReader::Create(app_args->data_path, camera_topic)};
     if (std::holds_alternative<ros1::BagError>(reader_result)) {
         // TODO(Jack): Should we use the libraries logging pattern here too?
         std::cerr << std::get<ros1::BagError>(reader_result).message << "\n";
@@ -48,7 +36,7 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
 
-    application::Calibrate(config_table, image_source, *data_signature, *db);
+    application::Calibrate(app_args->config, image_source, *data_signature, app_args->db);
 
     std::cout << "The future is calibrated!\n";
 
