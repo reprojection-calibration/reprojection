800be55f1cfb184ec4246b17f5afa62a24fab87e
reprojection.calibration <reprojection.calibration@gmail.com>
2026-04-21 12:39:32 +0200

    refactor ros2 app to compile and run tests

--- code/application_ros2/examples/reprojection_app.cpp
+++ code/application_ros2/examples/reprojection_app.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
-#include <application/calibrate.hpp>
-#include <application/io.hpp>
+#include <application/reprojection_calibration.hpp>
 #include <toml++/impl/table.hpp>
 
 #include "application_ros2/reprojection.hpp"
@@ -13,23 +12,12 @@ using namespace reprojection;
 // TODO(Jack): Should we use the generic templated toml key access function found in the library?
 
 int main(int argc, char* argv[]) {
-    auto const paths{application::ParseCommandLineInput(argc, argv)};
-    if (not paths) {
+    auto const app_args{application::ParseArgs(argc, argv)};
+    if (not app_args) {
         return EXIT_FAILURE;
     }
 
-    auto const config{application::LoadAndValidateConfig(paths->config_path)};
-    if (not config) {
-        return EXIT_FAILURE;
-    }
-
-    auto const db{application::Open(paths->workspace_dir, paths->data_path)};
-    if (not db) {
-        return EXIT_FAILURE;
-    }
-
-    toml::table const config_table{*config};
-    std::string const camera_topic{*config_table["sensor"]["camera_name"].value<std::string>()};
+    std::string const camera_topic{*app_args->config["sensor"]["camera_name"].value<std::string>()};
 
     // NOTE(Jack): We want to control the terminal output of our program entirely. But ROS loves to log so we need to
     // manually set the log level to the highest possible level in an effort to prevent ROS logging for normal errors
@@ -40,7 +28,7 @@ int main(int argc, char* argv[]) {
     //  ImageSource needs to take and store a mutable reference. This is not really a huge problem but alone the fact
     //  that we have a non owning mutable reference inside of the ImageSource is a sign we are not doing something
     //  right.
-    auto reader_result{ros2::SingleTopicBagReader::Create(paths->data_path, camera_topic)};
+    auto reader_result{ros2::SingleTopicBagReader::Create(app_args->data_path, camera_topic)};
     if (std::holds_alternative<ros2::BagError>(reader_result)) {
         std::cerr << std::get<ros2::BagError>(reader_result).message << "\n";
         return EXIT_FAILURE;
@@ -55,7 +43,7 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
 
-    application::Calibrate(config_table, image_source, *data_signature, *db);
+    application::Calibrate(app_args->config, image_source, *data_signature, app_args->db);
 
     std::cout << "The future is calibrated!\n";
 
