ba5bb2fa50d86635ab01ce2f7c13d24b933a0575
reprojection.calibration <reprojection.calibration@gmail.com>
2026-04-22 16:04:58 +0200

    minor formatting

--- code/sql/camera_info_table.sql
+++ code/sql/camera_info_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS camera_info
 (
-    step_name TEXT NOT NULL CHECK ( step_name IN ('camera_info')),
+    step_name    TEXT        NOT NULL CHECK ( step_name IN ('camera_info')),
     sensor_name  TEXT UNIQUE NOT NULL,
     camera_model TEXT CHECK ( camera_model IN ('double_sphere',
                                                'pinhole',
@@ -8,8 +8,6 @@ CREATE TABLE IF NOT EXISTS camera_info
                                                'unified_camera_model')),
     height       INTEGER     NOT NULL,
     width        INTEGER     NOT NULL,
-    PRIMARY KEY (sensor_name, camera_model)
-    FOREIGN KEY (step_name, sensor_name)
-        REFERENCES calibration_steps (step_name, sensor_name)
-        ON DELETE CASCADE
+    PRIMARY KEY (sensor_name, camera_model),
+    FOREIGN KEY (step_name, sensor_name) REFERENCES calibration_steps (step_name, sensor_name) ON DELETE CASCADE
 );
