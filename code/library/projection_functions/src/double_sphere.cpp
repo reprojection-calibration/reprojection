9e3d8150bd6a7e3899b208d9d53e98ded5a3ad6a
reprojection.calibration <reprojection.calibration@gmail.com>
2026-04-29 21:50:05 +0200

    fix ds unproject

--- code/library/projection_functions/src/double_sphere.cpp
+++ code/library/projection_functions/src/double_sphere.cpp
@@ -15,7 +15,7 @@ std::optional<Array3d> DoubleSphere::Unproject(Eigen::Array<double, Size, 1> con
     double const& my{ray.value()[1]};
     double const r2{mx * mx + my * my};
 
-    double const& alpha{intrinsics[5]};
+    double const& alpha{intrinsics[4]};
 
     // (Eqn. 51)
     bool const valid{alpha <= 0.5 ? true : r2 < 1.0 / (2 * alpha - 1)};
@@ -25,7 +25,7 @@ std::optional<Array3d> DoubleSphere::Unproject(Eigen::Array<double, Size, 1> con
 
     double const mz{(1 - alpha * alpha * r2) / (alpha * std::sqrt(1 - (2 * alpha - 1.0) * r2) + 1 - alpha)};  // Eqn. 50
 
-    double const& xi{intrinsics[4]};
+    double const& xi{intrinsics[3]};
     const double mz2{mz * mz};
     // TODO(Jack): What to name this thing?
     const double xxx{(mz * xi + std::sqrt(mz2 + (1 - xi * xi) * r2)) / (mz2 + r2)};  // Eqn. 46 fraction part
