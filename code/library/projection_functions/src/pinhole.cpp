c851250c928329e19909aabd215fa4c8c2f1ca38
reprojection.calibration <reprojection.calibration@gmail.com>
2026-04-29 21:49:26 +0200

    fix pinhole unproject

--- code/library/projection_functions/src/pinhole.cpp
+++ code/library/projection_functions/src/pinhole.cpp
@@ -13,13 +13,12 @@ std::optional<Array3d> Pinhole::Unproject(Eigen::Array<double, Size, 1> const& i
         return std::nullopt;
     }
 
-    double const& fx{intrinsics[0]};
-    double const& fy{intrinsics[1]};
-    double const& cx{intrinsics[2]};
-    double const& cy{intrinsics[3]};
+    double const& f{intrinsics[0]};
+    double const& cx{intrinsics[1]};
+    double const& cy{intrinsics[2]};
 
-    double const x_cam{(u - cx) / fx};
-    double const y_cam{(v - cy) / fy};
+    double const x_cam{(u - cx) / f};
+    double const y_cam{(v - cy) / f};
 
     // NOTE(Jack): Setting the z value here to 1 essentially captures the pure essence of the loss of
     // information/scale/depth associated with the projective transform. This returns a "ray" or direction, not a 3D
