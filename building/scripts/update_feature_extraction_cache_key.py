5dfe2836b29ea619d5923244a263ac5dc3017759
reprojection-calibration <reprojection.calibration@gmail.com>
2026-05-12 09:24:08 +0200

    black format

--- building/scripts/update_feature_extraction_cache_key.py
+++ building/scripts/update_feature_extraction_cache_key.py
@@ -21,7 +21,7 @@ conn.execute(
     INSERT INTO calibration_steps (step_name, sensor_name, cache_key)
     VALUES (?, ?, ?)
     """,
-    ('feature_extraction', '/cam0/image_raw', new_cache)
+    ("feature_extraction", "/cam0/image_raw", new_cache),
 )
 conn.commit()
 conn.close()
