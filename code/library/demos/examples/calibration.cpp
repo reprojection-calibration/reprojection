c85a157fe2141dacbfc7d3c1e1f97147ce3bfa47
reprojection.calibration <reprojection.calibration@gmail.com>
2026-05-12 13:15:36 +0200

    add video file smoke test

--- code/library/demos/examples/calibration.cpp
+++ code/library/demos/examples/calibration.cpp
@@ -38,5 +38,7 @@ int main(int argc, char* argv[]) {
 
     application::Calibrate(app_args->config, image_source, "", app_args->db);
 
+    std::cout << "The future is calibrated!\n";
+
     return EXIT_SUCCESS;
 }
