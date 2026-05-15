998d9e470d699f8892ace79261eb85c70f42578b
reprojection.calibration <reprojection.calibration@gmail.com>
2026-05-15 08:26:31 +0200

    add target info sql tables

---
+++ code/sql/target_info_insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO target_info (step_name, sensor_name, target_type, height, width, asymmetric)
+VALUES (?, ?, ?, ?, ?, ?);
--- code/sql/target_info_table.sql
+++ code/sql/target_info_table.sql
@@ -2,10 +2,10 @@ CREATE TABLE IF NOT EXISTS target_info
 (
     step_name   TEXT        NOT NULL CHECK ( step_name IN ('target_info')),
     sensor_name TEXT UNIQUE NOT NULL,
-    target_type TEXT CHECK ( target_type IN ('aprilgrid3', 'checkerboard', 'circle_grid')),
+    target_type TEXT        NOT NULL CHECK ( target_type IN ('aprilgrid3', 'checkerboard', 'circle_grid')),
     height      INTEGER     NOT NULL,
     width       INTEGER     NOT NULL,
-    asymmetric  INTEGER CHECK ( asymmetric IN (0, 1)),
+    asymmetric  INTEGER     NOT NULL CHECK ( asymmetric IN (0, 1)),
     PRIMARY KEY (sensor_name, target_type),
     FOREIGN KEY (step_name, sensor_name) REFERENCES calibration_steps ON DELETE CASCADE
 );
