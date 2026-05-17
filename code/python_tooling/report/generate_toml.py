5eb94d8631074bd67b0b5f3d81848026792f3e18
reprojection-calibration <reprojection.calibration@gmail.com>
2026-05-17 12:30:38 +0200

    camera_tomls

--- code/python_tooling/report/generate_toml.py
+++ code/python_tooling/report/generate_toml.py
@@ -34,22 +34,37 @@ def toml_to_intrinsic_array(intrinsics_str, camera_model):
     return intrinsics
 
 
-def write_sensor_tomls(camera_info, camera_intrinsics):
+def camera_tomls(camera_info, camera_intrinsics):
+    # NOTE(Jack): For now we only care about the "polished/final" intrinsics from the camera only nonlinear refinement
+    # step. This might change one day with the stereo or IMU calibration but that is future music :)
     refined_intrinsics = camera_intrinsics[
         camera_intrinsics["step_name"] == "camera_nonlinear_refinement"
         ]
 
-    for _, sensor in camera_info.iterrows():
+    output = []
+    for i, sensor in camera_info.iterrows():
         sensor_name = sensor["sensor_name"]
+        camera_intrinsic_row = refined_intrinsics[refined_intrinsics["sensor_name"] == sensor_name]
 
-        matches = refined_intrinsics[refined_intrinsics["sensor_name"] == sensor_name]
-
-        if matches.empty:
+        if camera_intrinsic_row.empty:
             continue
 
-        intrinsics = matches.iloc[0]["intrinsics"]
+        intrinsics_str = camera_intrinsic_row.iloc[0]["intrinsics"]
         camera_model = CameraModel(sensor["camera_model"])
-        print(toml_to_intrinsic_array(intrinsics, camera_model))
+        intrinsics_arr = toml_to_intrinsic_array(intrinsics_str, camera_model)
+
+        toml_text = (
+            f'[cam{i}]\n'
+            f'sensor_id = "{sensor_name}"\n'
+            f'camera_model = "{sensor["camera_model"]}"\n'
+            f"intrinsics = {intrinsics_arr}\n"
+            f'height = {int(sensor["height"])}\n'
+            f'width = {int(sensor["width"])}\n'
+        )
+
+        output.append(toml_text)
+
+    return "\n".join(output)
 
 
 def main():
@@ -58,7 +73,10 @@ def main():
     camera_info = load_camera_info_table(db_path)
     camera_intrinsics = load_camera_intrinsics_table(db_path)
 
-    write_sensor_tomls(camera_info, camera_intrinsics)
+    result = camera_tomls(camera_info, camera_intrinsics)
+
+    with open("cameras.toml", "w") as f:
+        f.write(result)
 
 
 if __name__ == "__main__":
