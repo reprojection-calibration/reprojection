6dc0292669453f515f78acf03c6863250b423f82
reprojection.calibration <reprojection.calibration@gmail.com>
2026-05-18 17:01:42 +0200

    first attempt at SplineImuCostFunction

--- code/library/optimization/src/ceres_kinematics.hpp
+++ code/library/optimization/src/ceres_kinematics.hpp
@@ -8,8 +8,6 @@
 
 namespace reprojection::optimization {
 
-// a = C_i_b * (C_b_w * (a_w - g_w) + w_dot_b.cross(r_b) + w_b.cross(w_b.cross(r_b)))
-// TODO(Jack): This is almost exactly copy and pasted from ceres_geometry.hpp we should fix that!
 template <typename T>
 Vector3<T> TransformRigidBodyAcceleration(Eigen::Ref<Eigen::Vector<T, 6> const> const& tf_i_j,
                                           Eigen::Ref<Vector3<T> const> const& omega_j,
@@ -19,9 +17,9 @@ Vector3<T> TransformRigidBodyAcceleration(Eigen::Ref<Eigen::Vector<T, 6> const> 
     Vector3<T> const XXX{a_j + alpha_j.cross(r3) + omega_j.cross(omega_j.cross(r3))};  // NAMING!!!!
 
     Vector3<T> const aa_i_j{tf_i_j.template topRows<3>()};
-    Vector3<T> const ai{RotatePoint<T>(aa_i_j, XXX)};
+    Vector3<T> const a_i{RotatePoint<T>(aa_i_j, XXX)};  // Is RotatePoint the right name????
 
-    return ai;
+    return a_i;
 }
 
 }  // namespace  reprojection::optimization
--- code/library/optimization/src/spline_imu_cost_functions.hpp
+++ code/library/optimization/src/spline_imu_cost_functions.hpp
@@ -8,8 +8,68 @@
 #include "types/eigen_types.hpp"
 
 #include "ceres_geometry.hpp"
+#include "ceres_kinematics.hpp"
 
 namespace reprojection::optimization {
+
+class SplineImuCostFunction {
+   public:
+    SplineImuCostFunction(ImuData const& imu_data, double const u_i, uint64_t const delta_t_ns)
+        : imu_data_{imu_data}, u_i_{u_i}, delta_t_ns_{delta_t_ns} {}
+
+    // TODO MAKE FULL 6D TF!!!
+    template <typename T>
+    bool operator()(T const* const tf_imu_co, T const* const gravity_w, T const* const control_point_0_ptr,
+                    T const* const control_point_1_ptr, T const* const control_point_2_ptr,
+                    T const* const control_point_3_ptr, T* const residual) const {
+        // Map control point pointers into a usable control point matrix block.
+        std::array<T const* const, spline::constants::order> ptrs{control_point_0_ptr, control_point_1_ptr,
+                                                                  control_point_2_ptr, control_point_3_ptr};
+        spline::Matrix2NK<T> control_points;
+        for (int i{0}; i < spline::constants::order; ++i) {
+            control_points.col(i) = Eigen::Map<Eigen::Vector<T, 6> const>(ptrs[i], 6, 1);
+        }
+
+        // FIGURE OUT SCALE!!! 1e9
+        Vector3<T> const omega_co{1e9 * spline::So3Spline::Evaluate<T, spline::DerivativeOrder::First>(
+                                            control_points.template topRows<3>(), u_i_, delta_t_ns_)};
+        Eigen::Map<Eigen::Vector<T, 6> const> tf_imu_co_XXX(tf_imu_co);  // NAMING!!!!
+        Array3<T> const omega_imu{geometry::Exp<T>(tf_imu_co_XXX.template topRows<3>()) * omega_co};
+
+        residual[0] = T(imu_data_.angular_velocity[0]) - omega_imu[0];
+        residual[1] = T(imu_data_.angular_velocity[1]) - omega_imu[1];
+        residual[2] = T(imu_data_.angular_velocity[2]) - omega_imu[2];
+
+        Vector3<T> const aa_co_w{spline::So3Spline::Evaluate<T, spline::DerivativeOrder::Null>(
+            control_points.template topRows<3>(), u_i_, delta_t_ns_)};
+        Eigen::Map<Eigen::Vector<T, 3> const> gravity_XXX(gravity_w);
+        Vector3<T> const a_w{1e18 * spline::R3Spline::Evaluate<T, spline::DerivativeOrder::Second>(
+                                        control_points.template bottomRows<3>(), u_i_, delta_t_ns_)};  // NAMING!
+        Array3<T> const a_co{geometry::Exp<T>(aa_co_w.template topRows<3>()) * (gravity_XXX - a_w)};
+
+        // FIGURE OUT SCALE!!! 1e9
+        Vector3<T> const alpha_co{1e18 * spline::So3Spline::Evaluate<T, spline::DerivativeOrder::Second>(
+                                             control_points.template topRows<3>(), u_i_, delta_t_ns_)};
+
+        Vector3<T> const a_imu{TransformRigidBodyAcceleration<T>(tf_imu_co_XXX, omega_co, alpha_co, a_co)};
+
+        residual[3] = T(imu_data_.linear_acceleration[0]) - a_imu[0];
+        residual[4] = T(imu_data_.linear_acceleration[1]) - a_imu[1];
+        residual[5] = T(imu_data_.linear_acceleration[2]) - a_imu[2];
+
+        return true;
+    }
+
+    static ceres::CostFunction* Create(ImuData const& imu_data, double const u_i, uint64_t const delta_t_ns) {
+        return new ceres::AutoDiffCostFunction<SplineImuCostFunction, 6, 6, 3, 6, 6, 6, 6>(
+            new SplineImuCostFunction(imu_data, u_i, delta_t_ns));
+    }
+
+    ImuData imu_data_;
+
+    double u_i_;
+    uint64_t delta_t_ns_;
+};
 
 class SplineRotationalVelocityCostFunction {
    public:
