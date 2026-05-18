806817b54549cb5b33516e89fa8ca533bef9fccb
reprojection.calibration <reprojection.calibration@gmail.com>
2026-05-18 15:42:40 +0200

    TestTransformRigidBodyAccelerationHeuristic

--- code/library/optimization/src/ceres_kinematics.test.cpp
+++ code/library/optimization/src/ceres_kinematics.test.cpp
@@ -16,6 +16,7 @@ TEST(OptimizationCeresKinematics, TestTransformRigidBodyAccelerationIdentity) {
         Vector3d const a_j{Vector3d::Random()};
 
         Vector3d const a_i{optimization::TransformRigidBodyAcceleration<double>(tf_i_j, omega_j, alpha_j, a_j)};
+
         EXPECT_TRUE(a_i.isApprox(a_j));
     }
 }
@@ -32,6 +33,20 @@ TEST(OptimizationCeresKinematics, TestTransformRigidBodyAccelerationSameAxis) {
         Vector3d const a_j{Vector3d::Random()};
 
         Vector3d const a_i{optimization::TransformRigidBodyAcceleration<double>(tf_i_j, omega_j, alpha_j, a_j)};
+
         EXPECT_TRUE(a_i.isApprox(a_j));
     }
+}
+
+TEST(OptimizationCeresKinematics, TestTransformRigidBodyAccelerationHeuristic) {
+    // Random data with a heuristic expected result so we can hopefully catch regressions.
+    Vector6d const tf_i_j{0.1, 0.2, 0.3, 1, 2, 3};
+
+    Vector3d const omega_j{1, 1, 1};
+    Vector3d const alpha_j{1, 1, 1};
+    Vector3d const a_j{1, 2, 3};
+
+    Vector3d const a_i{optimization::TransformRigidBodyAcceleration<double>(tf_i_j, omega_j, alpha_j, a_j)};
+
+    EXPECT_TRUE(a_i.isApprox(Vector3d{4.8889657223403367, 1.4466322506082459, 0.072589925481057083}));
 }
