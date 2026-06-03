2f8396678082fba1345d952ce0907eb7989ccfae
reprojection.calibration <reprojection.calibration@gmail.com>
2026-06-03 19:41:05 +0200

    satisfy foreign key constraint in TEST(StepsSteps, TestExtrinsicInitialization)

--- code/library/steps/test/steps.test.cpp
+++ code/library/steps/test/steps.test.cpp
@@ -116,7 +116,16 @@ TEST(StepsSteps, TestExtrinsicInitialization) {
     // the process mechanics. But still it would be nice to get a "proper" result here so maybe we change this.
     auto const [imu_data, spline]{testing_mocks::GenerateImuData(100, 1'000'000'000)};
 
-    steps::ExtrinsicInitialization const step{"tf_co_imu", imu_data, spline};
+
+    // NOTE(Jack): That using the sensor_name for the extrinsic calibration does not make sense  because it describes
+    // the relation between two sensors and not just a property of one single sensor. But we need to do it like this to
+    // meet the foreign key requirements of the ImuErrors written to the database during the following extrinsic
+    // intiialization step.
+    std::string const sensor_name{"/imu/polaris/123"};
+    // Satisfy foreign key constraint.
+    database::InsertImuData(db, sensor_name, imu_data);
+
+    steps::ExtrinsicInitialization const step{sensor_name, imu_data, spline};
 
     // TODO(Jack): Define a type instead of just using std::pair<Array6d, Array3d>!!!
     auto [result, cache_status]{RunStep<std::pair<Array6d, Array3d>>(step, db)};
