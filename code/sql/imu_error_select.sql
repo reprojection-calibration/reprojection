8f79ce7a67888cf717ee808dfe6e4188f7be1453
reprojection.calibration <reprojection.calibration@gmail.com>
2026-06-03 13:18:51 +0200

    fix sql missing delta_

--- code/sql/imu_error_select.sql
+++ code/sql/imu_error_select.sql
@@ -1,4 +1,4 @@
-SELECT timestamp_ns, omega_x, omega_y, omega_z, ax, ay, az
+SELECT timestamp_ns, delta_omega_x, delta_omega_y, delta_omega_z, delta_ax, delta_ay, delta_az
 FROM imu_error
 WHERE step_name = ?
   AND sensor_name = ?
