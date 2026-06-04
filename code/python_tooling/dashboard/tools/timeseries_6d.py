825cbda43c92683ed1d8c40ea27534a264d74a44
reprojection.calibration <reprojection.calibration@gmail.com>
2026-06-04 09:21:42 +0200

    use error bars

--- code/python_tooling/dashboard/tools/timeseries_6d.py
+++ code/python_tooling/dashboard/tools/timeseries_6d.py
@@ -20,13 +20,10 @@ def timeseries_6d_to_patch(data, error):
     # TODO(Jack): Consider doing a relative error instead of an absolute error. Cause right now when the thing is moving
     #  slowly it looks ok but it might be a large relative error.
     if error is not None:
-        # Convert the timestamp that are the error dict keys to int (invert the json serialization effect).
         error_by_timestamp = {
             int(timestamp): value for timestamp, value in error.items()
         }
 
-        # Insert nans at any timestamps which does not exist in the errors. This makes sure the errors stays
-        # correspondent with the underlying data.
         error_array = np.array(
             [
                 error_by_timestamp.get(timestamp, [np.nan] * 6)
@@ -34,24 +31,16 @@ def timeseries_6d_to_patch(data, error):
             ]
         )
 
-        def add_error_trace(data, id, patch):
-            patch["data"][id]["marker"]["color"] = data[:, i]
-            patch["data"][id]["marker"]["colorscale"] = "RdBu"
-            # TODO(Jack): We should be able to set these ranges from the GUI
-            patch["data"][id]["marker"]["cmin"] = -1
-            patch["data"][id]["marker"]["cmax"] = 1
-
-            # We only need to plot one scale bar
-            if id == 0:
-                patch["data"][id]["marker"]["showscale"] = True
-                patch["data"][id]["marker"]["colorbar"] = {
-                    "title": {"text": "Signed Error"},
-                    "x": 1.1,
-                    "y": 0.5,
-                    "len": 1,
-                }
-
         for i in range(6):
-            add_error_trace(error_array, i, patch)
+            patch["data"][i]["error_y"] = {
+                "color":"red",
+                "type": "data",
+                "symmetric": False,
+                "array": np.maximum(error_array[:, i], 0.0),
+                "arrayminus": np.maximum(-error_array[:, i], 0.0),
+                "visible": True,
+                "thickness": 1,
+                "width": 0,
+            }
 
     return patch
