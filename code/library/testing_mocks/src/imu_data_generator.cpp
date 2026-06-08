69ce5ec978af4928cb289febffbb3d4f258e86f2
reprojection-calibration <reprojection.calibration@gmail.com>
2026-06-08 07:26:51 +0200

    invert imu pose test data gen spline

--- code/library/testing_mocks/src/imu_data_generator.cpp
+++ code/library/testing_mocks/src/imu_data_generator.cpp
@@ -7,8 +7,14 @@
 namespace reprojection::testing_mocks {
 
 std::pair<ImuMeasurements, spline::Se3Spline> GenerateImuData(int const num_samples, uint64_t const timespan_ns) {
-    spline::Se3Spline const trajectory{TimedSphereTrajectorySpline(5 * num_samples, timespan_ns)};
+    spline::Se3Spline trajectory{TimedSphereTrajectorySpline(5 * num_samples, timespan_ns)};
     std::set<uint64_t> const times{SampleTimes(num_samples, timespan_ns)};
+
+    // TODO KEEP THIS?
+    for (int i{0}; i < trajectory.Size(); ++i) {
+        trajectory.MutableControlPoints().col(i) =
+            geometry::Log(geometry::Exp(trajectory.MutableControlPoints().col(i)).inverse());
+    }
 
     ImuMeasurements imu_data;
     for (auto const time_i : times) {
