3093ccad1a4d21b4f8959ce5f1123e2243f94b68
reprojection-calibration <reprojection.calibration@gmail.com>
2026-06-09 08:35:49 +0200

    update imu data table to have step dependency

--- code/sql/imu_data_table.sql
+++ code/sql/imu_data_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS imu_data
 (
+    step_name    TEXT    NOT NULL CHECK ( step_name IN ('imu_data_loading')),
     sensor_name  TEXT    NOT NULL,
     timestamp_ns INTEGER NOT NULL,
     omega_x      REAL    NOT NULL,
@@ -8,5 +9,7 @@ CREATE TABLE IF NOT EXISTS imu_data
     ax           REAL    NOT NULL,
     ay           REAL    NOT NULL,
     az           REAL    NOT NULL,
+
+    FOREIGN KEY (step_name, sensor_name) REFERENCES calibration_steps (step_name, entity_id) ON DELETE CASCADE,
     PRIMARY KEY (sensor_name, timestamp_ns)
 );
