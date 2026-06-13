e3de82fd036cddb19dc85496d29c0d2467c28c13
reprojection-calibration <reprojection.calibration@gmail.com>
2026-06-13 13:44:32 +0200

    seems to work for camera frame :)

--- code/library/calibration/src/camera_imu_initialization.cpp
+++ code/library/calibration/src/camera_imu_initialization.cpp
@@ -14,32 +14,33 @@ namespace reprojection::calibration {
 using namespace spline;
 
 // TODO(Jack): Unit test!
-Vector3d EstimateGravity(CubicBSplineC3 const& camera_orientation, AccelerationMeasurements const& imu_acceleration,
+Vector3d EstimateGravity(CubicBSplineC3 const& so3_co_w, AccelerationMeasurements const& acc_imu,
                          Matrix3d const& R_imu_co) {
-    (void)R_imu_co;  // USE!!!!
-
     // Calculate the camera orientation required to transform each linear acceleration into the camera world frame.
-    PositionMeasurements so3_w_b;
-    for (uint64_t const timestamp_ns : imu_acceleration | std::views::keys) {
-        auto const so3_i_b_w{EvaluateSpline<So3Spline>(camera_orientation, timestamp_ns, DerivativeOrder::Null)};
-        if (so3_i_b_w.has_value()) {
-            so3_w_b.insert({timestamp_ns, {so3_i_b_w.value()}});
+    PositionMeasurements aa_co_w;
+    for (uint64_t const timestamp_ns : acc_imu | std::views::keys) {
+        auto const so3_i_co_w{EvaluateSpline<So3Spline>(so3_co_w, timestamp_ns, DerivativeOrder::Null)};
+        if (so3_i_co_w.has_value()) {
+            aa_co_w.insert({timestamp_ns, {so3_i_co_w.value()}});
         }
     }
 
     // Transform the imu acceleration into the world frame and store it. This assumes zero translation between camera
     // and imu, of course not true but is acceptable approximation for small translations.
-    MatrixXd acceleration_w(std::size(so3_w_b), 3);
-    for (int i{0}; auto const& [timestamp_ns, so3_i_w_b] : so3_w_b) {
-        Matrix3d const R_w_b{geometry::Exp(so3_i_w_b.position)};
+    MatrixXd acceleration_w(std::size(aa_co_w), 3);
+    for (int i{0}; auto const& [timestamp_ns, so3_i_co_w] : aa_co_w) {
+        Matrix3d const R_co_w{geometry::Exp(so3_i_co_w.position)};
+        Matrix3d const R_w_co{R_co_w.inverse()};
+        Matrix3d const R_co_imu{R_imu_co.inverse()};
 
-        Vector3d const& acceleration_i_b{imu_acceleration.at(timestamp_ns).acceleration};
+        Vector3d const& acc_i_imu{acc_imu.at(timestamp_ns).acceleration};
 
-        Vector3d const acceleration_i_w{R_w_b * acceleration_i_b};
+        Vector3d const acceleration_i_w{R_w_co * R_co_imu * acc_i_imu};
         acceleration_w.row(i) = acceleration_i_w;
 
         i += 1;
     }
+
 
     Vector3d const net_acceleration_w{acceleration_w.colwise().mean()};
     double constexpr g{9.80665};
--- code/library/calibration/src/camera_imu_initialization.test.cpp
+++ code/library/calibration/src/camera_imu_initialization.test.cpp
@@ -2,7 +2,9 @@
 
 #include <gtest/gtest.h>
 
+#include "spline/spline_initialization.hpp"
 #include "testing_mocks/data_generators.hpp"
+#include "testing_utilities/constants.hpp"
 
 using namespace reprojection;
 
@@ -10,15 +12,22 @@ using namespace reprojection;
 // Matrix3d const& R_imu_co)
 
 TEST(CalibrationCameraImuInitialization, TestEstimateGravity) {
-    auto const [imu_data, spline]{testing_mocks::GenerateImuData(60, 10)};
+    double const duration_s{10};
+    auto const [imu_data, _]{testing_mocks::GenerateImuData(duration_s, 20)};
 
-    auto const orientation_spline{spline::CubicBSplineC3{spline.So3(), spline.GetTimeHandler()}};
+    CameraInfo const sensor{"", CameraModel::Pinhole, testing_utilities::image_bounds};
+    CameraState const gt_intrinsics{testing_utilities::pinhole_intrinsics};
+    auto const [_1, tf_co_w]{testing_mocks::GenerateMvgData(sensor, gt_intrinsics, duration_s, 10)};
+
+    spline::Se3Spline const spline_co_w{spline::InitializeSe3SplineState(tf_co_w, 50)};
+
+    auto const so3_co_w{spline::CubicBSplineC3{spline_co_w.So3(), spline_co_w.GetTimeHandler()}};
     auto imu_linear_acceleration{calibration::ExtractLinearAcceleration(imu_data)};
-    Matrix3d const R_imu_co{Matrix3d::Identity()};
+    static Matrix3d const R_co_imu{{0, -1, 0}, {0, 0, -1}, {1, 0, 0}};
+    static Matrix3d const R_imu_co{R_co_imu.inverse()};
 
 
-    Vector3d const gravity_w{calibration::EstimateGravity(orientation_spline, imu_linear_acceleration, R_imu_co)};
-
+    Vector3d const gravity_w{calibration::EstimateGravity(so3_co_w, imu_linear_acceleration, R_imu_co)};
     std::cout << gravity_w << std::endl;
 
     EXPECT_EQ(1, 2);
