2cd735145ccc942c752ff9449c9f23eea2a621ec
reprojection-calibration <reprojection.calibration@gmail.com>
2026-06-13 13:58:23 +0200

    TEST(CalibrationCameraImuInitialization, TestEstimateGravity)

--- code/library/calibration/src/camera_imu_initialization.test.cpp
+++ code/library/calibration/src/camera_imu_initialization.test.cpp
@@ -8,27 +8,23 @@
 
 using namespace reprojection;
 
-// Vector3d EstimateGravity(CubicBSplineC3 const& camera_orientation, AccelerationMeasurements const& imu_acceleration,
-// Matrix3d const& R_imu_co)
-
 TEST(CalibrationCameraImuInitialization, TestEstimateGravity) {
     double const duration_s{10};
     auto const [imu_data, _]{testing_mocks::GenerateImuData(duration_s, 20)};
 
     CameraInfo const sensor{"", CameraModel::Pinhole, testing_utilities::image_bounds};
     CameraState const gt_intrinsics{testing_utilities::pinhole_intrinsics};
-    auto const [_1, tf_co_w]{testing_mocks::GenerateMvgData(sensor, gt_intrinsics, duration_s, 10)};
+    auto const [_1, camera_poses]{testing_mocks::GenerateMvgData(sensor, gt_intrinsics, duration_s, 10)};
 
-    spline::Se3Spline const spline_co_w{spline::InitializeSe3SplineState(tf_co_w, 50)};
+    spline::Se3Spline const spline_co_w{spline::InitializeSe3SplineState(camera_poses, 50)};
 
     auto const so3_co_w{spline::CubicBSplineC3{spline_co_w.So3(), spline_co_w.GetTimeHandler()}};
     auto imu_linear_acceleration{calibration::ExtractLinearAcceleration(imu_data)};
     static Matrix3d const R_co_imu{{0, -1, 0}, {0, 0, -1}, {1, 0, 0}};
     static Matrix3d const R_imu_co{R_co_imu.inverse()};
 
-
     Vector3d const gravity_w{calibration::EstimateGravity(so3_co_w, imu_linear_acceleration, R_imu_co)};
-    std::cout << gravity_w << std::endl;
 
-    EXPECT_EQ(1, 2);
+    Vector3d const gt_gravity_w{0.066929787601231625, -0.058423503100995429, 9.8062475657265065};
+    EXPECT_TRUE(gravity_w.isApprox(gt_gravity_w));
 }
