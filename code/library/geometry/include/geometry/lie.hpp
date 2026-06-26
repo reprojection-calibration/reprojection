9c2795c5e5190492dc652328fa52b7755c3a4de0
reprojection-calibration <reprojection.calibration@gmail.com>
2026-06-26 08:43:19 +0200

    reorder InverseTransform

--- code/library/geometry/include/geometry/lie.hpp
+++ code/library/geometry/include/geometry/lie.hpp
@@ -34,13 +34,12 @@ Vector3<T> Log(Matrix3<T> const& SO3) {
 template <typename T>
 Array6<T> InverseTransform(Array6<T> const& tf_a_b) {
     Array3<T> const aa_a_b{tf_a_b.template head<3>()};
-    Vector3<T> const p_a_b{tf_a_b.template tail<3>()};
-
     Matrix3<T> const R_a_b{Exp<T>(aa_a_b)};
     Matrix3<T> const R_b_a{R_a_b.transpose()};
 
     Array6<T> tf_b_a;
     tf_b_a.template head<3>() = Log<T>(R_b_a);
+    Vector3<T> const p_a_b{tf_a_b.template tail<3>()};
     tf_b_a.template tail<3>() = -R_b_a * p_a_b;
 
     return tf_b_a;
