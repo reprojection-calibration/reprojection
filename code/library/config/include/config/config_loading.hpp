ef91626e46875fd37293f798d7b7e07958c74b58
reprojection.calibration <reprojection.calibration@gmail.com>
2026-06-27 13:01:09 +0200

    factory pattern

--- code/library/config/include/config/config_loading.hpp
+++ code/library/config/include/config/config_loading.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <filesystem>
 #include <string>
 #include <variant>
 
@@ -11,29 +12,46 @@
 
 namespace reprojection::config {
 
-
-
 struct CalibrationConfig {
+    static CalibrationConfig Load(std::filesystem::path const& path);
+
+   private:
+    CalibrationConfig() = default;
+
+   public:
     struct Application {
+        static Application Load(toml::table cfg);
+
         bool show_extraction;
         int threads;
-        Workflow workflow;
     };
 
     struct Camera {
+        static Camera Load(toml::table cfg);
+
         std::string sensor_name;
         CameraModel camera_model;
     };
 
     struct Imu {
+        static Imu Load(toml::table cfg);
+
         std::string sensor_name;
     };
 
     struct Target {
+        static Target Load(toml::table cfg);
+
         std::array<int, 2> size;
         TargetType target_type;
-
     };
+
+    Workflow Workflow() const { return imu.has_value() ? Workflow::CameraImu : Workflow::Camera; }
+
+    Application app;
+    Camera camera;
+    std::optional<Imu> imu;
+    Target target;
 };
 
 std::variant<toml::table, TomlErrorMsg> LoadConfigFile(std::string const& file);
