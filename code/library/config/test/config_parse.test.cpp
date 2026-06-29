93116eca187f9435a4f7a9bc4ce45120271324f8
reprojection-calibration <reprojection.calibration@gmail.com>
2026-06-29 14:55:19 +0200

    app and camera parsing

--- code/library/config/test/config_parse.test.cpp
+++ code/library/config/test/config_parse.test.cpp
@@ -54,7 +54,7 @@ TEST(ConfigParsingHelpers, TestConfigParseMinimum) {
         pattern_size = [3,4]
     )"};
     toml::table const minimum_config{toml::parse(minimum_table)};
-    auto const result {config::Config::Parse(minimum_config)};
+    auto const result{config::Config::Parse(minimum_config)};
 
     EXPECT_EQ(result.application.show_extraction, false);
     EXPECT_GE(result.application.threads, 2);
@@ -69,4 +69,85 @@ TEST(ConfigParsingHelpers, TestConfigParseMinimum) {
     EXPECT_EQ(result.target.size[1], 4);
     EXPECT_EQ(result.target.unit_dimension, 1.0);
     EXPECT_EQ(result.target.asymmetric, false);
+}
+
+TEST(ConfigParsingHelpers, TestConfigApplicationParse) {
+    std::vector<std::string_view> const valid_tables{
+        R"()",
+        R"(
+            show_extraction = true
+            threads = 10
+        )",
+        R"(
+            show_extraction = true
+        )",
+        R"(
+            threads = 10
+        )",
+    };
+
+    for (auto const& valid_table : valid_tables) {
+        toml::table const config{toml::parse(valid_table)};
+
+        EXPECT_NO_THROW(config::Config::Application::Parse(config));
+    }
+
+    std::vector<std::string_view> const invalid_tables{
+        R"(
+            [application]
+        )",
+        R"(
+            show_extraction = "wrong_type"
+        )",
+        R"(
+            threads = "wrong_type"
+        )",
+        R"(
+            unexpected_key = "value1"
+        )",
+    };
+
+    for (auto const& invalid_table : invalid_tables) {
+        toml::table const config{toml::parse(invalid_table)};
+
+        EXPECT_THROW(config::Config::Application::Parse(config), std::runtime_error);
+    }
+}
+
+TEST(ConfigParsingHelpers, TestConfigCameraParse) {
+    std::vector<std::string_view> const valid_tables{
+        R"(
+            sensor_name = "/cam0/image_raw"
+            camera_model = "double_sphere"
+        )",
+    };
+
+    for (auto const& valid_table : valid_tables) {
+        toml::table const config{toml::parse(valid_table)};
+
+        EXPECT_NO_THROW(config::Config::Camera::Parse(config));
+    }
+
+    std::vector<std::string_view> const invalid_tables{
+        R"()",
+        R"(
+            [camera]
+        )",
+        R"(
+            sensor_name = "/cam0/image_raw"
+        )",
+        R"(
+            camera_model = "double_sphere"
+        )",
+        R"(
+            sensor_name = 1.2
+            camera_model = 2.1
+        )",
+    };
+
+    for (auto const& invalid_table : invalid_tables) {
+        toml::table const config{toml::parse(invalid_table)};
+
+        EXPECT_THROW(config::Config::Camera::Parse(config), std::runtime_error);
+    }
 }
