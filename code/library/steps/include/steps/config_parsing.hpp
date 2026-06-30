dbe4fcddc96362b36e68d65c879a5884e08963bf
reprojection-calibration <reprojection.calibration@gmail.com>
2026-06-30 12:18:33 +0200

    comment

--- code/library/steps/include/steps/config_parsing.hpp
+++ code/library/steps/include/steps/config_parsing.hpp
@@ -5,13 +5,12 @@
 
 namespace reprojection::steps {
 
-// TODO UPDATE
-// NOTE(Jack): The entity holds a special place in the calibration process in that it is not part of the regular
-// cachable step workflow process. That is readily apparent by the foreign key dependency of the calibration step
-// table on the entity ids found in the entity table. The most important implication of this is that the entity
-// table needs a custom read/write step like object. And in addition to that, instead of pure "insert" semantics
-// the entity use "insert or ignore" semantics because every time the calibration runs it will try to insert
-// all the entities that are present. And if it is already present that is not an error!
+// WARN(Jack): Config parsing is not a normal step! It holds the unique position of initializing the "entities". Because
+// of this it cannot be in the calibration_steps table because that table itself has a foreign key dependency on the
+// entities. It is also unique because as of now the config file is required to always be present which means that there
+// is no real reason/logical need to cache it here.  This piece of code is the root of the tree and does not fit well
+// into our current step paradigm. Time will tell if this was the right choice or not. It would be cool to have this be
+// a step like all others, but I can't think of how to do it.
 config::Config ConfigParsing(toml::table const& cfg_table, SqlitePtr const db);
 
 }  // namespace reprojection::steps
