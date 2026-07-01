3a9ff1eb73cd4cee6e868ecae216cd6de4176eab
reprojection-calibration <reprojection.calibration@gmail.com>
2026-07-01 12:10:51 +0200

    introduce REPROJECTION_SQL_DIR

--- code/python_tooling/database/sql_statement_loading.py
+++ code/python_tooling/database/sql_statement_loading.py
@@ -1,4 +1,13 @@
-from importlib.resources import files
+import os
+from pathlib import Path
+
+
+def sql_root():
+    value = os.environ.get("REPROJECTION_SQL_DIR")
+    if value is None:
+        raise RuntimeError("REPROJECTION_SQL_DIR is not set")
+
+    return Path(value)
 
 
 # NOTE(Jack): `code/sql/` is symlinked to the python database package folder `sql/`. This means we have a single
@@ -7,4 +16,4 @@ from importlib.resources import files
 # Turns out it did cause a lot of problems! For the docker workflow we need to COPY the .sql files directly into
 # the `python_tooling/database` package. See the comment there for more details.
 def load_sql(name):
-    return files("database.sql").joinpath(name).read_text()
+    return (sql_root() / name).read_text(encoding="utf-8")
