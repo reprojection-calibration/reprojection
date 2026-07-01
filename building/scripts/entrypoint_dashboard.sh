dc4cc0aa31320c38f4f42805aedc36948c48130e
reprojection-calibration <reprojection.calibration@gmail.com>
2026-07-01 12:11:23 +0200

    remove report gen from dashboard

--- building/scripts/entrypoint_dashboard.sh
+++ building/scripts/entrypoint_dashboard.sh
@@ -6,8 +6,4 @@ set -eou pipefail
 source /buildroot/.reprojection_venv/bin/activate
 
 # TODO(Jack): Do not hardcode the "/workspace" directory!
-# TODO(Jack): Allow the user to configure what they are doing here. For example they might not want to generate the
-# reports and run the dashboard. This clearly needs to be handled with a little more care and thought.
-
-python3 /temporary/code/python_tooling/report/run.py
 python3 /temporary/code/python_tooling/dashboard/run.py
