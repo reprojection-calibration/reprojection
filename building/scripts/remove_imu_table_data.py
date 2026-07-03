9eab7fdacacd22725660a5442b126ab999a7e9da
reprojection.calibration <reprojection.calibration@gmail.com>
2026-07-03 10:58:30 +0200

    test data processing comment and cleanup

--- building/scripts/remove_imu_table_data.py
+++ building/scripts/remove_imu_table_data.py
@@ -1,7 +1,12 @@
 import sqlite3
 import sys
 
-# NOTE(Jack): EXPLAIN WHY WE ARE DOING THIS!
+# NOTE(Jack): The checked in test database which we use for integration and smoke testing already has the IMU data
+# inside. This is a problem however for the smoke tests which want to test the entire extrinsic calibration workflow
+# (only the ROS application as of 03.07.2026). The problem is that during the smoke test the calibration application
+# trys to write the freshly loaded IMU data into the database which already has it. This of course cannot work.
+# Therefore, what we do here is remove the IMU data from the database. This means that when the ROS smoke tests try to
+# write the IMU data into the database there is no conflict and the test can process with no problem :)
 
 db_path = sys.argv[1]
 
@@ -13,9 +18,9 @@ except Exception as e:
 
 conn.execute(
     """
-    DELETE FROM imu_data;
+    DELETE
+    FROM imu_data;
     """
 )
 conn.commit()
 conn.close()
-
