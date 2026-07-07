7811172327f5a7b8b7282b932790e99d5e2a07b2
reprojection-calibration <reprojection.calibration@gmail.com>
2026-07-07 08:41:23 +0200

    refactor into a "build camera sections" function

--- code/python_tooling/report/build_pdf_report.py
+++ code/python_tooling/report/build_pdf_report.py
@@ -26,69 +26,76 @@ def run_report_export(workspace_dir):
             textwrap.indent(f"Name: {db_name}\nPath: {db_path}", "  "),
         )
 
-        camera_info = load_camera_info_table(db_path)
-        extracted_targets = load_extracted_targets_table(db_path)
-        reprojection_errors = load_reprojection_errors_table(db_path)
-
-        # TODO(Jack): Honestly all we really need to construct the coverage map is the extracted targets. That contains
-        # all the information we need. We should refactor this logic here to allow the creation of the most possible
-        # figures using limited or partial databases.
-        if (
-            camera_info is None
-            or extracted_targets is None
-            or reprojection_errors is None
-        ):
-            log.info(
-                "Skipping pdf report export - missing data:\n%s",
-                textwrap.indent(
-                    f"camera_info: {'N/A' if camera_info is None else 'loaded'}\nextracted_targets: {'N/A' if extracted_targets is None else 'loaded'}\nreprojection_errors: {'N/A' if reprojection_errors is None else 'loaded'}",
-                    "  ",
-                ),
-            )
-            continue
-
-        camera_info_map = camera_info.set_index("sensor_name").to_dict("index")
-
-        camera_sections = []
-        for sensor_name in extracted_targets["sensor_name"].unique():
-            print(f"\tProcessing sensor {sensor_name}")
-
-            camera_info_i = camera_info_map.get(sensor_name)
-            extracted_targets_i = extracted_targets[
-                extracted_targets["sensor_name"] == sensor_name
-            ]
-            coverage_figure_i = coverage_figure(camera_info_i, extracted_targets_i)
-
-            reprojection_errors_i = reprojection_errors[
-                reprojection_errors["sensor_name"] == sensor_name
-            ]
-            error_figure_i = error_figure(
-                camera_info_i,
-                extracted_targets_i,
-                reprojection_errors_i,
-                "bundle_adjustment",
-            )
-
-            camera_section_i = {
-                "sensor_name": sensor_name,
-                "rows": [
-                    (
-                        {
-                            "fig": coverage_figure_i,
-                            "caption": "Extracted target pixel coverage.",
-                        },
-                        {
-                            "fig": error_figure_i,
-                            "caption": "Reprojection error magnitude.",
-                        },
-                    ),
-                ],
-            }
-
-            camera_sections.append(camera_section_i)
+        # ERROR(Jack): What do we do if this is None???
+        camera_sections = build_camera_sections(db_path)
 
         output_name = db_name.removesuffix(".db3") + ".pdf"
         output_path = Path(workspace_dir) / output_name
 
         print(f"\tAssembling pdf and saving to {output_path}")
         build_two_column_pdf(output_path=output_path, camera_sections=camera_sections)
+
+
+def build_camera_sections(db_path):
+    camera_info = load_camera_info_table(db_path)
+    extracted_targets = load_extracted_targets_table(db_path)
+    reprojection_errors = load_reprojection_errors_table(db_path)
+
+    # TODO(Jack): Honestly all we really need to construct the coverage map is the extracted targets. That contains
+    # all the information we need. We should refactor this logic here to allow the creation of the most possible
+    # figures using limited or partial databases.
+    if (
+            camera_info is None
+            or extracted_targets is None
+            or reprojection_errors is None
+    ):
+        log.info(
+            "Skipping pdf report export - missing data:\n%s",
+            textwrap.indent(
+                f"camera_info: {'N/A' if camera_info is None else 'loaded'}\nextracted_targets: {'N/A' if extracted_targets is None else 'loaded'}\nreprojection_errors: {'N/A' if reprojection_errors is None else 'loaded'}",
+                "  ",
+            ),
+        )
+        return None
+
+    camera_info_map = camera_info.set_index("sensor_name").to_dict("index")
+
+    camera_sections = []
+    for sensor_name in extracted_targets["sensor_name"].unique():
+        print(f"\tProcessing sensor {sensor_name}")
+
+        camera_info_i = camera_info_map.get(sensor_name)
+        extracted_targets_i = extracted_targets[
+            extracted_targets["sensor_name"] == sensor_name
+            ]
+        coverage_figure_i = coverage_figure(camera_info_i, extracted_targets_i)
+
+        reprojection_errors_i = reprojection_errors[
+            reprojection_errors["sensor_name"] == sensor_name
+            ]
+        error_figure_i = error_figure(
+            camera_info_i,
+            extracted_targets_i,
+            reprojection_errors_i,
+            "bundle_adjustment",
+        )
+
+        camera_section_i = {
+            "sensor_name": sensor_name,
+            "rows": [
+                (
+                    {
+                        "fig": coverage_figure_i,
+                        "caption": "Extracted target pixel coverage.",
+                    },
+                    {
+                        "fig": error_figure_i,
+                        "caption": "Reprojection error magnitude.",
+                    },
+                ),
+            ],
+        }
+
+        camera_sections.append(camera_section_i)
+
+    return camera_sections
