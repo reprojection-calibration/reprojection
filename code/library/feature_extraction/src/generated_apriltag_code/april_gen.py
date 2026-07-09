ba7b6ebaaaa07b66c623950fc7151f0dec0f86e0
reprojection.calibration <reprojection.calibration@gmail.com>
2026-07-09 13:26:15 +0200

    update april_gen.py to have two black rings and only one white ring

--- code/library/feature_extraction/src/generated_apriltag_code/april_gen.py
+++ code/library/feature_extraction/src/generated_apriltag_code/april_gen.py
@@ -4,14 +4,14 @@ tag = np.full((6, 6), "d", dtype="U1")
 
 tag = np.pad(tag, (1), "constant", constant_values=("w"))
 tag = np.pad(tag, (1), "constant", constant_values=("b"))
+tag = np.pad(tag, (1), "constant", constant_values=("b"))
 
 tag = np.pad(tag, (1), "constant", constant_values=("w"))
-tag = np.pad(tag, (1), "constant", constant_values=("w"))
 
-tag[0:2, 0:2] = "b"
-tag[0:2, -2:] = "b"
-tag[-2:, 0:2] = "b"
-tag[-2:, -2:] = "b"
+tag[0:1, 0:1] = "b"
+tag[0:1, -1:] = "b"
+tag[-1:, 0:1] = "b"
+tag[-1:, -1:] = "b"
 
 print(tag)
 
