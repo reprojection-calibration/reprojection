47a493ce4be4675680664c3ff2c59867d62655ba
reprojection-calibration <reprojection.calibration@gmail.com>
2026-07-19 14:45:46 +0200

    add asset id to images table

--- code/sql2/images_table.sql
+++ code/sql2/images_table.sql
@@ -6,5 +6,6 @@ CREATE TABLE IF NOT EXISTS images
     image        BLOB,
 
     FOREIGN KEY (artifact_id) REFERENCES artifacts (id) ON DELETE CASCADE,
-    PRIMARY KEY (artifact_id, timestamp_ns)
+    FOREIGN KEY (asset_id) REFERENCES assets (id),
+    PRIMARY KEY (artifact_id, asset_id, timestamp_ns)
 );
