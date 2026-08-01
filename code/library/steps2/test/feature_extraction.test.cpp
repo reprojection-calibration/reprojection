aed63491bd51d1dd10224982209a37c4f1ff4f90
reprojection-calibration <reprojection.calibration@gmail.com>
2026-08-01 10:23:27 +0200

    TEST_F(FeatureExtractionFixture, TestFeatureExtractionStep)

--- code/library/steps2/test/feature_extraction.test.cpp
+++ code/library/steps2/test/feature_extraction.test.cpp
@@ -42,8 +42,24 @@ TEST_F(FeatureExtractionFixture, TestFeatureExtractionStepRunner) {
     auto const owner{steps::StepOwner::Recording(recording_id_)};
 
     steps::FeatureExtraction const step{camera_id_, image_loading_id_, false, target_info_id_, target_id_, db_};
-
     StepId const step_id{RunStep<steps::FeatureExtraction>(owner, step, db_)};
+
+    // TODO(Jack): This is kind of an anti climatic result but its not our responsibility to check that the feature
+    // extraction works here.
+    auto const result{db_.ExtractedTargetsSelect(step_id, camera_id_)};
+    EXPECT_EQ(std::size(result), 0);
+}
+
+
+TEST_F(FeatureExtractionFixture, TestFeatureExtractionStep) {
+    // Build the step and check that the type and hash function are correct.
+    steps::FeatureExtraction const step{camera_id_, image_loading_id_, false, target_info_id_, target_id_, db_};
+    EXPECT_EQ(step.Type(), StepType::FeatureExtraction);
+    EXPECT_EQ(step.CacheKey(db_).value, "cebb7a03270d515c4a1fe8be46ce42e546b3958e655f3af29197303d055e5b1a");
+
+    // Build the actual database step id and execute the step.
+    auto const [step_id, _]{db_.GetOrCreateStep(recording_id_, std::nullopt, StepType::FeatureExtraction, "")};
+    EXPECT_NO_THROW(step.Execute(step_id, db_));
 
     auto const result{db_.ExtractedTargetsSelect(step_id, camera_id_)};
     EXPECT_EQ(std::size(result), 0);
