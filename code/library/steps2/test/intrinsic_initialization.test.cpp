e7a57bec4465c0355153224f6fd299ffb775684c
reprojection-calibration <reprojection.calibration@gmail.com>
2026-08-03 12:59:57 +0200

    IntrinsicInitializationFixture

--- code/library/steps2/test/intrinsic_initialization.test.cpp
+++ code/library/steps2/test/intrinsic_initialization.test.cpp
@@ -8,41 +8,23 @@
 #include "testing_mocks/data_generators.hpp"
 #include "testing_utilities/constants.hpp"
 
+#include "test_fixture.hpp"
+
 using namespace reprojection;
 
-class IntrinsicInitializationFixture : public ::testing::Test {
+class IntrinsicInitializationFixture : public StepTestFixture {
    protected:
     void SetUp() override {
-        camera_info_id_ = db_.GetOrCreateStep(StepType::CameraInfo, "").first;
         CameraInfo const camera_info{CameraModel::DoubleSphere, testing_utilities::image_bounds};
-        db_.CameraInfoInsert(camera_info_id_, camera_id_, camera_info);
+        camera_info_id_ = InsertCameraInfo(camera_info);
 
-        // NOTE(Jack): The setup required here is pretty wild. The reason we need to do this is that we need targets to
-        // actually exercise the intrinsic init step. Getting these into the db means that we also need images in the db
-        // to satisfy the extracted target foreign key constraint. That is what this big mess here is doing. If we use
-        // this in multiple places put it in one fixture.
         auto const [targets, _]{
             testing_mocks::GenerateMvgData(camera_info, {testing_utilities::double_sphere_intrinsics}, 11, 1)};
-
-        EncodedImages const images{[&targets]() {
-            EncodedImages images;
-            for (auto const timestamp_ns : targets | std::ranges::views::keys) {
-                images.insert({timestamp_ns, {}});
-            }
-            return images;
-        }()};
-
-        StepId const image_loading_id{db_.GetOrCreateStep(StepType::ImageLoading, "").first};
-        db_.ImagesInsert(image_loading_id, camera_id_, images);
-
-        targets_id_ = db_.GetOrCreateStep(StepType::FeatureExtraction, "").first;
-        db_.ExtractedTargetsInsert(targets_id_, image_loading_id, camera_id_, targets);
+        targets_id_ = InsertExtractedTargets(targets);
     }
 
-    database::CalibrationDatabase db_{database::CalibrationDatabase(":memory:", true)};
-    AssetId camera_id_{db_.GetOrCreateAsset(AssetType::Camera, 0, "")};
-    StepId camera_info_id_{-1};
-    StepId targets_id_{-1};
+    StepId camera_info_id_;
+    StepId targets_id_;
 };
 
 TEST_F(IntrinsicInitializationFixture, TestIntrinsicInitializationStepRunner) {
