2051e3faa68fa71ab92a82d238b0df5543398d4a
reprojection-calibration <reprojection.calibration@gmail.com>
2026-08-03 06:41:20 +0200

    add target info logging

--- code/library/steps2/src/target_info.cpp
+++ code/library/steps2/src/target_info.cpp
@@ -1,8 +1,15 @@
 #include "steps/target_info.hpp"
 
 #include "hashing/hashing.hpp"
+#include "logging/logging.hpp"
 
 namespace reprojection::steps {
+
+namespace {
+
+auto const log{logging::Get("steps")};
+
+}
 
 TargetInfoStep::TargetInfoStep(AssetId target_id, config::Config::Target const& target)
     : target_id_{target_id}, target_{target} {}
@@ -12,6 +19,12 @@ Hash TargetInfoStep::CacheKey() const { return hashing::HashArguments(target_id_
 void TargetInfoStep::Execute(StepId step_id, database::CalibrationDatabase& db) const {
     TargetInfo const target_info{target_.target_type, target_.size[0], target_.size[1], target_.unit_dimension,
                                  target_.asymmetric};
+
+    log->debug(
+        "{{'step_id': '{}', 'asset_id': '{}', 'target_info': {{'target_type': {}, 'rows': {}, 'cols': {}, "
+        "'unit_dimension': {}, 'asymmetric': {}}}}}}}",
+        step_id.value, target_id_.value, ToString(target_info.target_type), target_info.height, target_info.width,
+        target_info.unit_dimension, target_info.asymmetric);
 
     db.TargetInfoInsert(step_id, target_id_, target_info);
 }
