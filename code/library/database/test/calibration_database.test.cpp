fee927213fe59182746f309d07dbb92c334c3d84
reprojection-calibration <reprojection.calibration@gmail.com>
2026-08-05 13:46:52 +0200

    refactor testing

--- code/library/database/test/calibration_database.test.cpp
+++ code/library/database/test/calibration_database.test.cpp
@@ -13,68 +13,68 @@ TEST(DatabaseCalibrationDatbase, TestGetOrCreateAsset) {
     auto db{database::OpenCalibrationDatabase(":memory:", true)};
 
     // Repeated insert returns the same id with no problems.
-    AssetId result{db.GetOrCreateAsset(AssetType::Camera, 0, "/cam0/image_raw")};
+    AssetId result{database::GetOrCreateAsset(db.get(), AssetType::Camera, 0, "/cam0/image_raw")};
     EXPECT_EQ(result, AssetId{1});
-    result = db.GetOrCreateAsset(AssetType::Camera, 0, "/cam0/image_raw");
+    result = database::GetOrCreateAsset(db.get(), AssetType::Camera, 0, "/cam0/image_raw");
     EXPECT_EQ(result, AssetId{1});
 
     // Add a second camera at index 1, no problemo.
-    result = db.GetOrCreateAsset(AssetType::Camera, 1, "/cam1/image_raw");
+    result = database::GetOrCreateAsset(db.get(), AssetType::Camera, 1, "/cam1/image_raw");
     EXPECT_EQ(result, AssetId{2});
 
     // Adding an imu with an already used index is also no problem,
-    result = db.GetOrCreateAsset(AssetType::Imu, 0, "/imu0");
+    result = database::GetOrCreateAsset(db.get(), AssetType::Imu, 0, "/imu0");
     EXPECT_EQ(result, AssetId{3});
 
     // Trying to insert an asset with a different name at an already existing index is a no-go!
-    EXPECT_THROW(db.GetOrCreateAsset(AssetType::Camera, 0, "/cam1/image_raw"), std::runtime_error);
+    EXPECT_THROW(database::GetOrCreateAsset(db.get(), AssetType::Camera, 0, "/cam1/image_raw"), std::runtime_error);
 }
 
 TEST(DatabaseCalibrationDatbase, TestGetOrCreateRecording) {
     auto db{database::OpenCalibrationDatabase(":memory:", true)};
 
     // Repeated insert with matching name and hash is no problem!
-    RecordingId result{db.GetOrCreateRecording("recording.bag", "sha256-xxx")};
+    RecordingId result{database::GetOrCreateRecording(db.get(), "recording.bag", "sha256-xxx")};
     EXPECT_EQ(result, RecordingId{1});
-    result = db.GetOrCreateRecording("recording.bag", "sha256-xxx");
+    result = database::GetOrCreateRecording(db.get(), "recording.bag", "sha256-xxx");
     EXPECT_EQ(result, RecordingId{1});
 
     // Adding another recording with a unique name and hash is no problem.
-    result = db.GetOrCreateRecording("recording1.bag", "sha256-yyy");
+    result = database::GetOrCreateRecording(db.get(), "recording1.bag", "sha256-yyy");
     EXPECT_EQ(result, RecordingId{2});
 
     // Trying to insert an existing name with a different hash is a no-go!
-    EXPECT_THROW(db.GetOrCreateRecording("recording1.bag", "sha256-zzz"), std::runtime_error);
+    EXPECT_THROW(database::GetOrCreateRecording(db.get(), "recording1.bag", "sha256-zzz"), std::runtime_error);
     // Cannot insert two different recordings with the same hash.
-    EXPECT_THROW(db.GetOrCreateRecording("recording2.bag", "sha256-yyy"), database::SqliteException);
+    EXPECT_THROW(database::GetOrCreateRecording(db.get(), "recording2.bag", "sha256-yyy"), database::SqliteException);
 }
 
 TEST(DatabaseCalibrationDatabase, TestGetOrCreateStep) {
-    database::OpenCalibrationDatabase db_{":memory:", true};
+    auto const db{database::OpenCalibrationDatabase(":memory:", true)};
     Hash const hash_a{"sha256-aaa"};
 
     // Insert a new step - cache miss.
-    auto result{db_.GetOrCreateStep(StepType::ImageLoading, hash_a)};
+    auto result{database::GetOrCreateStep(db.get(), StepType::ImageLoading, hash_a)};
     EXPECT_EQ(result.first, StepId{1});
     EXPECT_EQ(result.second, CacheStatus::CacheMiss);
 
-    // Until we "StepCacheKeyUpdate()" it will insert a new step and be a cache miss because there is no cache_key in
-    // the database yet.
-    result = db_.GetOrCreateStep(StepType::ImageLoading, hash_a);
+    // Until we "database::StepCacheKeyUpdate()" it will insert a new step and be a cache miss because there is no
+    // cache_key in the database yet.
+    result = database::GetOrCreateStep(db.get(), StepType::ImageLoading, hash_a);
     EXPECT_EQ(result.first, StepId{2});
     EXPECT_EQ(result.second, CacheStatus::CacheMiss);
 
     // Complete the second step entry with the cache key!
-    db_.StepCacheKeyUpdate(result.first, hash_a);
+    database::StepCacheKeyUpdate(db.get(), result.first, hash_a);
 
     // Now that the cache key is there we get a cache hit.
-    result = db_.GetOrCreateStep(StepType::ImageLoading, hash_a);
+    result = database::GetOrCreateStep(db.get(), StepType::ImageLoading, hash_a);
     EXPECT_EQ(result.first, StepId{2});
     EXPECT_EQ(result.second, CacheStatus::CacheHit);
 
     // Adding another ImageLoading step with a different cache key increments the step id and is a cache miss.
     Hash const hash_b{"sha256-bbb"};
-    result = db_.GetOrCreateStep(StepType::ImageLoading, hash_b);
+    result = database::GetOrCreateStep(db.get(), StepType::ImageLoading, hash_b);
     EXPECT_EQ(result.first, StepId{3});
     EXPECT_EQ(result.second, CacheStatus::CacheMiss);
 }
@@ -82,89 +82,90 @@ TEST(DatabaseCalibrationDatabase, TestGetOrCreateStep) {
 TEST(DatabaseCalibrationDatabase, TestCameraInfo) {
     auto db{database::OpenCalibrationDatabase(":memory:", true)};
 
-    auto const step{db.GetOrCreateStep(StepType::CameraInfo, "")};
-    AssetId const asset_id{db.GetOrCreateAsset(AssetType::Camera, 0, "")};
+    auto const step{database::GetOrCreateStep(db.get(), StepType::CameraInfo, "")};
+    AssetId const asset_id{database::GetOrCreateAsset(db.get(), AssetType::Camera, 0, "")};
 
     CameraInfo const camera_info{CameraModel::DoubleSphere, {0, 512, 0, 512}};
-    EXPECT_NO_THROW(db.CameraInfoInsert(step.first, asset_id, camera_info));
+    EXPECT_NO_THROW(database::CameraInfoInsert(db.get(), step.first, asset_id, camera_info));
 
-    auto result{db.CameraInfoSelect(step.first, asset_id)};
+    auto result{database::CameraInfoSelect(db.get(), step.first, asset_id)};
     ASSERT_TRUE(result.has_value());
     EXPECT_EQ(result->camera_model, camera_info.camera_model);
     EXPECT_EQ(result->bounds.u_max, camera_info.bounds.u_max);
     EXPECT_EQ(result->bounds.u_min, camera_info.bounds.u_min);
 
-    EXPECT_NO_THROW(result = db.CameraInfoSelect(StepId{111}, asset_id));
+    EXPECT_NO_THROW(result = database::CameraInfoSelect(db.get(), StepId{111}, asset_id));
     EXPECT_FALSE(result.has_value());
-    EXPECT_NO_THROW(result = db.CameraInfoSelect(step.first, AssetId{111}));
+    EXPECT_NO_THROW(result = database::CameraInfoSelect(db.get(), step.first, AssetId{111}));
     EXPECT_FALSE(result.has_value());
 }
 
 class CalibrationDatabaseFixture : public ::testing::Test {
    protected:
     void InsertImage(StepId const step_id, AssetId const asset_id, uint64_t const timestamp_ns = 0) {
-        db_.ImagesInsert(step_id, asset_id, {{timestamp_ns, ImageBuffer{}}});
+        database::ImagesInsert(db_.get(), step_id, asset_id, {{timestamp_ns, ImageBuffer{}}});
     }
 
     StepId CreateExtractedTargets(StepId const image_loading_id, AssetId const asset_id,
                                   uint64_t const timestamp_ns = 0) {
-        auto const step_id{db_.GetOrCreateStep(StepType::FeatureExtraction, "").first};
+        auto const step_id{database::GetOrCreateStep(db_.get(), StepType::FeatureExtraction, "").first};
 
-        db_.ExtractedTargetsInsert(step_id, image_loading_id, asset_id, {{timestamp_ns, ExtractedTarget{}}});
+        database::ExtractedTargetsInsert(db_.get(), step_id, image_loading_id, asset_id,
+                                         {{timestamp_ns, ExtractedTarget{}}});
 
         return step_id;
     }
 
-    database::OpenCalibrationDatabase db_{":memory:", true};
+    SqlitePtr db_{database::OpenCalibrationDatabase(":memory:", true)};
 };
 
 TEST_F(CalibrationDatabaseFixture, TestCameraPoses) {
     // Satisfy foreign key dependencies - a pose depends on a target which depends on an image.
-    AssetId const asset_id{db_.GetOrCreateAsset(AssetType::Camera, 0, "")};
-    StepId const image_loading_id{db_.GetOrCreateStep(StepType::ImageLoading, "").first};
+    AssetId const asset_id{database::GetOrCreateAsset(db_.get(), AssetType::Camera, 0, "")};
+    StepId const image_loading_id{database::GetOrCreateStep(db_.get(), StepType::ImageLoading, "").first};
     InsertImage(image_loading_id, asset_id);
     StepId const extracted_targets_id{CreateExtractedTargets(image_loading_id, asset_id)};
 
     Frames const camera_poses{Frame{0, Array6d::Ones(6)}};
-    StepId const step_id{db_.GetOrCreateStep(StepType::PoseInit, "").first};
-    EXPECT_NO_THROW(db_.CameraPosesInsert(step_id, extracted_targets_id, asset_id, camera_poses));
+    StepId const step_id{database::GetOrCreateStep(db_.get(), StepType::PoseInit, "").first};
+    EXPECT_NO_THROW(database::CameraPosesInsert(db_.get(), step_id, extracted_targets_id, asset_id, camera_poses));
 
-    auto const result{db_.CameraPosesSelect(step_id, asset_id)};
+    auto const result{database::CameraPosesSelect(db_.get(), step_id, asset_id)};
     EXPECT_EQ(std::size(result), 1);
     EXPECT_TRUE(result.at(0).pose.isApprox(camera_poses.at(0).pose));
 }
 
 TEST(DatabaseCalibrationDatbase, TestControlPoints) {
-    database::OpenCalibrationDatabase db{":memory:", true};
+    auto const db{database::OpenCalibrationDatabase(":memory:", true)};
 
-    StepId const step_id{db.GetOrCreateStep(StepType::SplineInit, "").first};
-    AssetId const asset_id{db.GetOrCreateAsset(AssetType::Camera, 0, "")};
+    StepId const step_id{database::GetOrCreateStep(db.get(), StepType::SplineInit, "").first};
+    AssetId const asset_id{database::GetOrCreateAsset(db.get(), AssetType::Camera, 0, "")};
 
     spline::Matrix2NXd const control_points{spline::Matrix2NXd::Random(6, 3)};
 
-    EXPECT_NO_THROW(db.ControlPointsInsert(step_id, asset_id, control_points));
+    EXPECT_NO_THROW(database::ControlPointsInsert(db.get(), step_id, asset_id, control_points));
 
-    auto const result{db.ControlPointsSelect(step_id, asset_id)};
+    auto const result{database::ControlPointsSelect(db.get(), step_id, asset_id)};
     EXPECT_TRUE(result.isApprox(control_points));
 }
 
 TEST_F(CalibrationDatabaseFixture, TestImages) {
-    AssetId const asset_id{db_.GetOrCreateAsset(AssetType::Camera, 0, "")};
-    StepId const step_id{db_.GetOrCreateStep(StepType::ImageLoading, "").first};
+    AssetId const asset_id{database::GetOrCreateAsset(db_.get(), AssetType::Camera, 0, "")};
+    StepId const step_id{database::GetOrCreateStep(db_.get(), StepType::ImageLoading, "").first};
 
     uint64_t const timestamp_ns{0};
     EXPECT_NO_THROW(InsertImage(step_id, asset_id, timestamp_ns));
 
-    auto const result{db_.ImagesSelect(step_id, asset_id)};
+    auto const result{database::ImagesSelect(db_.get(), step_id, asset_id)};
     EXPECT_EQ(std::size(result), 1);
     EXPECT_EQ(std::size(result.at(timestamp_ns).data), 0);
 }
 
 TEST(DatabaseCalibrationDatbase, TestImuData) {
-    database::OpenCalibrationDatabase db{":memory:", true};
+    auto const db{database::OpenCalibrationDatabase(":memory:", true)};
 
-    auto const step{db.GetOrCreateStep(StepType::ImuDataLoading, "")};
-    AssetId const asset_id{db.GetOrCreateAsset(AssetType::Imu, 0, "")};
+    auto const step{database::GetOrCreateStep(db.get(), StepType::ImuDataLoading, "")};
+    AssetId const asset_id{database::GetOrCreateAsset(db.get(), AssetType::Imu, 0, "")};
 
     ImuMeasurements const imu_data{{0, {{1, 2, 3}, {4, 5, 6}}}, {1, {{1, 2, 3}, {4, 5, 6}}}};
 
@@ -175,52 +176,52 @@ TEST(DatabaseCalibrationDatbase, TestImuData) {
     // incorrect, but it does feel like we are leaving something on the table with respect to unused
     // information/constraints. Maybe we can use some post insertion consistency checks/logics to make sure the database
     // is consistent with these constraints after each operation.
-    EXPECT_NO_THROW(db.ImuDataInsert(step.first, asset_id, imu_data));
+    EXPECT_NO_THROW(database::ImuDataInsert(db.get(), step.first, asset_id, imu_data));
 
-    auto result{db.ImuDataSelect(step.first, asset_id)};
+    auto result{database::ImuDataSelect(db.get(), step.first, asset_id)};
     EXPECT_EQ(std::size(result), std::size(imu_data));
     EXPECT_TRUE(result.at(0).angular_velocity.isApprox(imu_data.at(0).angular_velocity));
 
     // If nonexistent data is requested this is not an error, it will just return an empty container.
-    EXPECT_NO_THROW(result = db.ImuDataSelect(StepId{111}, asset_id));
+    EXPECT_NO_THROW(result = database::ImuDataSelect(db.get(), StepId{111}, asset_id));
     EXPECT_EQ(std::size(result), 0);
-    EXPECT_NO_THROW(result = db.ImuDataSelect(step.first, AssetId{111}));
+    EXPECT_NO_THROW(result = database::ImuDataSelect(db.get(), step.first, AssetId{111}));
     EXPECT_EQ(std::size(result), 0);
 }
 
 TEST(DatabaseCalibrationDatbase, TestIntrinsics) {
-    database::OpenCalibrationDatabase db{":memory:", true};
+    auto const db{database::OpenCalibrationDatabase(":memory:", true)};
 
     // TODO(Jack): We absolutely need to add logic to the database that checks the owning step for a camera intrinsic is
     // valid (ex. comes from a step which produces an intrinsic) and that the asset id is a camera. We need this kind of
     // checks for all steps, not just here.
-    auto const step{db.GetOrCreateStep(StepType::IntrinsicInit, "")};
-    AssetId const asset_id{db.GetOrCreateAsset(AssetType::Camera, 0, "")};
+    auto const step{database::GetOrCreateStep(db.get(), StepType::IntrinsicInit, "")};
+    AssetId const asset_id{database::GetOrCreateAsset(db.get(), AssetType::Camera, 0, "")};
 
     CameraState const intrinsic{Array3d{1, 2, 3}};
 
-    EXPECT_NO_THROW(db.IntrinsicInsert(step.first, asset_id, CameraModel::Pinhole, intrinsic));
+    EXPECT_NO_THROW(database::IntrinsicInsert(db.get(), step.first, asset_id, CameraModel::Pinhole, intrinsic));
 
-    auto result{db.IntrinsicSelect(step.first, asset_id)};
+    auto result{database::IntrinsicSelect(db.get(), step.first, asset_id)};
     ASSERT_TRUE(result.has_value());
     EXPECT_TRUE(result->intrinsics.isApprox(intrinsic.intrinsics));
 
-    EXPECT_NO_THROW(result = db.IntrinsicSelect(StepId{111}, asset_id));
+    EXPECT_NO_THROW(result = database::IntrinsicSelect(db.get(), StepId{111}, asset_id));
     EXPECT_FALSE(result.has_value());
-    EXPECT_NO_THROW(result = db.IntrinsicSelect(step.first, AssetId{111}));
+    EXPECT_NO_THROW(result = database::IntrinsicSelect(db.get(), step.first, AssetId{111}));
     EXPECT_FALSE(result.has_value());
 }
 
 TEST_F(CalibrationDatabaseFixture, TestExtractedTargets) {
     // Satisfy foreign keys - a target requires a corresponding image to be present.
-    AssetId const asset_id{db_.GetOrCreateAsset(AssetType::Camera, 0, "")};
-    StepId const image_loading_id{db_.GetOrCreateStep(StepType::ImageLoading, "").first};
+    AssetId const asset_id{database::GetOrCreateAsset(db_.get(), AssetType::Camera, 0, "")};
+    StepId const image_loading_id{database::GetOrCreateStep(db_.get(), StepType::ImageLoading, "").first};
     InsertImage(image_loading_id, asset_id);
 
     StepId step_id;
     EXPECT_NO_THROW(step_id = CreateExtractedTargets(image_loading_id, asset_id));
 
-    CameraMeasurements const result{db_.ExtractedTargetsSelect(step_id, asset_id)};
+    CameraMeasurements const result{database::ExtractedTargetsSelect(db_.get(), step_id, asset_id)};
     EXPECT_EQ(std::size(result), 1);
     EXPECT_EQ(result.at(0).indices.size(), 0);
 }
@@ -228,14 +229,14 @@ TEST_F(CalibrationDatabaseFixture, TestExtractedTargets) {
 TEST(DatabaseCalibrationDatbase, TestExtrinsics) {
     auto db{database::OpenCalibrationDatabase(":memory:", true)};
 
-    StepId const step_id{db.GetOrCreateStep(StepType::ExtrinsicInit, "").first};
-    AssetId const cam_id{db.GetOrCreateAsset(AssetType::Camera, 0, "")};
-    AssetId const imu_id{db.GetOrCreateAsset(AssetType::Imu, 0, "")};
+    StepId const step_id{database::GetOrCreateStep(db.get(), StepType::ExtrinsicInit, "").first};
+    AssetId const cam_id{database::GetOrCreateAsset(db.get(), AssetType::Camera, 0, "")};
+    AssetId const imu_id{database::GetOrCreateAsset(db.get(), AssetType::Imu, 0, "")};
 
     Extrinsic const extrinsic_co_imu{cam_id, imu_id, Array6d::Random()};
-    EXPECT_NO_THROW(db.ExtrinsicInsert(step_id, extrinsic_co_imu));
+    EXPECT_NO_THROW(database::ExtrinsicInsert(db.get(), step_id, extrinsic_co_imu));
 
-    auto const result{db.ExtrinsicSelect(step_id, cam_id, imu_id)};
+    auto const result{database::ExtrinsicSelect(db.get(), step_id, cam_id, imu_id)};
     ASSERT_TRUE(result.has_value());
     EXPECT_EQ(result->frame_a, cam_id);
     EXPECT_EQ(result->frame_b, imu_id);
@@ -245,12 +246,12 @@ TEST(DatabaseCalibrationDatbase, TestExtrinsics) {
 TEST(DatabaseCalibrationDatbase, TestGravity) {
     auto db{database::OpenCalibrationDatabase(":memory:", true)};
 
-    StepId const step_id{db.GetOrCreateStep(StepType::ExtrinsicInit, "").first};
+    StepId const step_id{database::GetOrCreateStep(db.get(), StepType::ExtrinsicInit, "").first};
 
     Vector3d const gravity{Vector3d::Random()};
-    EXPECT_NO_THROW(db.GravityInsert(step_id, gravity));
+    EXPECT_NO_THROW(database::GravityInsert(db.get(), step_id, gravity));
 
-    auto const result{db.GravitySelect(step_id)};
+    auto const result{database::GravitySelect(db.get(), step_id)};
     ASSERT_TRUE(result.has_value());
     EXPECT_TRUE(result->isApprox(gravity));
 }
@@ -259,13 +260,13 @@ TEST(DatabaseCalibrationDatbase, TestSplineInfo) {
     auto db{database::OpenCalibrationDatabase(":memory:", true)};
 
     // Satisfy foreign key constraints
-    StepId const step_id{db.GetOrCreateStep(StepType::SplineInit, "").first};
-    AssetId const asset_id{db.GetOrCreateAsset(AssetType::Camera, 0, "")};
+    StepId const step_id{database::GetOrCreateStep(db.get(), StepType::SplineInit, "").first};
+    AssetId const asset_id{database::GetOrCreateAsset(db.get(), AssetType::Camera, 0, "")};
 
     spline::TimeHandler const time_handler{0, 1};
-    EXPECT_NO_THROW(db.SplineInfoInsert(step_id, asset_id, time_handler));
+    EXPECT_NO_THROW(database::SplineInfoInsert(db.get(), step_id, asset_id, time_handler));
 
-    auto result{db.SplineInfoSelect(step_id, asset_id)};
+    auto result{database::SplineInfoSelect(db.get(), step_id, asset_id)};
     ASSERT_TRUE(result.has_value());
     EXPECT_EQ(result->t0_ns_, time_handler.t0_ns_);
     EXPECT_EQ(result->delta_t_ns_, time_handler.delta_t_ns_);
@@ -275,19 +276,19 @@ TEST(DatabaseCalibrationDatbase, TestTargetInfo) {
     auto db{database::OpenCalibrationDatabase(":memory:", true)};
 
     // Satisfy foreign key constraints
-    auto const step{db.GetOrCreateStep(StepType::TargetInfo, "")};
-    AssetId const asset_id{db.GetOrCreateAsset(AssetType::Target, 0, "")};
+    auto const step{database::GetOrCreateStep(db.get(), StepType::TargetInfo, "")};
+    AssetId const asset_id{database::GetOrCreateAsset(db.get(), AssetType::Target, 0, "")};
 
     TargetInfo const target_info{TargetType::Checkerboard, 6, 6, 0.1, false};
-    EXPECT_NO_THROW(db.TargetInfoInsert(step.first, asset_id, target_info));
+    EXPECT_NO_THROW(database::TargetInfoInsert(db.get(), step.first, asset_id, target_info));
 
-    auto result{db.TargetInfoSelect(step.first, asset_id)};
+    auto result{database::TargetInfoSelect(db.get(), step.first, asset_id)};
     ASSERT_TRUE(result.has_value());
     EXPECT_EQ(result->target_type, target_info.target_type);
     EXPECT_EQ(result->height, target_info.height);
 
-    EXPECT_NO_THROW(result = db.TargetInfoSelect(StepId{111}, asset_id));
+    EXPECT_NO_THROW(result = database::TargetInfoSelect(db.get(), StepId{111}, asset_id));
     EXPECT_FALSE(result.has_value());
-    EXPECT_NO_THROW(result = db.TargetInfoSelect(step.first, AssetId{111}));
+    EXPECT_NO_THROW(result = database::TargetInfoSelect(db.get(), step.first, AssetId{111}));
     EXPECT_FALSE(result.has_value());
 }
