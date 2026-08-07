41de1ef4e74efb621ccb61b3147ce18c28c11f0e
reprojection.calibration <reprojection.calibration@gmail.com>
2026-08-07 16:54:03 +0200

    single asset per workflow

--- code/python_tooling/database/data_formatting.py
+++ code/python_tooling/database/data_formatting.py
@@ -10,7 +10,7 @@ class Workflow:
     id: int
     type: str
     signature: str
-    assets: list[dict]
+    assets: dict[str, dict]
     steps: dict[str, int]
 
 
@@ -19,10 +19,19 @@ def parse_workflows(db):
     for _, workflow_row in db["workflows"].iterrows():
         workflow_id = int(workflow_row["id"])
 
+        # NOTE(Jack): By storing the assets indexed in a dict by their asset type (i.e. camera/target/imu) we are
+        # hardcoding the fact that there can only be one of each asset type in any workflow. This will not scale to
+        # multisensor setups but for now it gets the job done.
         asset_ids = db["workflow_assets"].loc[
-            db["workflow_assets"]["workflow_id"] == workflow_id, "asset_id"
+            db["workflow_assets"]["workflow_id"] == workflow_id,
+            "asset_id",
         ]
-        assets = db["assets"].loc[db["assets"]["id"].isin(asset_ids)].to_dict("records")
+        assets = (
+            db["assets"]
+            .loc[db["assets"]["id"].isin(asset_ids)]
+            .set_index("type")
+            .to_dict("index")
+        )
 
         steps = (
             db["workflow_steps"]
@@ -43,4 +52,9 @@ def parse_workflows(db):
 
     return workflows
 
-#def process_workflow(db, workflow):
+
+def process_workflow(db, workflow):
+    db["camera_info"] = db["camera_info"].set_index(["step_id", "asset_id"])
+    row = db["camera_info"].loc[(workflow.steps["camera_info"], workflow.assets["camera"]["id"])]
+
+    print(row)
--- code/python_tooling/tests/database/test_sql_table_loading.py
+++ code/python_tooling/tests/database/test_sql_table_loading.py
@@ -1,4 +1,3 @@
-import os
 import sqlite3
 import unittest
 from tempfile import NamedTemporaryFile
