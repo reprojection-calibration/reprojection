d7b55d344516a478d1146e5b89cfd3a1db740bf0
reprojection-calibration <reprojection.calibration@gmail.com>
2026-08-19 17:11:49 +0200

    shared object hell escape

--- building/install_scripts/apt_install_python_binding_deps.sh
+++ building/install_scripts/apt_install_python_binding_deps.sh
@@ -8,4 +8,10 @@ apt-get install --no-install-recommends --yes \
     python3-build \
     python3-pybind11
 
+# TODO(Jack): This is the single dependency which I think comes as a shared object hanger on the projection function
+# library. For now we just install this here and expect that any user needs to have this on their system. That there are
+# not more dependencies surprises me! We escaped shared object dependency hell... for now -_-
+apt-get install --no-install-recommends --yes \
+    libgoogle-glog-dev
+
 rm --force --recursive /var/lib/apt/lists/*
