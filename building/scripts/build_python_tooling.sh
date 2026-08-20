cff4a759b6356017feecd30e80a9e3ff033387a4
reprojection-calibration <reprojection.calibration@gmail.com>
2026-08-20 09:57:48 +0200

    add run tests flag

--- building/scripts/build_python_tooling.sh
+++ building/scripts/build_python_tooling.sh
@@ -2,6 +2,14 @@
 
 set -eoux pipefail
 
+# TODO(Jack): We are shooting ourselves in the foot hard with this python binding environment problem! Ideally we would
+# run all the tests every time we build the tooling. The problem we have is that the report generation tooling needs to
+# be build for all applications and does not compile the bindings. Therefore we cannot run the binding tests when we are
+# only installing the report generation. As I do not know how to selectively run tests simply, we just add a flag to
+# turn them off completely. As long as the dashboard is always being built in CI we can sleep ok, but we should still
+# probably keep one eye open!
+RUN_TESTS=${RUN_TESTS:-1}
+
 # NOTE(Jack): We should set the default values of the environmental variables here and in all places so that a user could
 # run all scripts from the project root directory and everything works like it does in the dockerized environment.
 PACKAGE_DIRECTORY=${PACKAGE_DIRECTORY:-code/python_tooling}
@@ -16,6 +24,8 @@ source "${VENV_DIR}/bin/activate"
 # have access to all the packages in the venv. But this is speculation and needs more research.
 python3 -m grpc_tools.protoc -I "${PROTO_DIRECTORY}" --python_out="${PACKAGE_DIRECTORY}/generated" "${PROTO_DIRECTORY}"/*.proto
 
-# NOTE(Jack): We do an editable install so we can develop locally.
-python3 -m pip install --editable "${PACKAGE_DIRECTORY}"
-python3 -m unittest discover --start-directory "${PACKAGE_DIRECTORY}" --verbose
+python3 -m pip install "${PACKAGE_DIRECTORY}"
+
+if [[ "${RUN_TESTS}" == "1" ]]; then
+    python3 -m unittest discover --start-directory "${PACKAGE_DIRECTORY}" --verbose
+fi
