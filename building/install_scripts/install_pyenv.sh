5c074df9e6db4347067c25fbeaf263897f38b665
reprojection.calibration <reprojection.calibration@gmail.com>
2026-08-21 14:32:13 +0200

    set shell option for entire image and refactor install pyenv script

--- building/install_scripts/install_pyenv.sh
+++ building/install_scripts/install_pyenv.sh
@@ -2,29 +2,9 @@
 
 set -eoux pipefail
 
-# NOTE(Jack): This needs to be a separate script because we need to write the .bash_profile and then source that.
+curl -fsSL https://pyenv.run | bash
 
-# WARN(Jack): This logic here essentially is hardcoding the assumption that the user has installed pyenv on their system
-# using this script. If a user has already installed pyenv and added it to something else than the .bash_profile then
-# they will need to manually source that. But for our workflow it is internally consistent and works.
-
-# shellcheck disable=SC1090
-[[ -f ~/.bash_profile ]] && source ~/.bash_profile
-if command -v pyenv >/dev/null 2>&1; then
-    echo "Early exit - pyenv is already installed."
-    exit 0
-fi
-
-PYENV_ROOT=${PYENV_ROOT:-"${HOME}/.pyenv"}
-
-wget -qO- https://pyenv.run | bash
-
-# TODO(Jack): We create the .bash_profile because when we tried to use the .bashrc the other variables/script in the
-# .bashrc caused problems. This might cause problems for users that already have pyenv locally installed and want to run
-# this script.
-touch ~/.bash_profile
-cat <<'EOF' | tee -a ~/.bash_profile > /dev/null
-
-[[ -d ${PYENV_ROOT}/bin ]] && export PATH="${PYENV_ROOT}/bin:${PATH}"
-eval "$(pyenv init - bash)"
-EOF
+# TODO(Jack): For some reason if we do not do this then sqlite (which we kinda source install) can be found/linked
+# against. I thought I had solved this with ldconfig but I guess not - this gets the job done on 20.04 and 24.04
+LDFLAGS="-L/usr/local/lib -Wl,-rpath,/usr/local/lib" \
+  pyenv install 3.12
