8735e7a603b2da22e058ae0343c9147016392965
reprojection-calibration <reprojection.calibration@gmail.com>
2026-08-21 17:11:26 +0200

    comments

--- building/local/install_python_venv.sh
+++ building/local/install_python_venv.sh
@@ -2,16 +2,14 @@
 
 set -eou pipefail
 
-# NOTE(Jack): THis will install the "reprojection_env" python virtual environment on your host system! Things you should
-# be aware of:
+# NOTE(Jack): THis will install a python virtual environment on your host system! Things you should be aware of:
 #   1) Only tested on ubuntu 24.04
-#   2) Creates a .bash_profile file and writes to it (see install_pyenv.sh for details)
-#   3) Apt installs system packages (requires sudo)
+#   2) Apt installs system packages (requires sudo)
+#   3) Install pyenv and builds and install a python venv
 #   4) Creates a directory "wheels/" and copies a python wheel into it
 
 PYENV_ROOT="${PYENV_ROOT:-${HOME}/.pyenv}"
 PATH="${PYENV_ROOT}/bin:${PATH}"
-
 project_root="$(dirname "$(realpath -s "$0")")/../.."
 
 # Install apt dependencies and pyenv to host.
@@ -38,7 +36,6 @@ REPROJECTION_SQL_PYTHON_DIR="${project_root}/code/resources/sql" \
 RUN_TESTS=0 \
 REPROJECTION_VENV="${reprojection_venv}" \
   "${project_root}/building/scripts/build_python_tooling.sh"
-
 
 # Build the python bindings in a docker image and install them into our venv.
 "${project_root}/building/local/build_image.sh" \
