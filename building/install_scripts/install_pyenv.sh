1edce0892c0171c3f1040249635d20298e752b77
reprojection-calibration <reprojection.calibration@gmail.com>
2026-08-21 17:06:26 +0200

    local building!

--- building/install_scripts/install_pyenv.sh
+++ building/install_scripts/install_pyenv.sh
@@ -5,15 +5,13 @@ set -eoux pipefail
 # NOTE(Jack): Because a developer might also run this script locally where they may or may not have already have
 # either of the following things installed we put these in if else blocks.
 
-if command -v pyenv >/dev/null 2>&1; then
-  echo "pyenv is already installed."
-else
-  curl -fsSL https://pyenv.run | bash
+if ! command -v pyenv >/dev/null 2>&1; then
+    wget -qO- https://pyenv.run | bash
 fi
 
-if pyenv versions --bare | grep -q '^3\.12'; then
-  echo "Python 3.12 is already installed."
-else
-  LDFLAGS="-L/usr/local/lib -Wl,-rpath,/usr/local/lib" \
-    pyenv install 3.12
+if ! pyenv versions --bare | grep -q '^3\.12'; then
+    # NOTE(Jack): This LDFLAGS env var handles some sqlite linking error we get on ubuntu 20.04. Must have something
+    # to do with us source installing sqlite.
+    LDFLAGS="-L/usr/local/lib -Wl,-rpath,/usr/local/lib" \
+        pyenv install 3.12
 fi
--- building/local/install_python_venv.sh
+++ building/local/install_python_venv.sh
@@ -9,35 +9,28 @@ set -eou pipefail
 #   3) Apt installs system packages (requires sudo)
 #   4) Creates a directory "wheels/" and copies a python wheel into it
 
+PYENV_ROOT="${PYENV_ROOT:-${HOME}/.pyenv}"
+PATH="${PYENV_ROOT}/bin:${PATH}"
+
 project_root="$(dirname "$(realpath -s "$0")")/../.."
 
+# Install apt dependencies and pyenv to host.
+sudo "${project_root}/building/install_scripts/apt_install_pyenv_deps.sh"
 "${project_root}/building/install_scripts/install_pyenv.sh"
 
-# NOTE(Jack): Apt installing system packages requires sudo. The reason we do not run the entire local install script as
-# sudo is because we don't need too, and then number two it changes the HOME path to /root which the daily user does
-# not own.
-sudo "${project_root}/building/install_scripts/apt_install_pyenv_deps.sh"
-
-# NOTE(Jack): In the docker workflow we source build sqlite3 because we need to ensure that we have a version that
-# supports the "RETURNING" syntax - here we assume that a user is running a modern enough version of ubuntu that the
-# apt version is good enough - this will not scale, but I do not really want to source install a different sqlite on the
-# users system.
-# NOTE(Jack): In the docker workflow libgoogle-glog-dev is installed in the base-stage and is as of now the only dep of
-# the bindings which needs to be explicitly installed here (its handled in the base-stage in the dockerized workflow).
 sudo apt-get update
 sudo apt-get install --no-install-recommends --yes \
   libgoogle-glog-dev \
   sqlite3
 
+# Build and install the venv.
 reprojection_venv="${HOME}/.venv-reprojection"
-
-# TODO(Jack): Copy and pasted from a docker image stage
-python -m venv "${reprojection_venv}"
+PYENV_VERSION=3.12 \
+  pyenv exec python -m venv "${reprojection_venv}"
 "${reprojection_venv}/bin/python" \
   -m pip install -r "${project_root}/building/scripts/requirements-reprojection-env.txt"
 
-# TODO(Jack): I would like to be able to run the tests here but the bindings are not installed yet! This is the core of
-# our python installation problems.
+# Install our custom python tooling code into the venv.
 EDITABLE_INSTALL=1 \
 PACKAGE_DIRECTORY="${project_root}/code/python_tooling" \
 PROTO_DIRECTORY="${project_root}/code/resources/proto" \
@@ -47,12 +40,7 @@ REPROJECTION_VENV="${reprojection_venv}" \
   "${project_root}/building/scripts/build_python_tooling.sh"
 
 
-# NOTE(Jack): Here is the meat and potatoes. The basic complicating factor of the entire python bindings workflow, and
-# why we really need this script in the first place is that we need the python bindings from the library (only the
-# projection_functions as of 18.08.2026). As we do not want to even try to compile the library on the user host, we need
-# to build them in a docker, then copy them to the host and install them inside the python environment.
-# TODO(Jack): Is there some way that we can build the entire environment in docker and then copy that to the host
-# system? If we could do that somehow then we could avoid this entire script.
+# Build the python bindings in a docker image and install them into our venv.
 "${project_root}/building/local/build_image.sh" \
   --stage python-binding
 
