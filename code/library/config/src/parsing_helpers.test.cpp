41b79258c4daac8ce4120ffc99756bc78971bf88
reprojection.calibration <reprojection.calibration@gmail.com>
2026-08-26 10:35:56 +0200

    refactor TestRejectUnexpectedKeys to handle indexable keys

--- code/library/config/src/parsing_helpers.test.cpp
+++ code/library/config/src/parsing_helpers.test.cpp
@@ -43,17 +43,21 @@ TEST(ConfigParsingHelpers, TestRejectUnexpectedKeys) {
     static constexpr std::string_view table_content{R"(
         key1 = "value1"
 
+        [table]
+
         [table1]
-        key2 = "value2"
+
     )"};
     toml::table const table{toml::parse(table_content)};
 
     // Throws because key1 is not in the allowed_keys.
-    EXPECT_THROW(config::RejectUnexpectedKeys(table, {"table1"}, ""), std::runtime_error);
+    EXPECT_THROW(config::RejectUnexpectedKeys(table, {}, {"table"}, ""), std::runtime_error);
 
-    // Does not throw because both keys are in allowed_keys. Note that this method does not recurse into the child
-    // tables, therefore we do not need to specify key2 as part of the allowed_keys.
-    EXPECT_NO_THROW(config::RejectUnexpectedKeys(table, {"key1", "table1"}, ""));
+    // Does throw because table (and table1) is not specified explicitly as an indexable key.
+    EXPECT_THROW(config::RejectUnexpectedKeys(table, {"key1", "table"}, {}, ""), std::runtime_error);
+
+    // Does not throw because 'key1' and the indexable 'table' key are properly specified.
+    EXPECT_NO_THROW(config::RejectUnexpectedKeys(table, {"key1"}, {"table"}, ""));
 }
 
 TEST(ConfigParsingHelpers, TestOptional) {
