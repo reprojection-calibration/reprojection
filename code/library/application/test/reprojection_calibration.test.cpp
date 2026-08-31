5f04a5191a2287f313e3411b35d1bebcac2c1a4e
reprojection-calibration <reprojection.calibration@gmail.com>
2026-08-31 10:48:58 +0200

    refactor to remove test setup copy paste

--- code/library/application/test/reprojection_calibration.test.cpp
+++ code/library/application/test/reprojection_calibration.test.cpp
@@ -56,59 +56,60 @@ TEST(ApplicationReprojectionCalibration, TestParseSensors) {
     EXPECT_EQ(*sensors.imu_name, "/imu0");
 }
 
+// WARN(Jack): I would really really like to also be able to exercise the imu calibration component here but it
+// is not nearly as easy to generate cache hits for those steps with empty inputs/outputs. This requires some
+// more investigation and until then we just need pass std::nullopt for the imu input. NOTE(Jack): We do not
+// need to do anything for the pose_initialization and bundle_adjustment steps to manufacture a cache hit
+// because if their inputs are empty they themselves will just pass through with no problem. This might change
+// in the future but for now it stands.
+// TODO(Jack): This test is a little sketchy because we are trying to induce cache hits to avoid actually having to
+// calculate anything. As a principle we do not want to use the checked in test database which means this is as much
+// as we can do here. I guess we could also use the MVG test data generator, but that will be for a future
+// contributor :)
 TEST(ApplicationReprojectionCalibration, TestCalibrate) {
     toml::table const config{toml::parse(testing_utilities::calibration_config)};
     auto db{database::OpenCalibrationDatabase(":memory:", true)};
 
-    // TODO(Jack): This test is a little sketchy because we are trying to induce cache hits to avoid actually having to
-    // calculate anything. As a principle we do not want to use the checked in test database which means this is as much
-    // as we can do here. I guess we could also use the MVG test data generator, but that will be for a future
-    // contributor :)
-
     steps::CalibrationContext const context{steps::InitializeCalibration(config, db)};
 
-    std::array<testing_utilities::CameraTestData, 2> const camera_test_data{
-        {{
-             database::GetOrCreateStep(db.get(), StepType::ImageLoading, "img0").first,
-             "ftex0",
-             database::GetOrCreateStep(db.get(), StepType::FeatureExtraction, "ftex0").first,
-             hashing::HashArguments(context.assets.cameras.at(0).id.value,
-                                    context.assets.cameras.at(0).config.camera_model, EncodedImages{}),
-         },
-         {
-             database::GetOrCreateStep(db.get(), StepType::ImageLoading, "img1").first,
-             "ftex1",
-             database::GetOrCreateStep(db.get(), StepType::FeatureExtraction, "ftex1").first,
-             hashing::HashArguments(context.assets.cameras.at(1).id.value,
-                                    context.assets.cameras.at(1).config.camera_model, EncodedImages{}),
-         }}};
+    // TODO(Jack): Use a vector so we do not need to hardcode this to an array matching the number of cameras in the
+    // config?
+    std::array<testing_utilities::CameraTestData, 2> camera_test_data;
+    for (size_t i{0}; i < std::size(context.assets.cameras); ++i) {
+        auto const& camera{context.assets.cameras.at(i)};
+
+        // NOTE(Jack): We need to simulate some cache keys here because the database has UNIQUE constraints that prevent
+        // us from just entering a blank string or something like that.
+        Hash const image_cache_key{std::format("img{}", i)};
+        Hash const feature_cache_key{std::format("ftex{}", i)};
+
+        // WARN(Jack): If the camera info cache key calculation method changes then we will need to update this here
+        // too!
+        camera_test_data[i] = {
+            database::GetOrCreateStep(db.get(), StepType::ImageLoading, image_cache_key).first,
+            feature_cache_key,
+            database::GetOrCreateStep(db.get(), StepType::FeatureExtraction, feature_cache_key).first,
+            hashing::HashArguments(camera.id.value, camera.config.camera_model, EncodedImages{}),
+        };
+    }
 
     testing_utilities::TestDatabaseSetup(context.assets.cameras, camera_test_data, db);
 
-    auto step = database::GetOrCreateStep(db.get(), StepType::IntrinsicInit, "");
-    database::IntrinsicInsert(db.get(), step.first, context.assets.cameras.at(0).id,
-                              context.assets.cameras.at(0).config.camera_model, {Array5d{256, 256, 256, 0, 0.5}});
-    CameraInfo const cam_info_0{context.assets.cameras.at(0).config.camera_model, {0, 512, 0, 512}};
-    database::StepCacheKeyUpdate(
-        db.get(), step.first,
-        hashing::HashArguments(context.assets.cameras.at(0).id.value, cam_info_0, CameraMeasurements{}));
+    // Add the intrinsic init manually - this is the only step we are forced to manually add a value for to let the rest
+    // of the workflow run successfully on the empty data structures.
+    for (auto const& camera : context.assets.cameras) {
+        auto const [step_id, cache_status]{database::GetOrCreateStep(db.get(), StepType::IntrinsicInit, "")};
+        database::IntrinsicInsert(db.get(), step_id, camera.id, camera.config.camera_model,
+                                  {Array5d{256, 256, 256, 0, 0.5}});
 
-    step = database::GetOrCreateStep(db.get(), StepType::IntrinsicInit, "");
-    database::IntrinsicInsert(db.get(), step.first, context.assets.cameras.at(1).id,
-                              context.assets.cameras.at(1).config.camera_model, {Array5d{256, 256, 256, 0, 0.5}});
-    CameraInfo const cam_info_1{context.assets.cameras.at(1).config.camera_model, {0, 512, 0, 512}};
-    database::StepCacheKeyUpdate(
-        db.get(), step.first,
-        hashing::HashArguments(context.assets.cameras.at(1).id.value, cam_info_1, CameraMeasurements{}));
+        // WARN(Jack): This has to match the camera info used inside the TestDatabaseSetup() function. This will mess us
+        // up one day! Can we make the connection explicit?
+        CameraInfo const camera_info{camera.config.camera_model, {0, 512, 0, 512}};
+        database::StepCacheKeyUpdate(db.get(), step_id,
+                                     hashing::HashArguments(camera.id.value, camera_info, CameraMeasurements{}));
+    }
 
-    // WARN(Jack): I would really really like to also be able to exercise the imu calibration component here but it
-    // is not nearly as easy to generate cache hits for those steps with empty inputs/outputs. This requires some
-    // more investigation and until then we just need pass std::nullopt for the imu input. NOTE(Jack): We do not
-    // need to do anything for the pose_initialization and bundle_adjustment steps to manufacture a cache hit
-    // because if their inputs are empty they themselves will just pass through with no problem. This might change
-    // in the future but for now it stands.
-
-    // TODO(Jack): Also enable to trigger imu calibration! See warning above.
     application::ImageInputs const image_inputs{testing_utilities::TestDatabaseImageInputs(context.assets.cameras)};
+    // TODO(Jack): Also enable to trigger imu calibration! See warning above.
     EXPECT_NO_THROW(application::Calibrate(config, image_inputs, std::nullopt, db));
 }
