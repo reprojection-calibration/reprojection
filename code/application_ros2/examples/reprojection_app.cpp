a348b9785320dc4d78cf70a541750abac82b5c4e
reprojection-calibration <reprojection.calibration@gmail.com>
2026-09-01 08:24:35 +0200

    ros2 integration test passing

--- code/application_ros2/examples/reprojection_app.cpp
+++ code/application_ros2/examples/reprojection_app.cpp
@@ -21,30 +21,36 @@ int main(int argc, char* argv[]) {
     // like not being able to open a bag file. If I knew how to turn off the logging completely I would!
     rcutils_logging_set_default_logger_level(RCUTILS_LOG_SEVERITY_FATAL);
 
-    // TODO(Jack): As noted in the testing, the const semantics of the ROS2 image source is not really nice, because the
-    //  ImageSource needs to take and store a mutable reference. This is not really a huge problem but alone the fact
-    //  that we have a non owning mutable reference inside of the ImageSource is a sign we are not doing something
-    //  right.
-    auto image_reader_result{ros2::SingleTopicBagReader::Create(app_args->data_path, sensors.camera_names[0])};
-    if (std::holds_alternative<ros2::BagError>(image_reader_result)) {
-        std::cerr << std::get<ros2::BagError>(image_reader_result).message << "\n";
-        return EXIT_FAILURE;
+    // NOTE(Jack): ImageSource stores a mutable reference to its SingleTopicBagReader, so the readers need to remain
+    // alive and at stable addresses until Calibrate() returns. Reserving the complete camera count prevents the vector
+    // from reallocating while we construct the image inputs below.
+    std::vector<ros2::SingleTopicBagReader> image_bag_readers;
+    image_bag_readers.reserve(std::size(sensors.camera_names));
+
+    application::ImageInputs image_inputs;
+    for (auto const& camera_name : sensors.camera_names) {
+        auto image_reader_result{ros2::SingleTopicBagReader::Create(app_args->data_path, camera_name)};
+        if (std::holds_alternative<ros2::BagError>(image_reader_result)) {
+            std::cerr << std::get<ros2::BagError>(image_reader_result).message << "\n";
+            return EXIT_FAILURE;
+        }
+
+        image_bag_readers.push_back(std::move(std::get<ros2::SingleTopicBagReader>(image_reader_result)));
+        auto& image_bag_reader{image_bag_readers.back()};
+
+        auto const image_signature{ros2::SerializeBagTopic(image_bag_reader)};
+        if (not image_signature) {
+            std::cerr << "Unable to calculate image data signature for " << camera_name << "!\n";
+            return EXIT_FAILURE;
+        }
+
+        image_inputs.emplace(camera_name,
+                             application::ImageInput{ros2::ImageSource{image_bag_reader}, *image_signature});
     }
-    auto& image_bag_reader{std::get<ros2::SingleTopicBagReader>(image_reader_result)};
-
-    ros2::ImageSource image_source{image_bag_reader};
-
-    auto const image_signature{ros2::SerializeBagTopic(image_bag_reader)};
-    if (not image_signature) {
-        std::cerr << "Unable to calculate image data signature!\n";
-        return EXIT_FAILURE;
-    }
-
-    application::ImageInputs const inputs{{sensors.camera_names[0], {image_source, *image_signature}}};
 
     // Early execution and return for the camera only intrinsic only case.
     if (not sensors.imu_name.has_value()) {
-        application::Calibrate(app_args->config, inputs, std::nullopt, app_args->db);
+        application::Calibrate(app_args->config, image_inputs, std::nullopt, app_args->db);
 
         return EXIT_SUCCESS;
     }
@@ -64,8 +70,7 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
 
-    application::Calibrate(app_args->config, inputs,
-                           application::ImuInput{imu_source, *imu_signature}, app_args->db);
+    application::Calibrate(app_args->config, image_inputs, application::ImuInput{imu_source, *imu_signature}, app_args->db);
 
     return EXIT_SUCCESS;
 }
