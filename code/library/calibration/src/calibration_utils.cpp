83e99b98c7e09a4cd72870060ec6944793d70f1d
reprojection-calibration <reprojection.calibration@gmail.com>
2026-09-02 12:38:45 +0200

    TestSynchronizeFrames

--- code/library/calibration/src/calibration_utils.cpp
+++ code/library/calibration/src/calibration_utils.cpp
@@ -28,4 +28,43 @@ Frames AlignRotations(Frames data) {
     return data;
 }
 
+// TODO TEST? and split between hpp and cpp?
+auto FindClosest(Frames const& data, uint64_t const timestamp) {
+    auto const upper{data.lower_bound(timestamp)};
+    if (upper == std::cbegin(data)) {
+        return upper;
+    } else if (upper == std::cend(data)) {
+        return std::prev(upper);
+    }
+
+    auto const lower{std::prev(upper)};
+
+    uint64_t const upper_delta{upper->first - timestamp};
+    uint64_t const lower_delta{timestamp - lower->first};
+
+    return lower_delta <= upper_delta ? lower : upper;
+}
+
+// NOTE(Jack): We pass frames_a by value because we edit it and remove unsyncable entries and we pass frames_b by const
+// reference because we only copy out the values we actually need from it.
+std::pair<Frames, Frames> SynchronizeFrames(Frames frames_a, Frames const& frames_b, uint64_t const sync_delta_ns) {
+    Frames frames_b_synced;
+    for (auto const& [timestamp_ns_a, frame_a] : frames_a) {
+        auto const closest{FindClosest(frames_b, timestamp_ns_a)};
+
+        // NOTE(Jack): We used the unsigned long type so we need to be careful to protect against underflow here! Not
+        // 100% sure this is right but I think it works for us.
+        uint64_t const delta_ns{closest->first > timestamp_ns_a ? closest->first - timestamp_ns_a
+                                                                : timestamp_ns_a - closest->first};
+        if (delta_ns > sync_delta_ns) {
+            frames_a.erase(timestamp_ns_a);
+            continue;
+        }
+
+        frames_b_synced.insert({timestamp_ns_a, closest->second});
+    }
+
+    return {frames_a, frames_b_synced};
+}
+
 }  // namespace reprojection::calibration
--- code/library/calibration/test/calibration_utils.test.cpp
+++ code/library/calibration/test/calibration_utils.test.cpp
@@ -2,9 +2,11 @@
 
 #include <gtest/gtest.h>
 
+#include <ranges>
+
 using namespace reprojection;
 
-TEST(StepsBundleAdjustment, TestAlignRotations) {
+TEST(CalibrationCalibrationUtils, TestAlignRotations) {
     Vector6d const pose_1{1, 0, 0, 0, 0, 0};
     Vector6d const pose_2{-1 * pose_1};
     Frames const input{Frame{1, pose_1}, Frame{2, pose_2}};
@@ -12,4 +14,33 @@ TEST(StepsBundleAdjustment, TestAlignRotations) {
     Frames const output{calibration::AlignRotations(input)};
 
     EXPECT_TRUE(output.at(1).pose.isApprox(output.at(2).pose));
+}
+
+TEST(CalibrationCalibrationUtils, TestSynchronizeFrames) {
+    auto const pose{Array6d::Random(6)};
+
+    // All three match.
+    Frames const data_x{{0, {pose}}, {10, {pose}}, {20, {pose}}};
+    auto [frames_a, frames_b]{calibration::SynchronizeFrames(data_x, data_x, 2)};
+
+    std::vector<uint64_t> expected{0, 10, 20};
+    EXPECT_TRUE(std::ranges::equal(frames_a | std::views::keys, expected));
+    EXPECT_TRUE(std::ranges::equal(frames_b | std::views::keys, expected));
+
+    // The middle value of data_y does not match data_x and the others are with the synx tolerance so we only have two
+    // matches.
+    Frames const data_y{{1, {pose}}, {13, {pose}}, {19, {pose}}};
+    std::tie(frames_a, frames_b) = calibration::SynchronizeFrames(data_x, data_y, 2);
+
+    expected = {0, 20};
+    EXPECT_TRUE(std::ranges::equal(frames_a | std::views::keys, expected));
+    EXPECT_TRUE(std::ranges::equal(frames_b | std::views::keys, expected));
+
+    // All data is outside of the sync tolerance so we get no matches at all.
+    Frames const data_z{{3, {pose}}, {13, {pose}}, {23, {pose}}};
+
+    std::tie(frames_a, frames_b) = calibration::SynchronizeFrames(data_x, data_z, 2);
+    expected = {};
+    EXPECT_TRUE(std::ranges::equal(frames_a | std::views::keys, expected));
+    EXPECT_TRUE(std::ranges::equal(frames_b | std::views::keys, expected));
 }
