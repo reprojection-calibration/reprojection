3325a7689d46547502cf9b9a05650a88c48dac0f
reprojection-calibration <reprojection.calibration@gmail.com>
2026-09-07 17:28:08 +0200

    comments

--- code/library/steps/src/pose_initialization.cpp
+++ code/library/steps/src/pose_initialization.cpp
@@ -47,9 +47,6 @@ void PoseInitialization::Execute(StepId step_id, SqlitePtr const db) const {
     log->info("{{'step_id': {}, 'asset_id': {}, 'num_targets': '{}', 'num_poses: {}}}}}", step_id.value,
               camera_id_.value, std::size(targets_), std::size(camera_poses));
 
-    // Diagnostic output
-    // TODO(Jack): No optimization is happening here because we are just building the problem to use the reprojection
-    // error evaluation function, but we still need to specify 'optimize_intrinsic' regardless.
     Ba::Problem const ba_problem{
         Ba::SingleCamProblem(camera_info_, intrinsic_, targets_, camera_poses, {}, camera_id_)};
 
