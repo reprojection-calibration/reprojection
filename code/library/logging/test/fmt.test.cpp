e7353c85920ae2e2c10a1b28d22540461039bd7e
reprojection-calibration <reprojection.calibration@gmail.com>
2026-09-08 09:08:21 +0200

    TestCeresSolverSummary

--- code/library/logging/test/fmt.test.cpp
+++ code/library/logging/test/fmt.test.cpp
@@ -6,7 +6,7 @@
 
 using namespace reprojection;
 
-TEST(LoggingLogging, TestFmtExtrinsic) {
+TEST(LoggingFmt, TestFmtExtrinsic) {
     Extrinsic const data{AssetId{1}, AssetId{2}, Array6d::Ones()};
 
     std::string const result{fmt::format("{}", data)};
@@ -14,7 +14,7 @@ TEST(LoggingLogging, TestFmtExtrinsic) {
     EXPECT_EQ(result, "{'frame_a': 1, 'frame_b': 2, 'se3_a_b': [1, 1, 1, 1, 1, 1]}");
 }
 
-TEST(LoggingLogging, TestRigState) {
+TEST(LoggingFmt, TestRigState) {
     Frames const rig_poses{Frame{1, Array6d::Ones()}, Frame{2, Array6d::Ones()}};
     Extrinsic const extrinsic_1{AssetId{1}, AssetId{2}, Array6d::Ones()};
     Extrinsic const extrinsic_2{AssetId{2}, AssetId{3}, Array6d::Ones()};
@@ -25,4 +25,14 @@ TEST(LoggingLogging, TestRigState) {
     EXPECT_EQ(result,
               "{'rig_frame_asset_id': 1, 'num_poses': 2, 'extrinsics': [{'frame_a': 1, 'frame_b': 2, 'se3_a_b': [1, 1, "
               "1, 1, 1, 1]}, {'frame_a': 2, 'frame_b': 3, 'se3_a_b': [1, 1, 1, 1, 1, 1]}]}");
+}
+
+TEST(LoggingFmt, TestCeresSolverSummary) {
+    ceres::Solver::Summary const data;
+
+    std::string const result{fmt::format("{}", data)};
+
+    EXPECT_EQ(result,
+          "{'initial_cost': -1.00, 'final_cost': -1.00, 'num_successful_steps': -1, 'num_unsuccessful_steps': -1}");
+
 }
