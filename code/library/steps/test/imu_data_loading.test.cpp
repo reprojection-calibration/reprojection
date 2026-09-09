3a84fb6513de76ac0b22b19fc16f69663a6ab962
reprojection-calibration <reprojection.calibration@gmail.com>
2026-09-09 13:42:20 +0200

    ImuSamplerFixture passes with refactored test fixture!

--- code/library/steps/test/imu_data_loading.test.cpp
+++ code/library/steps/test/imu_data_loading.test.cpp
@@ -6,13 +6,17 @@
 
 #include "steps/step_runner.hpp"
 
+#include "test_fixture.hpp"
+
 using namespace reprojection;
 
-class ImuSamplerFixture : public ::testing::Test {
+class ImuSamplerFixture : public StepTestFixture {
    protected:
     void SetUp() override {
-        imu_data_ = ImuSamples{{0, {{1, 2, 3}, {4, 5, 6}}}, {1, {{1, 2, 3}, {4, 5, 6}}}};
+        StepTestFixture::SetUp();
 
+        imu_id_ = context_.assets.imu->id;  // Unprotected optional access!
+        imu_data_ = ImuSamples{{0, {{1, 2, 3}, {4, 5, 6}}}, {1, {{1, 2, 3}, {4, 5, 6}}}};
         imu_sampler_ =
             [itr = std::cbegin(imu_data_),
              end = std::cend(imu_data_)]() mutable -> std::optional<std::pair<uint64_t, std::array<double, 6>>> {
@@ -32,49 +36,48 @@ class ImuSamplerFixture : public ::testing::Test {
         };
     }
 
+    AssetId imu_id_;
     ImuSamples imu_data_;
     ImuSampler imu_sampler_;
 };
 
 TEST_F(ImuSamplerFixture, TestImuDataLoadingStepRunner) {
-    auto db{database::OpenCalibrationDatabase(":memory:", true)};
-    AssetId const imu_id{database::GetOrCreateAsset(db.get(), AssetType::Imu, 0, "")};
-    database::AssetGroupInsert(db.get(), {imu_id});
-    WorkflowId const workflow_id{database::GetOrCreateWorkflow(db.get(), WorkflowType::CamImu, {imu_id})};
+    steps::ImuDataLoading const step{imu_id_, "", imu_sampler_};
+    StepId const step_id{RunStep<steps::ImuDataLoading>(context_.workflow_id, step, db_)};
 
-    steps::ImuDataLoading const step{imu_id, "", imu_sampler_};
-    StepId const step_id{RunStep<steps::ImuDataLoading>(workflow_id, step, db)};
-
-    auto const result{database::ImuDataSelect(db.get(), step_id, imu_id)};
+    auto const result{database::ImuDataSelect(db_.get(), step_id, imu_id_)};
     EXPECT_EQ(std::size(result), std::size(imu_data_));
+
     for (auto const timestamp_ns : imu_data_ | std::views::keys) {
-        EXPECT_TRUE(result.at(timestamp_ns).angular_velocity.isApprox(imu_data_.at(timestamp_ns).angular_velocity));
-        EXPECT_TRUE(
-            result.at(timestamp_ns).linear_acceleration.isApprox(imu_data_.at(timestamp_ns).linear_acceleration));
+        auto const& result_i{result.at(timestamp_ns)};
+        auto const& input_i{imu_data_.at(timestamp_ns)};
+
+        EXPECT_TRUE(result_i.angular_velocity.isApprox(input_i.angular_velocity));
+        EXPECT_TRUE(result_i.linear_acceleration.isApprox(input_i.linear_acceleration));
     }
 }
 
 TEST_F(ImuSamplerFixture, TestImuDataLoadingStep) {
-    auto db{database::OpenCalibrationDatabase(":memory:", true)};
+    // Build the step and check that the basics function are correct.
+    steps::ImuDataLoading const step{imu_id_, "", imu_sampler_};
 
-    AssetId const imu_id{database::GetOrCreateAsset(db.get(), AssetType::Imu, 0, "")};
-
-    // Build the step and check that the type and hash function are correct.
-    steps::ImuDataLoading const step{imu_id, "", imu_sampler_};
     EXPECT_EQ(step.Type(), StepType::ImuDataLoading);
-    EXPECT_EQ(step.Assets(), std::vector{imu_id});
+    EXPECT_EQ(step.Assets(), std::vector{imu_id_});
     EXPECT_EQ(step.CacheKey().value, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
 
     // Build the actual database step id and execute the step.
-    auto const [step_id, _]{database::GetOrCreateStep(db.get(), StepType::ImuDataLoading, "")};
-    EXPECT_NO_THROW(step.Execute(step_id, db));
+    auto const [step_id, _]{database::GetOrCreateStep(db_.get(), StepType::ImuDataLoading, "")};
+    EXPECT_NO_THROW(step.Execute(step_id, db_));
 
     // Load the result and check it's the same as the input
-    auto const result{database::ImuDataSelect(db.get(), step_id, imu_id)};
+    auto const result{database::ImuDataSelect(db_.get(), step_id, imu_id_)};
     EXPECT_EQ(std::size(result), std::size(imu_data_));
+
     for (auto const timestamp_ns : imu_data_ | std::views::keys) {
-        EXPECT_TRUE(result.at(timestamp_ns).angular_velocity.isApprox(imu_data_.at(timestamp_ns).angular_velocity));
-        EXPECT_TRUE(
-            result.at(timestamp_ns).linear_acceleration.isApprox(imu_data_.at(timestamp_ns).linear_acceleration));
+        auto const& result_i{result.at(timestamp_ns)};
+        auto const& input_i{imu_data_.at(timestamp_ns)};
+
+        EXPECT_TRUE(result_i.angular_velocity.isApprox(input_i.angular_velocity));
+        EXPECT_TRUE(result_i.linear_acceleration.isApprox(input_i.linear_acceleration));
     }
 }
