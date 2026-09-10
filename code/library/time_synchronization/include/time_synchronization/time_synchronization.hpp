0021d2eb5204b56dfcc4df78125d3b5022c0b838
reprojection-calibration <reprojection.calibration@gmail.com>
2026-09-10 14:21:22 +0200

    refactor time sync to its own package

--- code/library/time_synchronization/include/time_synchronization/time_synchronization.hpp
+++ code/library/time_synchronization/include/time_synchronization/time_synchronization.hpp
@@ -3,10 +3,10 @@
 #include <cstdint>
 #include <set>
 
-namespace reprojection::calibration {
+namespace reprojection::time_synchronization {
 
 std::set<uint64_t>::const_iterator FindClosest(std::set<uint64_t> const& data, uint64_t timestamp);
 
 bool IsWithinThreshold(uint64_t lhs, uint64_t rhs, uint64_t threshold_ns);
 
-}  // namespace reprojection::calibration
+}  // namespace reprojection::time_synchronization
