739be3c8533ea95354dd6de5506c8ebfecb6c75e
reprojection-calibration <reprojection.calibration@gmail.com>
2026-09-11 10:35:26 +0200

    intrinsic init logging

--- code/library/steps/src/intrinsic_initialization.cpp
+++ code/library/steps/src/intrinsic_initialization.cpp
@@ -30,19 +30,19 @@ Hash IntrinsicInitialization::CacheKey() const {
 }
 
 void IntrinsicInitialization::Execute(StepId const step_id, SqlitePtr const db) const {
-    auto const intrinsics{calibration::InitializeIntrinsics(camera_info_.camera_model, camera_info_.bounds.v_max,
-                                                            camera_info_.bounds.u_max, targets_, num_threads_)};
-    if (not intrinsics.has_value()) {
+    auto const intrinsic{calibration::InitializeIntrinsics(camera_info_.camera_model, camera_info_.bounds.v_max,
+                                                           camera_info_.bounds.u_max, targets_, num_threads_)};
+    if (not intrinsic.has_value()) {
         // LCOV_EXCL_START
         log->error("{{{}, 'msg': 'Failed to initialize intrinsics.'}}", StepLogInfo{Type(), step_id, camera_id_});
         std::exit(1);
         // LCOV_EXCL_STOP
     }
 
-    log->info("{{{}, 'camera_model': '{}', 'intrinsic: {}}}}}", StepLogInfo{Type(), step_id, camera_id_},
-              ToString(camera_info_.camera_model), *intrinsics);
+    log->info("{{{}, 'camera_model': '{}', 'intrinsic: {:.3f}}}}}", StepLogInfo{Type(), step_id, camera_id_},
+              ToString(camera_info_.camera_model), fmt::join(*intrinsic, ", "));
 
-    database::IntrinsicInsert(db.get(), step_id, camera_id_, camera_info_.camera_model, Intrinsic{*intrinsics});
+    database::IntrinsicInsert(db.get(), step_id, camera_id_, camera_info_.camera_model, Intrinsic{*intrinsic});
 }
 
 }  // namespace reprojection::steps
