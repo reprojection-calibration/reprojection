5e21068852eb3ca7b0ba80a3f6909e17ccc339d3
reprojection-calibration <reprojection.calibration@gmail.com>
2026-09-15 14:42:05 +0200

    format

--- code/resources/sql/reprojection_errors_table.sql
+++ code/resources/sql/reprojection_errors_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS reprojection_errors
 (
-    step_id        INTEGER NOT NULL,
-    source_step_id INTEGER NOT NULL,
-    asset_id       INTEGER NOT NULL,
-    sample_timestamp_ns   INTEGER NOT NULL,
-    frame_timestamp_ns   INTEGER NOT NULL,
-    data           BLOB    NOT NULL,
+    step_id             INTEGER NOT NULL,
+    source_step_id      INTEGER NOT NULL,
+    asset_id            INTEGER NOT NULL,
+    sample_timestamp_ns INTEGER NOT NULL,
+    frame_timestamp_ns  INTEGER NOT NULL,
+    data                BLOB    NOT NULL,
 
     FOREIGN KEY (step_id) REFERENCES steps (id) ON DELETE CASCADE,
     FOREIGN KEY (source_step_id, asset_id, sample_timestamp_ns) REFERENCES extracted_targets (step_id, asset_id, timestamp_ns) ON DELETE CASCADE,
